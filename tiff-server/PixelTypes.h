97c3adff363fe5d45fcc6d3e07ccb4e51535504c
Jochen Kuepper <jochen.kuepper@cfel.de>
2009-05-28 12:02:27 +0000

    remove old tiff-server

--- tiff-server/PixelTypes.h
+++
@@ -1,9 +0,0 @@
-//
-//  PixelTypes.h
-//  Image pixel types
-//
-//  Created by Jochen Küpper on 1.. May 2009
-//  Copyright (C) 2009 Jochen Küpper. All rights reserved.
-//
-
-typedef unsigned char Pixel;
