534367c10c2cb27ba947844fbd334ed0132c0a41
Lutz Foucar <lutz.foucar@asg.mpg.de>
2009-06-03 11:52:05 +0000

    Changed REMIAnalysis.cpp and REMIAnalysis.h such that it would work with the classes that already exist.
    REMIAnalysis.h: 
    added Channel, Detector and Layerproperty Class. They contain the parameters, that the program needs to analyze the signals and sort the found peaks for detektorhits.
    Changed Class RawData, such that it now contains the ConfigV1 and DataDescV1 Classes from LCLS.
    Analysis::operator() now gets Rawdata and a reference to cass::Event and returns void.
    
    REMIAnalysis.cpp: 
    added and defined Analysis::init function.
    added and defined Analysis::operator() function. It now has a body that would work with existing Classes.

--- cass_REMI/REMIAnalysis.cpp
+++ cass_REMI/REMIAnalysis.cpp
@@ -1,2 +1,35 @@
 #include "REMIAnalysis.h"
 
+namespace cass
+{
+namespace REMI
+{
+
+void Analysis::init(const cass::REMI::Parameter& param)
+{
+	//copy the parameters//
+	_param = param;
+	//intitalize the signal analyzer//
+	siganalyzer.init(param._anaMethod);
+	//initialize the Detectorhit sorter//
+	sorter.init(param._sortMethod);
+}
+
+void Analysis::operator()(const RawData& data, cass::Event &cassEvent)
+{
+	//create the datastructure from the RawData and the parameters//
+	Remi::Event e(RawData.data(),RawData.config(),_param);
+
+	//find the peaks in the signals of all channels//
+	siganalyzer.FindPeaksIn(e);
+	
+	//extract the peaks for the layers//
+	//and sort the peaks for detektor hits//
+	//fill the results in the Cass Event//
+	sorter.Sort(e,cassEvent);
+}
+
+
+}//end namespace Remi
+}//end namespace cass
+
--- cass_REMI/REMIAnalysis.h
+++ cass_REMI/REMIAnalysis.h
@@ -9,6 +9,10 @@
 #include <vector>
 #include "cass_remi.h"
 #include "AnalysisBackend.h"
+#include "Config.h"
+#include "DataDescriptor.h"
+#include "Classes/SignalAnalyzer/SignalAnalyzer.h"
+#include "Classes/DetektorHitSorter/DetektorHitSorter.h"
 
 namespace cass {
 namespace REMI {
@@ -18,30 +22,81 @@ namespace REMI {
 @author Jochen Küpper
 @version 0.1
 */
-class Parameter : cass::BackendParameter {
+
+class Layerproperty
+{
 public:
-    /* a dictionary of all user settings
+	//the properties of one layer of the delayline-anode//
+	long			_polarity;			//the Polarity the Signal have
+	double			_tsLow;				//lower edge of the timesum
+	double			_tsHeigh;			//upper edge of the timesum
+	double			_trLow;				//lower edge of the timerange events can happen in
+	double			_trHigh;			//upper edge of the timerange events can happen in
+	double			_sf;				//scalefactor for layer
+	int				_chNbrFiEnd;		//the channel that we will find the Signals of the first end of the Anode
+	int				_chNbrSeEnd;		//the channel that we will find the Signals of the second end of the Anode
+};
 
-    The following entries (keys) must be present:
-    - "Scalefactor U"
-    - "Scalefactor V"
-    - Scalefactor W
-    ...
+class Detector
+{
+public:
+	//the settings for one detector//
+	double			_Runtime;			//the runtime over the anode
+	double			_wOff;				//the offset of w-layer towards u and v-layer
+	double			_mcpRadius;			//the radius of the MCP in mm
+	double			_deadMCP;			//the Deadtime between to Signals on the MCP
+	double			_deadAnode;			//the Deadtime between to Signals on the Layers
+	int				_chNbrMcp;			//the Channel that we will find the mcp signals in
+	Layerproperty	_uLayer;			//the properties of the U-Layer
+	Layerproperty	_vLayer;			//the properties of the V-Layer
+	Layerproperty	_wLayer;			//the properties of the W-Layer
+	const char *	_name;				//a name for this detector (ie. Electrondetector, Iondetector)
+};
 
-    The following entries (keys) are used if available:
-    <none>
-    */
-    std::map<std::string, double> settings;
+class Channel
+{
+
+public:
+	//the settings for one channel//
+	double			_thresh;			//the threshold of the channel
+	double			_offset;			//the offset
+	int				_back;				//the backsize
+	int				_stepsize;			//the stepsize
+	int				_delay;				//the delay of the cfd
+	double			_fraction;			//the fraction of the cfd
+	double			_walk;				//the walk of the cfd
+};
+
+
+class Parameter : cass::BackendParameter 
+{
+public:
+    //a dictionary of all user settings
+
+    //The following entries (keys) must be present:
+	std::map<Detector,int>	_detectors;	//we have the option to have 1 or 2 Detectors
+	std::map<Channel,int>	_channels;	//settings to extract peaks of the channels
+	int						_sortMethod;//way how peaks are sorted for detectorhits
+	int						_anaMethod;	//way how peaks are identified
+
+    //The following entries (keys) are used if available:
+    //<none>
+
+    std::map<std::string, double> _settings;
 };
 
 
 
 /** REMI data container */
-class RawData {
+class RawData 
+{
 public:
-    char data[0xfff]; // <-- this is a dummy -- the class would look like this:
-    // Pds::Acqiris::ConfigV1& config;
-    // Pds::Acqiris::DataDescV1& data;
+	const Pds::Acqiris::ConfigV1&	config()const	{return _config;}
+	const Pds::Acqiris::DataDescV1&	data()const		{return _data;}
+
+private:
+     Pds::Acqiris::ConfigV1&	_config;
+     Pds::Acqiris::DataDescV1&	_data;
 };
 
 
@@ -50,23 +105,23 @@ public:
 
 @author Jochen Küpper
 @version 0.1
-*/
-class Signal {
-public:
-    double x, y, t;
-};
+//*/
+//class Particle {
+//public:
+//    double _posx, _posy, _tof;
+//};
 
 
 
-enum HistogramType {
-    FWHM_U1, FWHM_U2, FWHM_U3
-};
-
-
-
-/* @class histogram container */
-class Histogram {
-};
+//enum HistogramType {
+//    FWHM_U1, FWHM_U2, FWHM_U3
+//};
+//
+//
+//
+///* @class histogram container */
+//class Histogram {
+//};
 
 
 
@@ -79,17 +134,16 @@ class CASS_REMISHARED_EXPORT Analysis : cass::AnalysisBackend
 {
 public:
 
-    Analysis(const Parameter& param);
+	Analysis(const Parameter& param)		{init(param);}
 
     /** initialize AnalysisBackend with new set of parameters */
     virtual void init(const Parameter& param);
 
     /* analyse dataset
-
     @param data Raw data to be analysed
     @return analysed data
     */
-    virtual std::vector<Signal> operator()(const RawData& data);
+	virtual operator()(const RawData& data, cass::Event &cassevent);
 
     /* provide analysis histogram
 
@@ -98,12 +152,17 @@ public:
     @param type Which histogram do we want
     @return histogram data
     */
-    Histogram histogram(HistogramType type);
+    //Histogram histogram(HistogramType type);
+
+private:
+	SignalAnalyzer		siganalyzer;
+	DetektorHitSorter	sorter;
+	Parameter			_param;
 };
 
 
-}
-}
+} //end namespace REMI
+} //end namespace CASS
 
 #endif
 
