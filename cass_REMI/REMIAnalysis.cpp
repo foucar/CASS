d148c771fe74e0923c07a02bb30a2bc0b975355c
Jochen Kuepper <jochen.kuepper@cfel.de>
2009-07-05 09:04:25 +0000

    Emacs footer, delete-trailing-whitespace

--- cass_REMI/REMIAnalysis.cpp
+++ cass_REMI/REMIAnalysis.cpp
@@ -15,10 +15,20 @@ void cass::REMI::Analysis::operator()(cass::REMI::REMIEvent& remievent)
 {
 	//find the peaks in the signals of all channels//
     fSiganalyzer.findPeaksIn(remievent);
-	
+
 	//extract the peaks for the layers//
 	//and sort the peaks for detektor hits//
 	//fill the results in the Cass Event//
 	//this has to be done for each detektor individually//
     fSorter.sort(remievent);
 }
+
+
+
+// Local Variables:
+// coding: utf-8
+// mode: C++
+// c-file-offsets: ((c . 0) (innamespace . 0))
+// c-file-style: "Stroustrup"
+// fill-column: 100
+// End:
