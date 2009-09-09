ec200830791bf1803e97c6fa24d4e56d6bc00c67
Lutz Foucar <lutz.foucar@asg.mpg.de>
2009-09-09 12:41:25 +0000

    forward declared Event Classes to not have to many dependencies

--- cass/cassevent.cpp
+++ cass/cassevent.cpp
@@ -1,2 +1,17 @@
 #include "cassevent.h"
 
+#include "REMIEvent.h"
+#include "VMIEvent.h"
+
+cass::CASSEvent::CASSEvent(uint64_t id):
+        _id(id),
+        _remievent(new REMI::REMIEvent()),
+        _vmievent(new VMI::VMIEvent())
+{
+}
+
+cass::CASSEvent::~CASSEvent()
+{
+    delete _vmievent;
+    delete _remievent;
+}
--- cass/cassevent.h
+++ cass/cassevent.h
@@ -1,25 +1,37 @@
 #ifndef CASSEVENT_H
 #define CASSEVENT_H
 
-#include "REMIEvent.h"
-#include "VMIEvent.h"
+#include <stdint.h>
 
 namespace cass
 {
+    namespace REMI
+    {
+        class REMIEvent;
+    }
+    namespace VMI
+    {
+        class VMIEvent;
+    }
+
     class CASSEvent
     {
     public:
-        CASSEvent() {}
+        CASSEvent(uint64_t id);
+        ~CASSEvent();
 
-        REMI::REMIEvent &REMIEvent()        {return _remievent;}
-        VMI::VMIEvent   &VMIEvent()         {return _vmievent;}
+        uint64_t         id()const          {return _id;}
+
+        REMI::REMIEvent &REMIEvent()        {return *_remievent;}
+        VMI::VMIEvent   &VMIEvent()         {return *_vmievent;}
 
 
     private:
-        REMI::REMIEvent     _remievent;
-        VMI::VMIEvent       _vmievent;
-//        pnCCDEvent      _pnccdevent;
-//        MACHINEEvent    _machineevent;
+        uint64_t            _id;
+        REMI::REMIEvent    *_remievent;
+        VMI::VMIEvent      *_vmievent;
+//        pnCCDEvent     *_pnccdevent;
+//        MACHINEEvent   *_machineevent;
     };
 }
 
