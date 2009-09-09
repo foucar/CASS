f37e70654f6c8e66efbe4bbb4d1321aee1996084
Lutz Foucar <lutz.foucar@asg.mpg.de>
2009-09-09 13:27:07 +0000

    added iterator for going through the xtc's of a datagram

---
+++ cass/xtciterator.h
@@ -0,0 +1,75 @@
+#ifndef XTCITERATOR_H
+#define XTCITERATOR_H
+
+#include <map>
+
+#include "FormatConverter.h"
+#include "ConversionBackend.h"
+
+#include "pdsdata/xtc/XtcIterator.hh"
+#include "pdsdata/xtc/Xtc.hh"
+
+namespace cass
+{
+    class XtcIterator : public Pds::XtcIterator
+    {
+    public:
+        enum {Stop, Continue};
+        XtcIterator(Pds::Xtc* xtc, std::map<FormatConverter::Converters,ConversionBackend*>& converters, CASSEvent *cassevent, unsigned depth) :
+                Pds::XtcIterator(xtc),
+                _depth(depth),
+                _converters(converters),
+                _cassevent(cassevent)
+        {
+        }
+
+        int process(Pds::Xtc* xtc)
+        {
+            //find out what type this xtc is//
+            switch (xtc->contains.id())
+            {
+            case (Pds::TypeId::Id_Xtc) ://if it is another xtc we iterate through it recursively
+                {
+                    XtcIterator iter(xtc, _converters, _cassevent, _depth+1);
+                    iter.iterate();
+                    break;
+                }
+            case (Pds::TypeId::Id_Frame) :
+                (*(_converters[FormatConverter::Pulnix]))(xtc,_cassevent);
+                break;
+            case (Pds::TypeId::Id_AcqWaveform) :
+                (*(_converters[FormatConverter::REMI]))(xtc,_cassevent);
+                break;
+            case (Pds::TypeId::Id_AcqConfig) :
+                {
+                    unsigned version = xtc->contains.version();
+                    switch (version)
+                    {
+                    case 1:
+                        (*(_converters[FormatConverter::REMI]))(xtc,_cassevent);
+                        break;
+                    default:
+                        break;
+                    }
+                }
+                break;
+            case (Pds::TypeId::Id_pnCCDconfig) :
+
+                break;
+            case (Pds::TypeId::Id_pnCCDframe) :
+
+                break;
+            default :
+                    break;
+            }
+            return Continue;
+        }
+    private:
+        unsigned _depth;
+        std::map<FormatConverter::Converters, ConversionBackend *>& _converters;
+        CASSEvent *_cassevent;
+    };
+
+}//end namespace
+
+#endif // XTCITERATOR_H
