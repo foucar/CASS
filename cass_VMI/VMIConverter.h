a96064a715d2c2410f22b2e8bf3d022078b26dc7
Lutz Foucar <lutz.foucar@asg.mpg.de>
2009-09-14 09:28:43 +0000

    forward declared vmievent

--- cass_VMI/VMIConverter.h
+++ cass_VMI/VMIConverter.h
@@ -2,18 +2,18 @@
 #define VMICONVERTER_H
 
 #include "cass_VMI.h"
-#include "VMIEvent.h"
 #include "ConversionBackend.h"
 
 namespace cass
 {
+    class CASSEvent;
+
     namespace VMI
     {
         class CASS_VMISHARED_EXPORT Converter : public cass::ConversionBackend
         {
         public:
-           //called for LCLS event//
-//            void operator()(const Pds::Camera::FrameV1&, VMIEvent&);
+            //called for LCLS event//
             void operator()(const Pds::Xtc*, cass::CASSEvent*);
 
         };
