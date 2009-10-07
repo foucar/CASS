876a9c97a71d514cbc865ba7e374cba58f263995
Lutz Foucar <lutz.foucar@asg.mpg.de>
2009-10-07 17:24:17 +0000

    updated to webpage

--- LCLS/pdsdata/xtc/ClockTime.hh
+++ LCLS/pdsdata/xtc/ClockTime.hh
@@ -1,37 +1,26 @@
 #ifndef Pds_ClockTime_hh
 #define Pds_ClockTime_hh
 
-namespace Pds {
+#include <stdint.h>
 
+namespace Pds {
   class ClockTime {
   public:
-    ClockTime() {}
-    ClockTime(const ClockTime& t) : low(t.low), high(t.high) {}
-    ClockTime(unsigned sec,
-	      unsigned nsec) : low(nsec), high(sec) {}
+    ClockTime();
+    ClockTime(const ClockTime& t);
+    ClockTime(unsigned sec, unsigned nsec);
 
-    unsigned seconds    () const { return high; }
-    unsigned nanoseconds() const { return low; }
+  public:
+    unsigned seconds    () const {return _high;}
+    unsigned nanoseconds() const {return _low;}
 
+  public:
     ClockTime& operator=(const ClockTime&);
     bool operator>(const ClockTime&) const; 
 
-    unsigned low;
-    unsigned high;
+  private:
+    uint32_t _low;
+    uint32_t _high;
   };
 }
-
-
-inline Pds::ClockTime& Pds::ClockTime::operator=(const Pds::ClockTime& input)
-{
-  low  = input.low;
-  high = input.high;
-  return *this;
-}
-
-inline bool Pds::ClockTime::operator>(const Pds::ClockTime& t) const
-{
-  return (high > t.high) | (high == t.high && low > t.low);
-}
-
 #endif
