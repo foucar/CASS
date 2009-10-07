e996ed66361c61425f76d22b43913bf1198a3562
Lutz Foucar <lutz.foucar@asg.mpg.de>
2009-10-07 17:14:02 +0000

    added stuff to compile LCLS Programs

---
+++ LCLS/projects.mk
@@ -0,0 +1,11 @@
+# List of projects (low level first)
+projects := pdsdata acqiris evgr leutron pds pdsapp qt qwt
+
+acqiris_use := /reg/g/pcds/package/external/acqiris_V00-00-00
+evgr_use    := /reg/g/pcds/package/external/evgr_V00-00-02
+leutron_use := /reg/g/pcds/package/external/leutron_V00-00-00
+qt_use      := /reg/g/pcds/package/qt-4.3.4
+qwt_use     := /reg/g/pcds/package/external/qwt-5.1.1
+pds_use     := release
+pdsdata_use := release
+pdsapp_use  := release
