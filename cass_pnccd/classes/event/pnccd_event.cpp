5a759a3a1f750825db93c659430f36e28b7758b9
Lutz Foucar <lutz.foucar@asg.mpg.de>
2009-10-24 19:25:23 +0000

    another test

--- cass_pnccd/classes/event/pnccd_event.cpp
+++ cass_pnccd/classes/event/pnccd_event.cpp
@@ -171,7 +171,7 @@ cass::pnCCD::pnCCDEvent::init
 
   //test
   size_t idx=0;
-  uint16_t* data = frm_data;
+  uint16_t* data = reinterpret_cast<uint16_t*>(frm_data);
   for (size_t iy=0;iy<array_y_size_.at(ccd_id-1);++iy)
   {
       for (size_t ix=0;ix<array_y_size_.at(ccd_id-1);++ix)
