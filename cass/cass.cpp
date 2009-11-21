b54fee66bb73a8a4d986e5cfbcf390b61bc53506
Lutz Foucar <lutz.foucar@asg.mpg.de>
2009-11-21 23:37:56 +0000

    changed indentation

--- cass/cass.cpp
+++ cass/cass.cpp
@@ -23,59 +23,59 @@
 
 int main(int argc, char **argv)
 {
-    // construct Qt application object
-    QApplication app(argc, argv);
+  // construct Qt application object
+  QApplication app(argc, argv);
 
-    // create event queue object
-    cass::EventQueue *input(new cass::EventQueue());
-    // create event manager object
-    cass::EventManager *eventmanager(new cass::EventManager());
-    // create format converter object
-    cass::FormatConverter *conversion(cass::FormatConverter::instance(input,eventmanager));
-    // create analysis object
-    cass::Analyzer *analysis(new cass::Analyzer());
-    // create database object
-    cass::database::Database *database(new cass::database::Database());
-    // create a ratemeter object
-    cass::Ratemeter *ratemeter(new cass::Ratemeter());
-    // create a dialog object
-    cass::Window * window(new cass::Window());
+  // create event queue object
+  cass::EventQueue *input(new cass::EventQueue());
+  // create event manager object
+  cass::EventManager *eventmanager(new cass::EventManager());
+  // create format converter object
+  cass::FormatConverter *conversion(cass::FormatConverter::instance(input,eventmanager));
+  // create analysis object
+  cass::Analyzer *analysis(new cass::Analyzer());
+  // create database object
+  cass::database::Database *database(new cass::database::Database());
+  // create a ratemeter object
+  cass::Ratemeter *ratemeter(new cass::Ratemeter());
+  // create a dialog object
+  cass::Window * window(new cass::Window());
 
-    // connect the objects
-    QObject::connect (input, SIGNAL(nextEvent(quint32)), conversion, SLOT(processDatagram(quint32)));
-    QObject::connect (conversion, SIGNAL(nextEvent(cass::CASSEvent*)), analysis, SLOT(processEvent(cass::CASSEvent*)));
-    QObject::connect (analysis, SIGNAL(nextEvent(cass::CASSEvent*)), database, SLOT(add(cass::CASSEvent*)));
-    QObject::connect (database, SIGNAL(nextEvent()), ratemeter, SLOT(nextEvent()));
+  // connect the objects
+  QObject::connect (input, SIGNAL(nextEvent(quint32)), conversion, SLOT(processDatagram(quint32)));
+  QObject::connect (conversion, SIGNAL(nextEvent(cass::CASSEvent*)), analysis, SLOT(processEvent(cass::CASSEvent*)));
+  QObject::connect (analysis, SIGNAL(nextEvent(cass::CASSEvent*)), database, SLOT(add(cass::CASSEvent*)));
+  QObject::connect (database, SIGNAL(nextEvent()), ratemeter, SLOT(nextEvent()));
 
-    // connect controls
-    QObject::connect (window, SIGNAL (load()), analysis, SLOT(loadSettings()));
-    QObject::connect (window, SIGNAL (save()), analysis, SLOT(saveSettings()));
-    QObject::connect (window, SIGNAL (start()), input, SLOT(start()));
-    QObject::connect (window, SIGNAL (quit()), input, SLOT(end()));
+  // connect controls
+  QObject::connect (window, SIGNAL (load()), analysis, SLOT(loadSettings()));
+  QObject::connect (window, SIGNAL (save()), analysis, SLOT(saveSettings()));
+  QObject::connect (window, SIGNAL (start()), input, SLOT(start()));
+  QObject::connect (window, SIGNAL (quit()), input, SLOT(end()));
 
-    // when the thread has finished, we want to close this application
-    QObject::connect(input, SIGNAL(finished()), window, SLOT(close()));
+  // when the thread has finished, we want to close this application
+  QObject::connect(input, SIGNAL(finished()), window, SLOT(close()));
 
-    //show dialog//
-    window->show();
+  //show dialog//
+  window->show();
 
-    // start input thread
-    input->start();
+  // start input thread
+  input->start();
 
-    // start Qt event loop
-    int retval(app.exec());
+  // start Qt event loop
+  int retval(app.exec());
 
-    // clean up
-    delete window;
-    delete ratemeter;
-    delete database;
-    delete analysis;
-    cass::FormatConverter::destroy();
-    delete eventmanager;
-    delete input;
+  // clean up
+  delete window;
+  delete ratemeter;
+  delete database;
+  delete analysis;
+  cass::FormatConverter::destroy();
+  delete eventmanager;
+  delete input;
 
-    // finish
-    return retval;
+  // finish
+  return retval;
 }
 
 
