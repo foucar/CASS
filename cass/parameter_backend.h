059e3d2da02001c0d80659436d29a45781f87666
Nicola Coppola <nicola.coppola@xfel.edu>
2010-03-26 14:17:35 +0000

    introduced ROIBackend

--- cass/parameter_backend.h
+++ cass/parameter_backend.h
@@ -21,6 +21,25 @@ namespace cass
     virtual void load()=0;
     virtual void save()=0;
   };
+
+  class CASSSHARED_EXPORT ROIBackend : public QSettings
+  {
+  public:
+    ROIBackend():
+      QSettings("./cass.ini",QSettings::IniFormat)
+      {
+        beginGroup("cass");
+      }
+
+      virtual ~ROIBackend()
+      {
+        endGroup();
+      }
+
+      virtual void load()=0;
+      virtual void save()=0;
+  };
+
 }//end namespace cass
 
 #endif // PARAMETER_BACKEND_H
