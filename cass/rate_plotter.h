2d9c40b2a2963c70e9617c07dc7c77d06b0635e3
Lutz Foucar <lutz.foucar@asg.mpg.de>
2010-04-08 18:35:22 +0000

    created doc

--- cass/rate_plotter.h
+++ cass/rate_plotter.h
@@ -1,4 +1,4 @@
-//Copyright (C) 2010 lmf
+//Copyright (C) 2010 Lutz Foucar
 
 #ifndef _RATE_PLOTTER_H_
 #define _RATE_PLOTTER_H_
@@ -12,23 +12,33 @@
 
 namespace cass
 {
+  //forward declarations
   class Ratemeter;
-  //class that will plot the rates calculated in the given ratemeters
+
+  /*! class that will plot the rates calculated in the given ratemeters
+
+    @author Lutz Foucar
+    */
   class CASSSHARED_EXPORT RatePlotter : public QObject
   {
     Q_OBJECT;
 
   public:
+    /** constructor
+      @param inputrate the rate of the input thread
+      @param analyzerate the rate how fast the workers work on the events
+      @param parent the qt parent of this object
+      */
     RatePlotter(Ratemeter &inputrate,Ratemeter &analyzerate, QObject *parent=0);
-    ~RatePlotter();
 
   private slots:
+    /** slot to tell the plotter to plot the current rate*/
     void plot();
 
   private:
-    QTimer     _timer;        //the timer that tells it to plot the rate
-    Ratemeter &_inputrate;    //reference to the input rate
-    Ratemeter &_analyzerate;  //reference to the analyze rate
+    QTimer     _timer;        //!< the timer that tells it to plot the rate
+    Ratemeter &_inputrate;    //!< reference to the input rate
+    Ratemeter &_analyzerate;  //!< reference to the analyze rate
   };
 }
 
