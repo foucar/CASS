fcf8281b63ecc9d1b360dc8d5a656f47e59aded0
Lutz Foucar <lutz.foucar@asg.mpg.de>
2010-04-08 22:58:16 +0000

    added doc

--- cass_machinedata/machine_converter.h
+++ cass_machinedata/machine_converter.h
@@ -1,3 +1,5 @@
+// Copyright (C) Lutz Foucar
+
 #ifndef MACHINEDATACONVERTER_H
 #define MACHINEDATACONVERTER_H
 
@@ -12,21 +14,27 @@ namespace cass
 
   namespace MachineData
   {
+    /*! Converter for Beamline-, Cavity-, Epics- and EVR Data
+
+      Will convert Beamline data, Cavity data, Epics Data and EVR Data
+      @todo include EVR Data
+      @author Lutz Foucar
+      */
     class CASS_MACHINEDATASHARED_EXPORT Converter : public cass::ConversionBackend
     {
     public:
-      Converter()   {}
-      //called for appropriate xtc part//
+      //! called for appropriate xtc part//
       void operator()(const Pds::Xtc*, cass::CASSEvent*);
 
     private:
+      /** convenience typedef for easier readable code*/
       typedef std::map<int,std::string> indexMap_t;
 
     private:
-      indexMap_t        _index2name;  //map to convert indexes to strings
-      MachineDataDevice _store;       //a container for the machindata values
-                                      //this is necessary, since not every shot there
-                                      //is info about the epics values
+      indexMap_t        _index2name;  //!< map to convert epics indexes to strings
+      //! a container for the machindata values
+      /** this is necessary, since not every shot there is info about the epics values*/
+      MachineDataDevice _store;
     };
   }//end namespace MachineData
 }//end namespace cass
