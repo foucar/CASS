02f769796333ed3af8f868bf9fe8cd06f38f773a
Lutz Foucar <lutz.foucar@asg.mpg.de>
2010-04-10 18:27:26 +0000

    created the offset of the evreventcode, needed for the converter

--- cass_machinedata/cass_machine.h
+++ cass_machinedata/cass_machine.h
@@ -1,3 +1,6 @@
+// Copyright (C) 2009 Jochen Kuepper
+// Copyright (C) 2009 Jochen Kuepper
+
 #ifndef CASS_MACHINE_GLOBAL_H
 #define CASS_MACHINE_GLOBAL_H
 
@@ -9,4 +12,15 @@
 #  define CASS_MACHINEDATASHARED_EXPORT Q_DECL_IMPORT
 #endif
 
+namespace cass
+{
+  namespace MachineData
+  {
+    /** we are assigned a given eventcode range which is 8 long
+        this range does not start at 0 but at the offset which
+        is defined here.
+      */
+    const int EVREventCodeOffset=67;
+  }
+}
 #endif // CASS_MACHINE_GLOBAL_H
