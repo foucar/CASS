89fabf3ec7d472af4de34d2d49eda1070ac3b2e9
Lutz Foucar <lutz.foucar@asg.mpg.de>
2010-04-11 17:17:03 +0000

    modified layout

--- cass/cass.cpp
+++ cass/cass.cpp
@@ -1,6 +1,6 @@
 // Copyright (C) 2009,2010 Jochen Küpper
 // Copyright (C) 2009,2010 Lutz Foucar
-//
+
 #include <iostream>
 #include <QtGui/QApplication>
 
@@ -16,147 +16,147 @@
 
 
 
-/*!
-@mainpage CASS (CFEL ASG Software Suite)
-
-@section toc Table of Contents
-<ul>
- <li> @ref download
- <li> @ref desc
- <li> @ref inst
- <ul>
-  <li> @ref pre
-  <li> @ref env
-  <li> @ref build
-  <ul>
-   <li> @ref build_LCLS
-   <li> @ref build_CASS
-  </ul>
- </ul>
- <li> @ref run
-</ul>
-
-@section download Getting CASS
-You can access cass via svn from the following Repository:\n
-https://www.mpi-hd.mpg.de/repos/lutz/diode \n
-The version we are working with right now is to be found in /branches/lutz
-(please contact Lutz Foucar for access)
-
-@section desc Brief description about program flow
-<ul>
- <li>Shared mem
- <ul>
-  <li>get lcls data
-  <li>takes out a cassevent from ringbuffer
-  <li>converter covnerts lcls -> cassevent
-  <li>puts it back to ringbuffer
- </ul>
- <li>worker (mulitple)
- <ul>
-  <li>takes cassevent out of ringbuffer
-  <li>puts it to
-  <ul>
-   <li>analyzer
-   <ul>
-    <li>puts it to preanalyzers of different devices
-   </ul>
-   <li>postanalyzers
-   <ul>
-    <li>list of userdefined analyzers that extract info from cassevent and put results it in histograms
-   </ul>
-  </ul>
-  <li>puts it back to ringbuffer
- </ul>
-</ul>
-program control is done via a tcpip interface\n
-accesss histograms vi tcpip interface\n
-parameters are loaded using qt's qsettings.
-
-@section inst Installing CASS
-@subsection pre Prerequisites
-The following software packages need to be installed and available for building and running CASS:\n
-  - Qt version 4.5.x or 4.6.x
-@subsection env Needed Environment Variables
-LCLSSYSINCLUDE: needs to point to the root folder of LCLS Stuff that contains the pdsdata folder\n
-LCLSSYSLIB: needs to point to the folder the build libraries are in.\n
-QTDIR:   needs to point where Qt is installed\n
-@subsection build Building CASS
-@subsubsection build_LCLS Compile the LCLS libraries
-cd ${CASS top-level directory}/LCLS \n
-make x86_64-linux
-@subsubsection build_CASS Compile CASS
-Go back to the CASS top-level directory and use qmake with appropriate options to create Makefiles, i. e.,\n
-  cd ${CASS top-level directory}\n
-  qmake -r\n
-then run\n
-  make\n
-  (optional)sudo make install\n
-to build and install the software.
-
-@section run Running CASS
-In Order to run the cass you need to start it with a parameter, the partition tag. When running
-at LCLS in the daq state you need to provide the partition tag of the shared memory server you
-want to connect to. \n
-User settable parameters are to be found in "cass.ini". The later has to be in the folder where
-you start CASS from.
-@subsection testing Testing CASS in offline modus
-For testing CASS you can create the shared memory using xtcmonserver located in the
-build directory of the LCLS subfolder. This will take a xtc file and put the contents into the
-shared memory. You have to give it serval commands which are documented when you run it without
-any command. A typical start command will look like this:\n
-
-LCLS/build/pdsdata/bin/x86_64-linux/xtcmonserver -f xtcfile.xtc -n 4 -s 0x1000000 -p test -r 120 -l\n
-<ul>
- <li>f: filename of file that you want to create the shared memory with
- <li>n: number of datagrams to be stored in the buffer
- <li>s: size of the buffer for that stores one datagram
- <li>p: the name of the partition tag
- <li>r: the rate that you want to simulate
- <li>l: loop. If the end of the file has been reached start from the beginning
-</ul>
-
-@author Nicola Coppola \n
-- depreciated cass_database\n
-- depreciated cass_dictionary\n
-- new pnCCD analysis\n
-- Region of Interest (ROI) implementation\n
-- CASS testing, debug and development\n
-
-@author Lutz Foucar \n
-- general leader \n
-- cass, cass_acqiris, cass_ccd, cass_machinedata implementation \n
-- CASS design, infrastructure development\n
-
-@author Nils Kimmel \n
-- original pnCCD analysis\n
-
-@author Jochen Kuepper \n
-- CASS design, infrastructure development\n
-- cass framework implementation\n
-- TCP server\n
-
-@date 2009-2010
-
-@todo create a client for the tcpserver that will retrieve
-      the postprocessors histgrams and display them
-@todo get updated xtcmonserver.cc from matt so that we
-      can start testing.
-@todo look whether pnccd offsetcorrection is build in and  works
-@todo look whether photonit extraction is build in and works
-@todo describe how to use Nicolas ROI.
-@todo add to mainpage a describtion how to add a custom
-      build postprocessor
-@todo find out why there are the warning messages about const
-      iterators (size_t) when compiling at SLAC
-@todo create GUI for setting the cass.ini variables
-@todo create another acqiris detector, that will just measure the
-      voltage on a given channel
-@todo make detector evaluation lazy by having the getters calc
-      all values.
+/**
+*@mainpage CASS (CFEL ASG Software Suite)
+*
+*@section toc Table of Contents
+*<ul>
+* <li> @ref download
+* <li> @ref desc
+* <li> @ref inst
+* <ul>
+*  <li> @ref pre
+*  <li> @ref env
+*  <li> @ref build
+*  <ul>
+*   <li> @ref build_LCLS
+*   <li> @ref build_CASS
+*  </ul>
+* </ul>
+* <li> @ref run
+*</ul>
+*
+*@section download Getting CASS
+*You can access cass via svn from the following Repository:\n
+*https://www.mpi-hd.mpg.de/repos/lutz/diode \n
+*The version we are working with right now is to be found in /branches/lutz
+*(please contact Lutz Foucar for access)
+*
+*@section desc Brief description about program flow
+*<ul>
+* <li>Shared mem
+* <ul>
+*  <li>get lcls data
+*  <li>takes out a cassevent from ringbuffer
+*  <li>converter covnerts lcls -> cassevent
+*  <li>puts it back to ringbuffer
+* </ul>
+* <li>worker (mulitple)
+* <ul>
+*  <li>takes cassevent out of ringbuffer
+*  <li>puts it to
+*  <ul>
+*   <li>analyzer
+*   <ul>
+*    <li>puts it to preanalyzers of different devices
+*   </ul>
+*   <li>postanalyzers
+*   <ul>
+*    <li>list of userdefined analyzers that extract info from cassevent and put results it in histograms
+*   </ul>
+*  </ul>
+*  <li>puts it back to ringbuffer
+* </ul>
+*</ul>
+*program control is done via a tcpip interface\n
+*accesss histograms vi tcpip interface\n
+*parameters are loaded using qt's qsettings.
+*
+*@section inst Installing CASS
+*@subsection pre Prerequisites
+*The following software packages need to be installed and available for building and running CASS:\n
+*  - Qt version 4.5.x or 4.6.x
+*@subsection env Needed Environment Variables
+*LCLSSYSINCLUDE: needs to point to the root folder of LCLS Stuff that contains the pdsdata folder\n
+*LCLSSYSLIB: needs to point to the folder the build libraries are in.\n
+*QTDIR:   needs to point where Qt is installed\n
+*@subsection build Building CASS
+*@subsubsection build_LCLS Compile the LCLS libraries
+*cd ${CASS top-level directory}/LCLS \n
+*make x86_64-linux
+*@subsubsection build_CASS Compile CASS
+*Go back to the CASS top-level directory and use qmake with appropriate options to create Makefiles, i. e.,\n
+*  cd ${CASS top-level directory}\n
+*  qmake -r\n
+*then run\n
+*  make\n
+*  (optional)sudo make install\n
+*to build and install the software.
+*
+*@section run Running CASS
+*In Order to run the cass you need to start it with a parameter, the partition tag. When running
+*at LCLS in the daq state you need to provide the partition tag of the shared memory server you
+*want to connect to. \n
+*User settable parameters are to be found in "cass.ini". The later has to be in the folder where
+*you start CASS from.
+*@subsection testing Testing CASS in offline modus
+*For testing CASS you can create the shared memory using xtcmonserver located in the
+*build directory of the LCLS subfolder. This will take a xtc file and put the contents into the
+*shared memory. You have to give it serval commands which are documented when you run it without
+*any command. A typical start command will look like this:\n
+*
+*LCLS/build/pdsdata/bin/x86_64-linux/xtcmonserver -f xtcfile.xtc -n 4 -s 0x1000000 -p test -r 120 -l\n
+*<ul>
+* <li>f: filename of file that you want to create the shared memory with
+* <li>n: number of datagrams to be stored in the buffer
+* <li>s: size of the buffer for that stores one datagram
+* <li>p: the name of the partition tag
+* <li>r: the rate that you want to simulate
+* <li>l: loop. If the end of the file has been reached start from the beginning
+*</ul>
+*
+*@author Nicola Coppola \n
+*- depreciated cass_database\n
+*- depreciated cass_dictionary\n
+*- new pnCCD analysis\n
+*- Region of Interest (ROI) implementation\n
+*- CASS testing, debug and development\n
+*
+*@author Lutz Foucar \n
+*- general leader \n
+*- cass, cass_acqiris, cass_ccd, cass_machinedata implementation \n
+*- CASS design, infrastructure development\n
+*
+*@author Nils Kimmel \n
+*- original pnCCD analysis\n
+*
+*@author Jochen Kuepper \n
+*- CASS design, infrastructure development\n
+*- cass framework implementation\n
+*- TCP server\n
+*
+*@date 2009-2010
+*
+*@todo create a client for the tcpserver that will retrieve
+*      the postprocessors histgrams and display them
+*@todo get updated xtcmonserver.cc from matt so that we
+*      can start testing.
+*@todo look whether pnccd offsetcorrection is build in and  works
+*@todo look whether photonit extraction is build in and works
+*@todo describe how to use Nicolas ROI.
+*@todo add to mainpage a describtion how to add a custom
+*      build postprocessor
+*@todo find out why there are the warning messages about const
+*      iterators (size_t) when compiling at SLAC
+*@todo create GUI for setting the cass.ini variables
+*@todo create another acqiris detector, that will just measure the
+*      voltage on a given channel
+*@todo make detector evaluation lazy by having the getters calc
+*      all values.
 */
 
 
-/** the main program*/
+/** The main program*/
 int main(int argc, char **argv)
 {
     // construct Qt application object
