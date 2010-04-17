3d2f40997e2b849fc3081ff9d26a4a1e465a656e
Lutz Foucar <lutz.foucar@asg.mpg.de>
2010-04-17 02:12:14 +0000

    fixed doc

--- cass/histogram_getter.h
+++ cass/histogram_getter.h
@@ -1,4 +1,4 @@
-// Copyright (C) 2010 lmf
+// Copyright (C) 2010 Lutz Foucar
 // Copyright (C) 2010 Jochen Küpper
 
 #ifndef __HISTOGRAM_GETTER_H__
@@ -13,48 +13,44 @@
 namespace cass
 {
 
-/** Histogram retrievel parameters
-@author Jochen Küpper
-*/
-struct HistogramParameter
-{
+  /** Histogram retrievel parameters.
+   * @author Jochen Küpper
+   */
+  struct HistogramParameter
+  {
     explicit HistogramParameter(PostProcessors::id_t _type)
-        : type(_type)
-        {};
+      : type(_type)
+    {};
 
     explicit HistogramParameter(size_t _type)
-        : type(PostProcessors::id_t(_type))
-        {};
+      : type(PostProcessors::id_t(_type))
+    {};
 
     PostProcessors::id_t type;
-};
+  };
 
 
 
-/*! class that will retrive a histogram from the histogram container
-@author Lutz Foucar
-*/
-class CASSSHARED_EXPORT HistogramGetter
-{
-public:
-    /** constructor
-    @param histograms container of all histogram, we will retrieve the requested histograms from there
-    */
+  /** class that will retrive a histogram from the histogram container.
+   * @author Lutz Foucar
+   */
+  class CASSSHARED_EXPORT HistogramGetter
+  {
+  public:
+    /** constructor.
+     * @param histograms container of all histogram,
+              we will retrieve the requested histograms from there
+     */
     HistogramGetter(const PostProcessors::histograms_t& histograms)
-        : _histograms(histograms)
+      : _histograms(histograms)
     {}
-
     //! function that will serialize the requested histogram to a string and return it
     const std::string operator()(const HistogramParameter&) const;
-
     /** Get the Histogram, create an QImage and return that */
     QImage qimage(const HistogramParameter&) const;
-
-
-protected:
-
+  protected:
     const PostProcessors::histograms_t &_histograms;  //!< histogram container
-};
+  };
 
 } //end namespace cass
 
