4063ea17773a2a347071100f4ce99b932b3282c3
Lutz Foucar <lutz.foucar@asg.mpg.de>
2010-04-19 17:57:02 +0000

    fixed doc

--- cass_acqiris/classes/waveformanalyzer/cfd.h
+++ cass_acqiris/classes/waveformanalyzer/cfd.h
@@ -1,4 +1,4 @@
-//Copyright (C) 2009,2010 Lutz Foucar
+//Copyright (C) 2003-2010 Lutz Foucar
 
 #ifndef _CFD_H_
 #define _CFD_H_
@@ -11,32 +11,34 @@ namespace cass
 {
   namespace ACQIRIS
   {
-    /*! @brief Finds Signals in a waveform.
-       Analyzes a waveform and find signals using a constant fraction algorithm.
-       It then does all the further analysis of the identified Signal.
-       This class will work on waveforms of old 8 Bit Acqiris Instruments.
-       @note we should let this class only identify the Signals and create the
-             Signal list. The further analysis of the Signal should be done,
-             when the user requests a property.
-    @author Lutz Foucar
-  */
+    /** Finds Signals in a waveform.
+     * Analyzes a waveform and find signals using a constant fraction algorithm.
+     * It then does all the further analysis of the identified Signal.
+     * This class will work on waveforms of old 8 Bit Acqiris Instruments.
+     * @note we should let this class only identify the Signals and create the
+     *       Signal list. The further analysis of the Signal should be done,
+     *       when the user requests a property.
+     * @note we might be able to not have this a template by using
+     *       waveform_t::value_type..
+     * @author Lutz Foucar
+     */
     class CASS_ACQIRISSHARED_EXPORT CFD8Bit : public WaveformAnalyzerBackend
     {
     public:
       /** constructor*/
       CFD8Bit()    {std::cout << "adding 8 bit Constant Fraction Discriminator waveformanalyzer"<<std::endl;}
       /** analzye the waveform of the channel.
-        @return void
-        @param c The channel that we need to analyze
-        @param r The found peaks go to the result
-      */
+       * @return void
+       * @param c The channel that we need to analyze
+       * @param r The found peaks go to the result
+       */
       virtual void operator()(const Channel&c, ResultsBackend&r);
     };
 
-    /*! @brief Finds signals in a 16 bit waveform
-        @see class CoM8Bit
-        @author Lutz Foucar
-    */
+    /** Finds signals in a 16 bit waveform.
+     * @see class CoM8Bit
+     * @author Lutz Foucar
+     */
     class CASS_ACQIRISSHARED_EXPORT CFD16Bit : public WaveformAnalyzerBackend
     {
     public:
