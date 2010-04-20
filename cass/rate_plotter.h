f925b4ee1be9c44d0f2468f25eb9b939b50ca15c
Lutz Foucar <lutz.foucar@asg.mpg.de>
2010-04-20 02:17:45 +0000

    fixed doc

--- cass/rate_plotter.h
+++ cass/rate_plotter.h
@@ -25,20 +25,20 @@ namespace cass
 
   public:
     /** constructor.
-     * @param inputrate the rate of the input thread
-     * @param analyzerate the rate how fast the workers work on the events
+     * @param inputrate the ratemeter of the input thread
+     * @param analyzerate the ratemeter of the worker threads
      * @param parent the qt parent of this object
      */
     RatePlotter(Ratemeter &inputrate,Ratemeter &analyzerate, QObject *parent=0);
 
   private slots:
-    /** slot to tell the plotter to plot the current rate*/
+    /** slot to tell the plotter to plot the current rates*/
     void plot();
 
   private:
     QTimer     _timer;        //!< the timer that tells it to plot the rate
-    Ratemeter &_inputrate;    //!< reference to the input rate
-    Ratemeter &_analyzerate;  //!< reference to the analyze rate
+    Ratemeter &_inputrate;    //!< reference to the input Ratemeter
+    Ratemeter &_analyzerate;  //!< reference to the workers (analysis) Ratemeter
   };
 }
 
