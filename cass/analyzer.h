ed64d116b633f5298c04d566f6c551ac6c3cd1a1
Lutz Foucar <lutz.foucar@asg.mpg.de>
2010-04-21 01:44:58 +0000

    created documentation for pre analyzers.
    took out the _param. Everything is now handled from within loadsettings

--- cass/analyzer.h
+++ cass/analyzer.h
@@ -1,4 +1,5 @@
 // Copyright (C) 2009 Jochen Küpper
+// Copyright (C) 2009,2010 Lutz Foucar
 
 #ifndef CASS_ANALYZER_H
 #define CASS_ANALYZER_H
@@ -15,43 +16,53 @@ namespace cass
   class CASSEvent;
   class AnalysisBackend;
 
-  class CASSSHARED_EXPORT AnalysisParameter : public cass::ParameterBackend
-  {
-  public:
-    AnalysisParameter()     {beginGroup("PreAnalyzer");}
-    ~AnalysisParameter()    {endGroup();}
-    void load();
-    void save();
+//  class CASSSHARED_EXPORT AnalysisParameter : public cass::ParameterBackend
+//  {
+//  public:
+//    AnalysisParameter()     {beginGroup("PreAnalyzer");}
+//    ~AnalysisParameter()    {endGroup();}
+//    void load();
+//    void save();
+//
+//  public:
+//    bool _useCCD;
+//    bool _useAcqiris;
+//    bool _usepnCCD;
+//    bool _useMachine;
+//  };
 
-  public:
-    bool _useCCD;
-    bool _useAcqiris;
-    bool _usepnCCD;
-    bool _useMachine;
-  };
 
-
+  /** Container and call handler for all Pre Analyzers.
+   * All pre analyzer are here and are getting called depending
+   * on an users choice in cass.ini
+   * @author Jochen Kuepper
+   * @author Lutz Foucar
+   */
   class CASSSHARED_EXPORT Analyzer : public QObject
   {
     Q_OBJECT;
 
   public:
-    //list of known individual analyzers//
+    /** list of known individual analyzers*/
     enum Analyzers {Acqiris, ccd, MachineData, pnCCD};
-    //creates an instace if not it does not exist already//
+    /** creates an instace if not it does not exist already*/
     static Analyzer *instance();
-    //this destroys the the instance//
+    /** this destroys the the instance*/
     static void destroy();
-    //function to use the analyzers for the different instruments//
+    /** function to use the analyzers for the different instruments*/
     void processEvent(cass::CASSEvent*);
 
     //slots called by the gui//
   public slots:
+    /** save the settings of the analyzers*/
     void saveSettings();
+    /** load the settings of the analyzers*/
     void loadSettings(size_t what);
 
   protected:
+    /** protected constructor, should only be called through instance*/
     Analyzer();
+    /** protected destructor, should only be called through destroy*/
     ~Analyzer();
 
   public:
@@ -60,17 +71,15 @@ namespace cass
 
   protected:
     //the parameters//
-    AnalysisParameter _param;
-    //map of available analyzers//
+//    AnalysisParameter _param;
+    /** map of available analyzers*/
     analyzers_t _analyzer;
-    //a set of the active analyzers//
+    /** a set of the active analyzers*/
     active_analyzers_t _activeAnalyzers;
-    //pointer to the instance//
+    /** pointer to the instance*/
     static Analyzer *_instance;
-    //Singleton operation locker in a multi-threaded environment.//
+    /** Singleton operation locker in a multi-threaded environment.*/
     static QMutex _mutex;
-    //
-
   };
 }//end namespace cass
 
