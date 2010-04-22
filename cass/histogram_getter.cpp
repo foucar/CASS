f8ccc06a7f20feb5adfce296af837986862e6436
Lutz Foucar <lutz.foucar@asg.mpg.de>
2010-04-22 02:42:16 +0000

    added some error checking

--- cass/histogram_getter.cpp
+++ cass/histogram_getter.cpp
@@ -15,6 +15,13 @@ const std::string HistogramGetter::operator()(const HistogramParameter& hp) cons
     Serializer serializer;
     // serialize the wanted histogram using the serializer
     PostProcessors::histograms_t::const_iterator iter(_histograms.find(hp.type));
+    //throw an exeption when requested histogram is not present
+    if (iter == _histograms.end())
+      throw std::runtime_error(QString("histogram %1 not found for serialization").arg(hp.type).toStdString());
+    //thow exeption when requested histogram is not created//
+    if (!iter->second)
+      throw std::runtime_error(QString("histogram %1 has not been created").arg(hp.type).toStdString());
+    //serialize requested histogram
     iter->second->serialize(serializer);
     //return the buffer (std::string) of the serializer
     return serializer.buffer();
@@ -25,6 +32,12 @@ QImage HistogramGetter::qimage(const HistogramParameter& hp) const
 {
     // get an iterator to the requested histogram
     PostProcessors::histograms_t::const_iterator iter(_histograms.find(hp.type));
+    //throw an exeption when requested histogram is not present
+    if (iter == _histograms.end())
+      throw std::runtime_error(QString("histogram %1 not found for convertion to qimage").arg(hp.type).toStdString());
+    //thow exeption when requested histogram is not created//
+    if (!iter->second)
+      throw std::runtime_error(QString("histogram %1 has not been created").arg(hp.type).toStdString());
     // and return the QImage of that histogram
     return dynamic_cast<Histogram2DFloat *>(iter->second)->qimage();
 }
