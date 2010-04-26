4b5aa47b4921a8c1f43038c88fa7131256e149e3
Lutz Foucar <lutz.foucar@asg.mpg.de>
2010-04-26 19:14:21 +0000

    updated doc

--- cass_ccd/ccd_converter.h
+++ cass_ccd/ccd_converter.h
@@ -1,3 +1,5 @@
+//Copyright (C) 2009,2010 Lutz Foucar
+
 #ifndef _CCD_CONVERTER_H_
 #define _CCD_CONVERTER_H_
 
@@ -10,12 +12,21 @@ namespace cass
 
   namespace CCD
   {
+    /** Converter for commercial ccd cameras.
+     * This converter will convert the OPAL Camera image to
+     * the ccd detector type of cassevent.
+     * @author Lutz Foucar
+     */
     class CASS_CCDSHARED_EXPORT Converter : public cass::ConversionBackend
     {
     public:
+      /** constructor.*/
       Converter() {}
-      //called for LCLS event//
-      void operator()(const Pds::Xtc*, cass::CASSEvent*);
+      /** convert contents of xtc to cassevent
+       * @param[in] xtc The XTC that contains the info about the frame and the frame itself
+       * @param[out] evt The cassevent that we store the information from the xtc in.
+       */
+      void operator()(const Pds::Xtc*xtc, cass::CASSEvent*evt);
     };
   }//end namespace ccd
 }//end namespace cass
