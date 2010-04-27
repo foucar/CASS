b32aee598facda7973aaa4326872bf1fc7530270
Lutz Foucar <lutz.foucar@asg.mpg.de>
2010-04-27 17:09:03 +0000

    trying to get rid of the segfault occuring in the commercial ccd cameras

--- cass_ccd/ccd_analysis.cpp
+++ cass_ccd/ccd_analysis.cpp
@@ -31,92 +31,97 @@ void cass::CCD::Analysis::operator()(cass::CASSEvent *cassevent)
 {
   //retrieve a pointer to the ccd device we are working on//
   cass::CCD::CCDDevice* dev(dynamic_cast<cass::CCD::CCDDevice*>(cassevent->devices()[cass::CASSEvent::CCD]));
-  //retrieve a reference to the pulnix detector//
-  cass::CCDDetector& det((*dev->detectors())[0]);
-
-  //clear the pixel list//
-  det.pixellist().clear();
-
-  //initialize the start values for integral and max pixel value//
-  pixel_t maxpixelvalue                       = 0;
-  float integral                              = 0;
-  uint16_t framewidth                         = det.columns();
-  uint16_t frameheight                        = det.rows();
-  const cass::CCDDetector::frame_t& frame      = det.frame();
-
-  //go through all pixels of the frame//
-  for (size_t i=0; i<frame.size(); ++i)
+  //go through all detectors and do your stuff//
+  detectors_t::iterator detIt (dev->detectors()->begin());
+  for (; detIt != dev->detectors()->end(); ++detIt)
   {
-    //extract the value and coordinate from the frame//
-    pixel_t pixel         = frame[i];
-    uint16_t xcoordinate  = i % framewidth;
-    uint16_t ycoordinate  = i / framewidth;
+    //retrieve a reference to the ccd detector from the iterator//
+    cass::CCDDetector& det(*detIt);
 
-    //calc integral//
-    integral += pixel;
+    //clear the pixel list//
+    det.pixellist().clear();
 
-    //check whether pixel is above threshold//
-    //then check whether pixel is a local maximum//
-    //if so add it to the pixel list//
-    if (pixel > _param._threshold)
-      //check wether point is not at an edge
-      if (ycoordinate > 0 &&
-          ycoordinate < frameheight-1 &&
-          xcoordinate > 0 &&
-          xcoordinate < framewidth+1)
-      // Check all surrounding pixels
-      if (frame[i-framewidth-1] < pixel && //upper left
-          frame[i-framewidth]   < pixel && //upper middle
-          frame[i-framewidth+1] < pixel && //upper right
-          frame[i-1]            < pixel && //left
-          frame[i+1]            < pixel && //right
-          frame[i+framewidth-1] < pixel && //lower left
-          frame[i+framewidth]   < pixel && //lower middle
-          frame[i+framewidth+1] < pixel)   //lower right
-      {
+    //initialize the start values for integral and max pixel value//
+    pixel_t maxpixelvalue                       = 0;
+    float integral                              = 0;
+    uint16_t framewidth                         = det.columns();
+    uint16_t frameheight                        = det.rows();
+    const cass::CCDDetector::frame_t& frame     = det.frame();
+
+    //go through all pixels of the frame//
+    for (size_t i=0; i<frame.size(); ++i)
+    {
+      //extract the value and coordinate from the frame//
+      pixel_t pixel         = frame[i];
+      uint16_t xcoordinate  = i % framewidth;
+      uint16_t ycoordinate  = i / framewidth;
+
+      //calc integral//
+      integral += pixel;
+
+      //check whether pixel is above threshold//
+      //then check whether pixel is a local maximum//
+      //if so add it to the pixel list//
+      if (pixel > _param._threshold)
+        //check wether point is not at an edge
+        if (ycoordinate > 0 &&
+            ycoordinate < frameheight-1 &&
+            xcoordinate > 0 &&
+            xcoordinate < framewidth+1)
+          // Check all surrounding pixels
+          if (frame[i-framewidth-1] < pixel && //upper left
+              frame[i-framewidth]   < pixel && //upper middle
+              frame[i-framewidth+1] < pixel && //upper right
+              frame[i-1]            < pixel && //left
+              frame[i+1]            < pixel && //right
+              frame[i+framewidth-1] < pixel && //lower left
+              frame[i+framewidth]   < pixel && //lower middle
+              frame[i+framewidth+1] < pixel)   //lower right
+          {
         det.pixellist().push_back(Pixel(xcoordinate,ycoordinate,pixel));
       }
 
-    //get the maximum pixel value//
-    if (maxpixelvalue < pixel)
-      maxpixelvalue = pixel;
-  }
-  //write the found integral and maximum Pixel value to the event//
-  det.integral()     = static_cast<uint32_t>(integral);
-  det.maxPixelValue()= maxpixelvalue;
-
-  //rebinning the frame//
-  //rebin image frame//
-  if (_param._rebinfactor != 1)
-  {
-    //lock this section due to the access to the tmp frame//
-    QMutexLocker locker(&_mutex);
-
-    //get the new dimensions//
-    const size_t newRows = framewidth  / _param._rebinfactor;
-    const size_t newCols = frameheight / _param._rebinfactor;
-    //set the new dimensions in the detector//
-    det.rows()    = newRows;
-    det.columns() = newCols;
-    //resize the temporary container to fit the rebinned image
-    //initialize it with 0
-    _tmp.assign(newRows * newCols,0);
-    //rebin the frame//
-    for (size_t iIdx=0; iIdx<frame.size() ;++iIdx)
-    {
-      //calculate the row and column of the current Index//
-      const size_t row = iIdx / framewidth;
-      const size_t col = iIdx % framewidth;
-      //calculate the index of the rebinned frame//
-      const size_t newRow = row / _param._rebinfactor;
-      const size_t newCol = col / _param._rebinfactor;
-      //calculate the index in the rebinned frame//
-      //that newRow and newCol belongs to//
-      const size_t newIndex = newRow*newCols + newCol;
-      //add this index value to the newIndex value//
-      _tmp[newIndex] += frame[iIdx];
+      //get the maximum pixel value//
+      if (maxpixelvalue < pixel)
+        maxpixelvalue = pixel;
     }
-    //copy the temporary frame to the right place
-    det.frame().assign(_tmp.begin(), _tmp.end());
+    //write the found integral and maximum Pixel value to the event//
+    det.integral()     = static_cast<uint32_t>(integral);
+    det.maxPixelValue()= maxpixelvalue;
+
+    //rebinning the frame//
+    //rebin image frame//
+    if (_param._rebinfactor != 1)
+    {
+      //lock this section due to the access to the tmp frame//
+      QMutexLocker locker(&_mutex);
+
+      //get the new dimensions//
+      const size_t newRows = framewidth  / _param._rebinfactor;
+      const size_t newCols = frameheight / _param._rebinfactor;
+      //set the new dimensions in the detector//
+      det.rows()    = newRows;
+      det.columns() = newCols;
+      //resize the temporary container to fit the rebinned image
+      //initialize it with 0
+      _tmp.assign(newRows * newCols,0);
+      //rebin the frame//
+      for (size_t iIdx=0; iIdx<frame.size() ;++iIdx)
+      {
+        //calculate the row and column of the current Index//
+        const size_t row = iIdx / framewidth;
+        const size_t col = iIdx % framewidth;
+        //calculate the index of the rebinned frame//
+        const size_t newRow = row / _param._rebinfactor;
+        const size_t newCol = col / _param._rebinfactor;
+        //calculate the index in the rebinned frame//
+        //that newRow and newCol belongs to//
+        const size_t newIndex = newRow*newCols + newCol;
+        //add this index value to the newIndex value//
+        _tmp[newIndex] += frame[iIdx];
+      }
+      //copy the temporary frame to the right place
+      det.frame().assign(_tmp.begin(), _tmp.end());
+    }
   }
 }
--- cass_ccd/ccd_converter.cpp
+++ cass_ccd/ccd_converter.cpp
@@ -5,6 +5,7 @@
 
 #include "pdsdata/camera/FrameV1.hh"
 #include "pdsdata/xtc/Xtc.hh"
+#include "pdsdata/xtc/DetInfo.hh"
 #include "cass_event.h"
 #include "ccd_device.h"
 #include "ccd_detector.h"
@@ -13,13 +14,19 @@
 
 void cass::CCD::Converter::operator()(const Pds::Xtc* xtc, cass::CASSEvent* cassevent)
 {
+  //Get the the detector's device id //
+  const Pds::DetInfo& info = *(Pds::DetInfo*)(&xtc->src);
+  const size_t detectorId = info.devId();
   //retrieve a reference to the frame contained int the xtc//
   const Pds::Camera::FrameV1 &frame =
     *reinterpret_cast<const Pds::Camera::FrameV1*>(xtc->payload());
   //retrieve a pointer to the ccd device we are working on//
   cass::CCD::CCDDevice* dev = dynamic_cast<cass::CCD::CCDDevice*>(cassevent->devices()[cass::CASSEvent::CCD]);
+  //if necessary resize the detector container//
+  if (detectorId >= dev->detectors()->size())
+    dev->detectors()->resize(detectorId+1);
   //retrieve a reference to the pulnix detector//
-  cass::CCDDetector& det = (*dev->detectors())[0];
+  cass::CCDDetector& det = (*dev->detectors())[detectorId];
 
   //copy the values status values from the frame to the detector//
   det.columns()          = frame.width();
