61770482c21b92a04697783ce437af2ab6e5ae8e
Jochen Kuepper <jochen.kuepper@cfel.de>
2010-04-27 19:47:02 +0000

    about dialog text

--- jocassview/imageviewer.cpp
+++ jocassview/imageviewer.cpp
@@ -253,19 +253,8 @@ void ImageViewer::on_fitToWindow_triggered()
 
 void ImageViewer::on_about_triggered()
 {
-    QMessageBox::about(this, tr("About Image Viewer"),
-            tr("<p>The <b>Image Viewer</b> example shows how to combine QLabel "
-               "and QScrollArea to display an image. QLabel is typically used "
-               "for displaying a text, but it can also display an image. "
-               "QScrollArea provides a scrolling view around another widget. "
-               "If the child widget exceeds the size of the frame, QScrollArea "
-               "automatically provides scroll bars. </p><p>The example "
-               "demonstrates how QLabel's ability to scale its contents "
-               "(QLabel::scaledContents), and QScrollArea's ability to "
-               "automatically resize its contents "
-               "(QScrollArea::widgetResizable), can be used to implement "
-               "zooming and scaling features. </p><p>In addition the example "
-               "shows how to use QPainter to print an image.</p>"));
+    QMessageBox::about(this, tr("About joCASSview"),
+            tr("<p>The <b>joCASSview</b> is a display client for the CASS software.</p>"));
 }
 
 
