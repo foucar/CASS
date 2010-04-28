449adfd0c7489eb463e25e42df446cec2eabe491
Jochen Kuepper <jochen.kuepper@cfel.de>
2010-04-28 09:25:26 +0000

    set correct application name for QSettings

--- jocassview/jocassview.cpp
+++ jocassview/jocassview.cpp
@@ -6,12 +6,10 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-
     QCoreApplication::setOrganizationName("CFEL-ASG");
     QCoreApplication::setOrganizationDomain("endstation.asg.cfel.de");
-    QCoreApplication::setApplicationName("IMAGEVIEWER");
+    QCoreApplication::setApplicationName("jocassview");
     QSettings::setDefaultFormat(QSettings::IniFormat);
-
     ImageViewer window;
     window.show();
     return app.exec();
