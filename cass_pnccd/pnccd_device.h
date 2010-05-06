b2011d3894ce79285314365225287da67a62a15c
Lutz Foucar <lutz.foucar@asg.mpg.de>
2010-05-06 00:47:10 +0000

    updated documentation and moved defines of pnCCD_default_size and pnCCD_default_size_sq to global header file into the namespace to make sure that they don't spam it.

--- cass_pnccd/pnccd_device.h
+++ cass_pnccd/pnccd_device.h
@@ -7,37 +7,47 @@
 #include <iostream>
 #include "cass_pnccd.h"
 #include "device_backend.h"
-//#include "ccd_detector.h"
 #include "pixel_detector.h"
 
 namespace cass
 {
   namespace pnCCD
   {
+    /** the pnccd device.
+     *
+     * The device contains a list of detectors and is serializable.
+     *
+     * @author Lutz Foucar
+     */
     class CASS_PNCCDSHARED_EXPORT pnCCDDevice : public DeviceBackend
     {
     public:
+      /** default constructor. defining the version*/
       pnCCDDevice()
         :DeviceBackend(1)
       {}
+      /** default destructor*/
       ~pnCCDDevice()       {}
 
-#define pnCCD_default_size    1024
-#define pnCCD_default_size_sq 1024*1024
-
     public:
+      /** define the list of pixel detectors*/
       typedef std::vector<PixelDetector> detectors_t;
 
     public:
+      /** serialize the device to the Serializer*/
       void serialize(cass::Serializer&);
+      /** deserialize the device from the Serializer*/
       void deserialize(cass::Serializer&);
 
     public:
+      /** getter */
       const detectors_t   *detectors()const   {return &_detectors;}
+      /** setter */
       detectors_t         *detectors()        {return &_detectors;}
 
     private:
-      detectors_t          _detectors;        //!< a vector containing all ccd detectors
+      /** container for all pnccd detectors*/
+      detectors_t          _detectors;
     };
   } // end of scope of namespace pnCCD
 } // end of scope of namespace cass
