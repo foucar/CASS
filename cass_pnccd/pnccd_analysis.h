b14e3dd441efe168965eb13deaa58dc8d2b6b4d0
Nicola Coppola <nicola.coppola@xfel.edu>
2010-05-08 05:04:13 +0000

    comment added to param definitions

--- cass_pnccd/pnccd_analysis.h
+++ cass_pnccd/pnccd_analysis.h
@@ -32,6 +32,46 @@ namespace cass
       typedef std::vector<double> correctionmap_t;
     public:
       size_t          _nbrDarkframes;     //the number of fills for each detector//
+      /** User settable parameters via CASS.ini
+       *  each of the following is defined for each pnCCD detector and
+       *  may be defined in a different way
+       *  RebinFactor:
+       *        The rebinfactor for rebinning of the frame accectable values are 1,2,4 and any power of 2
+       *  MaxNoise:
+       *        The max allowed noise level before a pixel is mask off, the limit is interpreted as
+       *        3 x std dev, if the pixel displays a noise level largen than 3x_max_noise than it is removed
+       *  SigmaMultiplier:
+       *        The number of std deviation a pixel must be above noise to be selected as a photon
+       *  Adu2ev
+       *        The adu to eV conversion constant
+       *  CreatePixelList
+       *        true of false, if true and if _doOffsetCorrection==true
+       *        (irregardless of the value of _useCommonMode) a list of Photons will be created
+       *        for each pixel with value larger that _sigmaMultiplier*noise(of the pixel)
+       *  DoOffsetCorrection
+       *        true of false, if true the darkcalibration maps will be used to subtract the ADC
+       *        offset, to correct the raw frame
+       *  useCommonMode
+       *       true of false, useful only if _doOffsetCorrection==true
+       *            if true the CommonMode correction will be calculated and applied to the pixels
+       *            each "row" of 128-pixel is separately considered: the pixels that have
+       *            value below _sigmaMultiplier*noise(of the pixel), once their offset is removed,
+       *            and that are not marked as BAD are used, the arithmetical mean is calculated and
+       *            subtracted from all the pixels
+       *  IntegralOverThres (here called _thres_for_integral )
+       *       any Integer>=0 is accepted, in case the value is >0 than a second integral over the
+       *            corrected frame is calculated, this time using only those pixel that have a
+       *            value>_thres_for_integral
+       *  DarkCalibrationFileName
+       *       if set the darkframe calibrations will be taken out of the named file
+       *       PLEASE DO not set unless you know what you are using,
+       *       darkframe files created via Xonline/Raccoon will be ignored as they have
+       *       a completely different structure
+       *  DarkCalibrationSaveFileName
+       *       it contains the name of the files where darkframe calibrations will be written into
+       *       the values are ANYWAY overwritten by the saveParameter function to avoid
+       *       overwriting of previously saved darkframe files
+       **/
       correctionmap_t _offset;            //offsetmap
       correctionmap_t _noise;             //noise map
       uint32_t        _rebinfactor;       //the rebinfactor for rebinning
@@ -69,6 +109,25 @@ namespace cass
       typedef std::vector<DetectorParameter> detparameters_t;
     public:
       detparameters_t _detectorparameters;  //the parameters of the detector
+
+      /**
+       *  The following is a user settable parameter
+       *  it decide for both pnCCD detectors if the following frames are
+       *  to be used to calculate the offset corrections and noise map
+       *  that are needed to calculate a corrected frame from the raw one
+       *
+       *    After 200 Frames have been seen a warning will be printed out,
+       *    the user can then send a "Save ini" command to CASS to save the
+       *    calibration constants to files. The calculation will proceed anyway
+       *    even after the user has save the darkcal-frames to file
+       *    allowing for a still improved statistical accuracy, in case needed.
+       *
+       *    if IsDarkFrames is set to true all the previously described parameters
+       *    are NOT active as also any ROI that may be defined in the CASS.ini file
+       *
+       *    unfortunately there is a similar parameter for the commercial CCD
+       *    and they can be set to opposite values
+      **/
       bool            _isDarkframe;         //switch telling whether we are collecting darkframes right now
       //flag to set the dark/not-dark run condition
       bool            _This_is_a_dark_run;
