cb7e3720533fa83e947a221b642fc905bc0f6ab6
Lutz Foucar <lutz.foucar@asg.mpg.de>
2010-05-09 05:13:42 +0000

    added a check whether the detector is valid

--- cass/postprocessing/ccd.cpp
+++ cass/postprocessing/ccd.cpp
@@ -175,7 +175,8 @@ void cass::pp101::loadSettings(size_t)
         <<" which has id:"<<_conditionDetector
         <<std::endl;
 
-    HelperAcqirisDetectors::instance(_conditionDetector)->loadSettings();
+    if (_conditionDetector)
+      HelperAcqirisDetectors::instance(_conditionDetector)->loadSettings();
 
     _pp.histograms_delete(_id);
     _image = new Histogram2DFloat(cols,rows);
