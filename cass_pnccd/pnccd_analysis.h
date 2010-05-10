0fc80250cdc61c01573e2d4a68cf93facabf64b3
Nicola Coppola <nicola.coppola@xfel.edu>
2010-05-10 22:50:25 +0000

    comment modif

--- cass_pnccd/pnccd_analysis.h
+++ cass_pnccd/pnccd_analysis.h
@@ -97,7 +97,7 @@ namespace cass
        */
 
       uint32_t        _rebinfactor;       //!< the rebinfactor for rebinning
-      double          _max_noise;         //!< pixels with noise larger than will be masked
+      double          _max_noise;         //!< pixels with noise larger _max_noise than will be masked
       double          _sigmaMultiplier;   //!< how big is "above noise"
       double          _adu2eV;            //!< conversion from adu to eV
       bool            _createPixellist;   //!< flag to switch pixellist on / off
