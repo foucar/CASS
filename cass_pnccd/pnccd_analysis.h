f624ecf4ec4d77ee44aab1d4122f0c0c2b43c02a
Nicola Coppola <nicola.coppola@xfel.edu>
2010-05-11 05:28:46 +0000

    documentation modified

--- cass_pnccd/pnccd_analysis.h
+++ cass_pnccd/pnccd_analysis.h
@@ -25,49 +25,39 @@ namespace cass
   class CASSEvent;
   namespace pnCCD
   {
-
-    class CASS_PNCCDSHARED_EXPORT DetectorParameter
-    {
-    public:
-      typedef std::vector<double> correctionmap_t;
-    public:
-      size_t          _nbrDarkframes;     //the number of fills for each detector//
-      correctionmap_t _offset;            //!< offset map
-      correctionmap_t _noise;             //!< noise map
-
       /**
        *  User settable parameters via CASS.ini
        *  each of the following is defined for each pnCCD detector and
        *  may be defined in a different way on different pnCCD detectors
-       *  @param
+       *  @cassttng
        *  RebinFactor
        *        The rebinfactor for rebinning of the frame accectable values are 1,2,4 and any power of 2.
        *        By default the value is set to 1.
-       *  @param
+       *  @cassttng
        *  MaxNoise
        *        The max allowed noise level before a pixel is mask off, the limit is interpreted as
        *        std dev, if the pixel displays a noise level largen than _max_noise than it is removed.
        *        By default the value is set to 4000.
-       *  @param
+       *  @cassttng
        *  SigmaMultiplier
        *        The number of std deviation a pixel must be above noise to be selected as a photon.
        *        By default the value is set to 4.
-       *  @param
+       *  @cassttng
        *  Adu2ev
        *        The adu to eV conversion constant.
        *        By default the value is set to 1.
-       *  @param
+       *  @cassttng
        *  CreatePixelList
        *        true of false, if true and if _doOffsetCorrection==true
        *            (irregardless of the value of _useCommonMode) a list of Photons will be created
        *            for each pixel with value larger that _sigmaMultiplier*noise(of the pixel).
        *        By default the value is set to false.
-       *  @param
+       *  @cassttng
        *  DoOffsetCorrection
        *        true of false, if true the darkcalibration maps will be used to subtract the ADC
        *            offset, to correct the raw frame.
        *        By default the value is set to false.
-       *  @param
+       *  @cassttng
        *  useCommonMode
        *       true of false, useful only if _doOffsetCorrection==true,
        *            if true the CommonMode correction will be calculated and applied to the pixels
@@ -76,20 +66,20 @@ namespace cass
        *            and that are not marked as BAD are used, the arithmetical mean is calculated and
        *            subtracted from all the pixels.
        *        By default the value is set to false.
-       *  @param
+       *  @cassttng
        *  IntegralOverThres (here called _thres_for_integral )
        *       any Integer>=0 is accepted, in case the value is >0 than a second integral over the
        *            corrected frame is calculated, this time using only those pixel that have a
        *            value>_thres_for_integral
        *        By default the value is set to 0.
-       *  @param
+       *  @cassttng
        *  DarkCalibrationFileName
        *       if set the darkframe calibrations will be taken out of the named file
        *       PLEASE DO NOT SET unless you know what you are using,
        *       (for example if using offline CASS)
        *       darkframe files created via Xonline/Raccoon will be ignored as they have
        *       a completely different structure
-       *  @param
+       *  @cassttng
        *  DarkCalibrationSaveFileName
        *       it contains the name of the files where darkframe calibrations will be written into
        *       the values are ANYWAY overwritten by the saveParameter function to avoid
@@ -97,7 +87,16 @@ namespace cass
        *  @author Nicola Coppola
        *
        */
-      //
+
+
+    class CASS_PNCCDSHARED_EXPORT DetectorParameter
+    {
+    public:
+      typedef std::vector<double> correctionmap_t;
+    public:
+      size_t          _nbrDarkframes;     //!< the number of frames used to calculate offset/noise map, this is an internal value is not "saved" in the calfiles
+      correctionmap_t _offset;            //!< offset map
+      correctionmap_t _noise;             //!< noise map
 
       uint32_t        _rebinfactor;       //!< the rebinfactor for rebinning
       double          _max_noise;         //!< pixels with noise larger _max_noise than will be masked
