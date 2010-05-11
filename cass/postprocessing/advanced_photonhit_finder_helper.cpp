a886b0cdc6011a30363141f429a4bf8a13057896
Lutz Foucar <lutz.foucar@asg.mpg.de>
2010-05-11 22:27:06 +0000

    updated todo list

--- cass/postprocessing/advanced_photonhit_finder_helper.cpp
+++ cass/postprocessing/advanced_photonhit_finder_helper.cpp
@@ -1,3 +1,7 @@
 //Copyright (C) 2010 Lutz Foucar
 
 #include "advanced_photonhit_finder_helper.h"
+
+/** @todo rename this to averaging offset correction, since this is
+ *        actually what it does
+ */
