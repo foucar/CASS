8502afe56ccf465756c0281988ba08e934957e0b
Lutz Foucar <lutz.foucar@asg.mpg.de>
2010-05-11 22:22:23 +0000

    filled helper with first life

---
+++ cass/postprocessing/advanced_photonhit_finder_helper.h
@@ -0,0 +1,19 @@
+//Copyright (C) 2010 Lutz Foucar
+
+#ifndef _PHOTONHIT_HELPER_H_
+#define _PHOTONHIT_HELPER_H_
+
+#include <stdint.h>
+#include <utility>
+#include <algorithm>
+#include <QtCore/QMutex>
+#include <QtCore/QMutexLocker>
+
+#include "cass_event.h"
+
+namespace cass
+{
+
+}
+
+#endif
