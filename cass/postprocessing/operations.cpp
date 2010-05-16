bd309bdb1dddb7e7db5f36220e069b556ce90619
Lutz Foucar <lutz.foucar@asg.mpg.de>
2010-05-16 06:00:29 +0000

    implemented pp803

--- cass/postprocessing/operations.cpp
+++ cass/postprocessing/operations.cpp
@@ -125,7 +125,7 @@ void cass::pp106::loadSettings(size_t)
   _pp.histograms_replace(_id,_result);
 
   std::cout << "PostProcessor_"<<_id
-      << " will substract Histogram in PostProcessor_"<<_idOne
+      <<" will substract Histogram in PostProcessor_"<<_idOne
       <<" from Histogram in PostProcessor_"<<_idTwo
       <<std::endl;
 }
@@ -215,7 +215,7 @@ void cass::pp800::loadSettings(size_t)
   _pp.histograms_replace(_id,_result);
 
   std::cout << "PostProcessor_"<<_id
-      << " will check whether Histogram in PostProcessor_"<<_idOne
+      <<" will check whether Histogram in PostProcessor_"<<_idOne
       <<" is smaller than Histogram in PostProcessor_"<<_idTwo
       <<std::endl;
 }
@@ -309,7 +309,7 @@ void cass::pp801::loadSettings(size_t)
   _pp.histograms_replace(_id,_result);
 
   std::cout << "PostProcessor_"<<_id
-      << " will check whether Histogram in PostProcessor_"<<_idOne
+      <<" will check whether Histogram in PostProcessor_"<<_idOne
       <<" is equal to Histogram in PostProcessor_"<<_idTwo
       <<std::endl;
 }
@@ -405,7 +405,7 @@ void cass::pp802::loadSettings(size_t)
   _pp.histograms_replace(_id,_result);
 
   std::cout << "PostProcessor_"<<_id
-      << " will divide Histogram in PostProcessor_"<<_idOne
+      <<" will divide Histogram in PostProcessor_"<<_idOne
       <<" by Histogram in PostProcessor_"<<_idTwo
       <<std::endl;
 }
@@ -428,6 +428,101 @@ void cass::pp802::operator()(const CASSEvent&)
                  two->memory().begin(),
                  _result->memory().begin(),
                  divides<float>());
+  _result->lock.unlock();
+  one->lock.unlock();
+  two->lock.unlock();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// *** postprocessors 803 multiplies two histograms ***
+
+cass::pp803::pp803(PostProcessors& pp, cass::PostProcessors::id_t id)
+  : PostprocessorBackend(pp, id), _result(0)
+{
+  loadSettings(0);
+}
+
+cass::pp803::~pp803()
+{
+  _pp.histograms_delete(_id);
+  _result = 0;
+}
+
+std::list<cass::PostProcessors::id_t> cass::pp803::dependencies()
+{
+  std::list<PostProcessors::id_t> list;
+  list.push_front(_idOne);
+  list.push_front(_idTwo);
+  return list;
+}
+
+void cass::pp803::loadSettings(size_t)
+{
+  QSettings settings;
+  settings.beginGroup("PostProcessor");
+  settings.beginGroup(QString("p") + QString::number(_id));
+
+  if (!retrieve_and_validate(_id,"HistOne",_idOne))
+    return;
+  if (!retrieve_and_validate(_id,"HistTwo",_idTwo))
+    return;
+
+  //retrieve histograms from the two pp//
+  const HistogramFloatBase *one (dynamic_cast<HistogramFloatBase*>(_pp.histograms_checkout().find(_idOne)->second));
+  _pp.histograms_release();
+  const HistogramFloatBase *two (dynamic_cast<HistogramFloatBase*>(_pp.histograms_checkout().find(_idTwo)->second));
+  _pp.histograms_release();
+  //check whether they are the same type//
+  if ((one->dimension() != two->dimension()) ||
+      (one->memory().size() != two->memory().size()))
+  {
+    throw std::runtime_error("pp802 idOne is not the same type as idTwo or they have not the same size");
+  }
+
+  //creat the resulting histogram from the first histogram
+  _pp.histograms_delete(_id);
+  _result = new HistogramFloatBase(*one);
+  _pp.histograms_replace(_id,_result);
+
+  std::cout << "PostProcessor_"<<_id
+      <<" will multiply Histogram in PostProcessor_"<<_idOne
+      <<" with Histogram in PostProcessor_"<<_idTwo
+      <<std::endl;
+}
+
+void cass::pp803::operator()(const CASSEvent&)
+{
+  using namespace std;
+  //retrieve the memory of the to be substracted histograms//
+  HistogramFloatBase *one (dynamic_cast<HistogramFloatBase*>(_pp.histograms_checkout().find(_idOne)->second));
+  _pp.histograms_release();
+  HistogramFloatBase *two (dynamic_cast<HistogramFloatBase*>(_pp.histograms_checkout().find(_idTwo)->second));
+  _pp.histograms_release();
+
+  //substract using transform with a special build function//
+  one->lock.lockForRead();
+  two->lock.lockForRead();
+  _result->lock.lockForWrite();
+  std::transform(one->memory().begin(),
+                 one->memory().end(),
+                 two->memory().begin(),
+                 _result->memory().begin(),
+                 multiplies<float>());
   _result->lock.unlock();
   one->lock.unlock();
   two->lock.unlock();
