cee71b2dbef28c69469610317911d286b89709e4
Lutz Foucar <lutz.foucar@asg.mpg.de>
2010-05-16 05:43:57 +0000

    implemented pp 801

--- cass/postprocessing/operations.cpp
+++ cass/postprocessing/operations.cpp
@@ -248,3 +248,98 @@ void cass::pp800::operator()(const CASSEvent&)
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+// *** postprocessors 801 compare two histos for equality ***
+
+cass::pp801::pp801(PostProcessors& pp, cass::PostProcessors::id_t id)
+  : PostprocessorBackend(pp, id), _result(0)
+{
+  loadSettings(0);
+}
+
+
+cass::pp801::~pp801()
+{
+  _pp.histograms_delete(_id);
+  _result = 0;
+}
+
+std::list<cass::PostProcessors::id_t> cass::pp801::dependencies()
+{
+  std::list<PostProcessors::id_t> list;
+  list.push_front(_idOne);
+  list.push_front(_idTwo);
+  return list;
+}
+
+void cass::pp801::loadSettings(size_t)
+{
+  QSettings settings;
+  settings.beginGroup("PostProcessor");
+  settings.beginGroup(QString("p") + QString::number(_id));
+
+  if (!retrieve_and_validate(_id,"HistOne",_idOne))
+    return;
+  if (!retrieve_and_validate(_id,"HistTwo",_idTwo))
+    return;
+
+  //retrieve histograms from the two pp//
+  const HistogramFloatBase *one (dynamic_cast<HistogramFloatBase*>(_pp.histograms_checkout().find(_idOne)->second));
+  _pp.histograms_release();
+  const HistogramFloatBase *two (dynamic_cast<HistogramFloatBase*>(_pp.histograms_checkout().find(_idTwo)->second));
+  _pp.histograms_release();
+  //check whether they are the same type//
+  if ((one->dimension() != two->dimension()) ||
+      (one->memory().size() != two->memory().size()))
+  {
+    throw std::runtime_error("pp106 idOne is not the same type as idTwo or they have not the same size");
+  }
+
+  //creat the resulting histogram from the first histogram
+  _pp.histograms_delete(_id);
+  _result = new Histogram0DFloat();
+  _pp.histograms_replace(_id,_result);
+
+  std::cout << "PostProcessor_"<<_id
+      << " will check whether Histogram in PostProcessor_"<<_idOne
+      <<" is equal to Histogram in PostProcessor_"<<_idTwo
+      <<std::endl;
+}
+
+void cass::pp801::operator()(const CASSEvent&)
+{
+  using namespace std;
+  //retrieve the memory of the to be substracted histograms//
+  HistogramFloatBase *one (dynamic_cast<HistogramFloatBase*>(_pp.histograms_checkout().find(_idOne)->second));
+  _pp.histograms_release();
+  HistogramFloatBase *two (dynamic_cast<HistogramFloatBase*>(_pp.histograms_checkout().find(_idTwo)->second));
+  _pp.histograms_release();
+
+  //substract using transform with a special build function//
+  one->lock.lockForRead();
+  two->lock.lockForRead();
+  float first (accumulate(one->memory().begin(),
+                          one->memory().end(),
+                          0.f));
+  float second (accumulate(one->memory().begin(),
+                           one->memory().end(),
+                           0.f));
+  one->lock.unlock();
+  two->lock.unlock();
+  _result->lock.lockForWrite();
+  *_result = (first == second);
+  _result->lock.unlock();
+}
+
+
