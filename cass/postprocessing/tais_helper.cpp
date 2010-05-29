e911c03f6137bcc2e8fa11ea66813cc34159c580
Lutz Foucar <lutz.foucar@asg.mpg.de>
2010-05-29 02:39:35 +0000

    implemented taishelper

---
+++ cass/postprocessing/tais_helper.cpp
@@ -0,0 +1,169 @@
+//Copyright (C) 2010 Lutz Foucar
+
+#include <QtCore/QSettings>
+
+#include "tais_helper.h"
+#include "acqiris_device.h"
+#include "pixel_detector.h"
+
+//initialize static members//
+QMutex cass::Tais::TaisHelper::_mutex;
+cass::Tais::TaisHelper *cass::Tais::TaisHelper::_instance(0);
+
+cass::Tais::TaisHelper* cass::Tais::TaisHelper::instance()
+{
+  //lock this//
+  QMutexLocker lock(&_mutex);
+  //check if an instance of the helper class already exists//
+  //return it, otherwise create one and return it//
+  if (0 == _instance)
+  {
+    VERBOSEOUT(std::cout << "creating an instance of the Tais Helper"
+               <<std::endl);
+    _instance = new TaisHelper();
+  }
+  return _instance;
+}
+
+void cass::Tais::TaisHelper::destroy()
+{
+  //lock this//
+  QMutexLocker lock(&_mutex);
+  delete _instance;
+  _instance=0;
+}
+
+
+cass::Tais::TaisHelper::TaisHelper()
+{
+  loadSettings();
+}
+
+void cass::Tais::TaisHelper::loadSettings()
+{
+  using namespace std;
+  QSettings settings;
+  settings.beginGroup("TaisSpecial");
+  _conditiontype = static_cast<ConditionType>(settings.value("ConditionType",0).toUInt());
+  _tofBound = make_pair(settings.value("TofLowerBound",0).toFloat(),
+                        settings.value("TofUpperBound",0).toFloat());
+  _tofCond = make_pair(settings.value("TofCondRangeLow",0).toFloat(),
+                       settings.value("TofCondRangeUp",0).toFloat());
+  _ccdBound = make_pair(make_pair(settings.value("CCDXLowerLeftXBound",0).toFloat(),
+                                  settings.value("CCDXLowerLeftYBound",0).toFloat()),
+                        make_pair(settings.value("CCDXUpperRightXBound",0).toFloat(),
+                                  settings.value("CCDXUpperRightYBound",0).toFloat()));
+  _ccdBound2 = make_pair(make_pair(settings.value("CCDXLowerLeftXBoundTwo",0).toFloat(),
+                                   settings.value("CCDXLowerLeftYBoundTwo",0).toFloat()),
+                         make_pair(settings.value("CCDXUpperRightXBoundTwo",0).toFloat(),
+                                   settings.value("CCDXUpperRightYBoundTwo",0).toFloat()));
+  _ccdCond = make_pair(settings.value("CCDCondRangeLow",0).toFloat(),
+                       settings.value("CCDCondRangeUp",0).toFloat());
+
+}
+
+bool cass::Tais::TaisHelper::process(const CASSEvent& evt)
+{
+  bool cond(false);
+
+  switch(_conditiontype)
+  {
+  case Tof:
+    {
+      using namespace cass::ACQIRIS;
+
+      const cass::ACQIRIS::Instruments _instrument (Camp1);
+      const size_t _channel(0);
+
+      const Device *dev
+          (dynamic_cast<const Device*>(evt.devices().find(CASSEvent::Acqiris)->second));
+      //retrieve a reference to the right instument//
+      Device::instruments_t::const_iterator instrIt (dev->instruments().find(_instrument));
+      //check if instrument exists//
+      if (dev->instruments().end() == instrIt)
+        throw std::runtime_error(QString("TaisHelper: Data doesn't contain Instrument %1")
+                                 .arg(_instrument).toStdString());
+      const Instrument &instr(instrIt->second);
+      //retrieve a reference to the right channel//
+      if (instr.channels().size() <= _channel)
+        throw std::runtime_error(QString("TaisHelper: Instrument %1 doesn't contain channel %2")
+                                 .arg(_instrument)
+                                 .arg(_channel).toStdString());
+      const Channel &channel (instr.channels()[_channel]);
+      const double gain(channel.gain());
+      const double offset(channel.offset());
+      //retrieve a reference to the waveform of the channel//
+      const waveform_t &waveform (channel.waveform());
+      float integral (0);
+      waveform_t::const_iterator it(waveform.begin() +static_cast<size_t>(_tofBound.first));
+      waveform_t::const_iterator end(waveform.begin() +static_cast<size_t>(_tofBound.second));
+      for (; it !=end;++it)
+        integral += *it * gain - offset;
+
+      cond = ((_tofCond.first < integral) && (integral < _tofCond.second));
+    }
+    break;
+  case VMI:
+    {
+      cass::CASSEvent::Device _device (CASSEvent::CCD);
+      size_t _detector(0);
+      if (evt.devices().find(_device)->second->detectors()->size() <= _detector)
+        throw std::runtime_error(QString("TaisHelper: Detector %1 does not exist in Device %2")
+                                 .arg(_detector)
+                                 .arg(_device).toStdString());
+      const PixelDetector::frame_t &frame
+          ((*(evt.devices().find(_device)->second)->detectors())[_detector].frame());
+
+      float integral (0);
+      const size_t colwidth ((*(evt.devices().find(_device)->second)->detectors())[_detector].columns());
+      const size_t mincol (static_cast<size_t>(_ccdBound.first.first));
+      const size_t minrow (static_cast<size_t>(_ccdBound.first.second));
+      const size_t maxcol (static_cast<size_t>(_ccdBound.second.first));
+      const size_t maxrow (static_cast<size_t>(_ccdBound.second.second));
+      for (size_t row(minrow); row <maxrow;++row)
+        for (size_t col(mincol); col <maxcol;++col)
+        integral += frame[row*colwidth + col];
+      float integral2 (0);
+      const size_t mincol2 (static_cast<size_t>(_ccdBound2.first.first));
+      const size_t minrow2 (static_cast<size_t>(_ccdBound2.first.second));
+      const size_t maxcol2 (static_cast<size_t>(_ccdBound2.second.first));
+      const size_t maxrow2 (static_cast<size_t>(_ccdBound2.second.second));
+      for (size_t row(minrow2); row <maxrow2;++row)
+        for (size_t col(mincol2); col <maxcol2;++col)
+        integral2 += frame[row*colwidth + col];
+
+      cond = ((_ccdCond.first < (integral/integral2)) && ((integral/integral2) < _ccdCond.second));
+    }
+    break;
+  case PnCCD:
+    {
+      cass::CASSEvent::Device _device (CASSEvent::pnCCD);
+      size_t _detector(0);
+      if (evt.devices().find(_device)->second->detectors()->size() <= _detector)
+        throw std::runtime_error(QString("TaisHelper: Detector %1 does not exist in Device %2")
+                                 .arg(_detector)
+                                 .arg(_device).toStdString());
+      const PixelDetector::frame_t &frame
+          ((*(evt.devices().find(_device)->second)->detectors())[_detector].frame());
+
+      float integral (0);
+      const size_t colwidth ((*(evt.devices().find(_device)->second)->detectors())[_detector].columns());
+      const size_t mincol (static_cast<size_t>(_ccdBound.first.first));
+      const size_t minrow (static_cast<size_t>(_ccdBound.first.second));
+      const size_t maxcol (static_cast<size_t>(_ccdBound.second.first));
+      const size_t maxrow (static_cast<size_t>(_ccdBound.second.second));
+      for (size_t row(minrow); row <maxrow;++row)
+        for (size_t col(mincol); col <maxcol;++col)
+        integral += frame[row*colwidth + col];
+
+      cond = ((_ccdCond.first < integral) && (integral < _ccdCond.second));
+
+    }
+    break;
+  default:
+    break;
+  }
+
+  return cond;
+}
+
---
+++ cass/postprocessing/tais_helper.h
@@ -0,0 +1,138 @@
+//Copyright (C) 2010 Lutz Foucar
+
+#ifndef _DETECTOR_HELPER_H_
+#define _DETECTOR_HELPER_H_
+
+#include <stdint.h>
+#include <utility>
+#include <algorithm>
+#include <list>
+#include <map>
+
+#include <QtCore/QMutex>
+#include <QtCore/QMutexLocker>
+
+#include "cass_event.h"
+
+
+namespace cass
+{
+  namespace Tais
+  {
+    /** predicate class for find_if.
+     * this helps finding the right key in the list of pairs
+     * @author Lutz Foucar
+     */
+    class IsKey
+    {
+    public:
+      /** initialize the key in the constructor*/
+      IsKey(const uint64_t key):_key(key){}
+      /** compares the first element of the pair to the key*/
+      bool operator()(const std::pair<uint64_t,bool>& p)const
+      { return (p.first == _key); }
+    private:
+      /** the key that we will compare to in the operator*/
+      const uint64_t _key;
+    };
+
+    /** Helper for Tais.
+     * this is just a helper that we need since the whole concept is not working
+     * yet.
+     * @author Lutz Foucar
+     */
+    class TaisHelper
+    {
+    public:
+      /** static function creating instance of this. */
+      static TaisHelper * instance();
+
+      /** destroy the whole helper*/
+      static void destroy();
+
+      /** retrieve detector for event. */
+      bool condition(const CASSEvent& evt)  {return validate(evt);}
+
+      /** tell the detector owned by this instance to reload its settings*/
+      void loadSettings();
+
+    protected:
+      /** typdef defining the list of conditions for more readable code*/
+      typedef std::list<std::pair<uint64_t, bool> > conditionList_t;
+
+      /** validation of event */
+      bool validate(const CASSEvent &evt)
+      {
+        //lock this so that only one helper will retrieve the detector at a time//
+        QMutexLocker lock(&_mutex);
+        //find the pair containing the detector//
+        conditionList_t::iterator it =
+          std::find_if(_conditionList.begin(), _conditionList.end(), IsKey(evt.id()));
+        //check wether id is not already on the list//
+        if(_conditionList.end() == it)
+        {
+          //process the event and find condition
+          bool cond (process(evt));
+          //create a new key from the id with the reloaded detector
+          conditionList_t::value_type newPair = std::make_pair(evt.id(),cond);
+          //put it to the beginning of the list//
+          _conditionList.push_front(newPair);
+          //erase the outdated element at the back//
+          _conditionList.pop_back();
+          //make the iterator pointing to the just added element of the list//
+          it = _conditionList.begin();
+        }
+        return it->second;
+      }
+
+      /** process an event and find the condition */
+      bool process(const CASSEvent& evt);
+
+    protected:
+      /** list of pairs of id-condition */
+      conditionList_t _conditionList;
+
+      /** the condition type */
+      enum ConditionType{Tof,VMI,PnCCD} _conditiontype;
+
+      /** tof boundaries for calc integral */
+      std::pair<float,float> _tofBound;
+
+      /** range for condition of integral */
+      std::pair<float,float> _tofCond;
+
+      /** ccd integral boundaries */
+      std::pair< std::pair<float,float> , std::pair<float,float> > _ccdBound;
+
+      /** second ccd integral boundaries */
+      std::pair< std::pair<float,float> , std::pair<float,float> > _ccdBound2;
+
+      /** range for condition of integral */
+      std::pair<float,float> _ccdCond;
+
+    private:
+      /** prevent people from constructin other than using instance().*/
+      TaisHelper();
+
+      /** prevent copy-construction*/
+      TaisHelper(const TaisHelper&);
+
+      /** private desctuctor.
+       * prevent destruction other than trhough destroy(),
+       * delete the detector and the detectorlist for this instance
+       */
+      ~TaisHelper(){}
+
+      /** prevent assingment */
+      TaisHelper& operator=(const TaisHelper&);
+
+      /** Singleton Mutex to lock write operations*/
+      static QMutex _mutex;
+
+      /** a static instance of this */
+      static TaisHelper* _instance;
+    };
+  }
+}
+
+#endif
