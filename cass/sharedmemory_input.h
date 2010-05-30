074fd6b815fd58be6a07441a034b2098e487e659
Lutz Foucar <lutz.foucar@asg.mpg.de>
2010-05-30 23:03:46 +0000

    corrected doc

--- cass/sharedmemory_input.h
+++ cass/sharedmemory_input.h
@@ -38,28 +38,34 @@ namespace cass
      * @param PartitionTag the name of the partition tag we want to connect to
      * @param buffer the ringbuffer, that we take events out and fill it
      *        with the incomming information
+     * @param index the client index of the shared memory
      * @param parent the parent of this object
      */
     SharedMemoryInput(char * PartitionTag,
                       int index,
                       cass::RingBuffer<cass::CASSEvent,cass::RingBufferSize>& buffer,
                       QObject *parent=0);
+
     /** deletes the thread*/
     ~SharedMemoryInput();
+
     /** starts the thread.
      * Starts the thread and the loop in the shared memory client we inerhited
      * from. The loop will be notified when there are new events available in
      * the shared memory.
      */
     void run();
+
     /** suspends the thread.
      * Suspends the thread after it has executed the event we are working on
      * right now. Will return only when the thread has really suspenden by calling
      * @see waitUntilSuspended() internally.
      */
     void suspend();
+
     /** resumes the thread, when it was suspended. Otherwise it just retruns*/
     void resume();
+
     /** overwrite the base class function with our code.
      * This is called once the eventqueue has new data available.
      * @return the errorcode, when != 0, then the baseclasses will exit its loop
@@ -78,6 +84,7 @@ namespace cass
      * Makes the thread execute the last event and then quit normally.
      */
     void end();
+
     /** load the parameters used for this thread*/
     void loadSettings(size_t what);
 
@@ -87,27 +94,37 @@ namespace cass
      * suspended.
      */
     void waitUntilSuspended();
+
   private:
     /** the Ringbuffer we take events out and fill them with the incomming data*/
     cass::RingBuffer<cass::CASSEvent,cass::RingBufferSize>  &_ringbuffer;
+
     /** the name of the partition tag we connect to*/
     char *_partitionTag;
-    /** the client index of the shared memory*/
+
+    /** the client index of the shared memory */
     int _index;
+
     /** a flag to tell the thread wther the user wants to quit, is set by @see end()*/
     bool _quit;
+
     /** a pointer to the format converter.
      * The converter will convert the incomming data to our CASSEvent
      */
     FormatConverter *_converter;
+
     /** a mutex for suspending the thread*/
     QMutex _pauseMutex;
+
     /** a condition that we will wait on until we are not suspended anymore*/
     QWaitCondition _pauseCondition;
+
     /** flag telling whether we shouodl suspend ourselves*/
     bool _pause;
+
     /** flag telling whether we are already suspended*/
     bool _paused;
+
     /** condition that will wait until the thread is rally suspended*/
     QWaitCondition _waitUntilpausedCondition;
   };
