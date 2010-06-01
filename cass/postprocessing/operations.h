a8151ead374535c1143aec4f52634dc34c5d15a2
Nicola Coppola <nicola.coppola@xfel.edu>
2010-06-01 01:47:07 +0000

    comments modified to respect code

--- cass/postprocessing/operations.h
+++ cass/postprocessing/operations.h
@@ -1188,10 +1188,10 @@ namespace cass
    *           Default is 0.
    * @cassttng PostProcessor/\%name\%/{MinTime|MaxTime} \n
    *           Minimum and Maximum Time to plot in the histogram. Default
-   *           is 0 ... 500
+   *           is 0 ... 300
    * @cassttng PostProcessor/\%name\%/{NbrSamples} \n
    *           Number of values that are used per second to calculate the average. 
-   *           Default is 10
+   *           Default is 5
    *
    * @author Nicola Coppola
    */
