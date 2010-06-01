e7b80131c42de315f2738555eaff75259cad5f45
Nicola Coppola <nicola.coppola@xfel.edu>
2010-06-01 22:53:51 +0000

    re-implemented mathematically precise radial_projection code, for radar_plot to have back the mathematically precise algorithm I need to get more informations the calling function

--- cass/histogram.cpp
+++ cass/histogram.cpp
@@ -76,54 +76,59 @@ namespace cass
 
   Histogram1DFloat Histogram2DFloat::radial_project(const std::pair<size_t,size_t> &center, size_t maxRadius)const
   {
-    /** @todo make new function with nicolas version, which is marked out*/
-//    size_t NbrBins=static_cast<size_t>(ceil(maxRadius));
-//    const float NbrBins2=pow(NbrBins,2);
-//    Histogram1DFloat hist(NbrBins, 0., maxRadius);
-//    Histogram1DFloat norms(NbrBins, 0., maxRadius);
-//
-//    const int32_t xc(static_cast<int32_t>(center.first));
-//    const int32_t yc(static_cast<int32_t>(center.second));
-//
-//    const int32_t row_min(static_cast<int32_t>(yc-maxRadius));
-//    const int32_t row_max(static_cast<int32_t>(yc+maxRadius));
-//
-//    const int32_t col_min(static_cast<int32_t>(xc-maxRadius));
-//    const int32_t col_max(static_cast<int32_t>(xc+maxRadius));
-//
-//    //here the following are safe as the radius has been safely reduced
-//    for(int32_t row=row_min; row<row_max; ++row)
-//      for(int32_t col=col_min; col<col_max; ++col)
-//      {
-//        float iradius2= square(row-yc)+square(col-xc);
-//        //only if inside the radius add the bin-values
-//        if(iradius2<=NbrBins2)
-//        {
-//          hist.bin( static_cast<int32_t>( floor(std::sqrt(iradius2)) ) ) += bin(row, col);
-//          norms.bin(static_cast<int32_t>( floor(std::sqrt(iradius2)) ) ) +=1.;
-//        }
-//      }
-//    //and now normalise the output histogram
-//    for(size_t ibin=0; ibin<NbrBins; ibin++)
-//      if(norms.bin(ibin)>0)
-//        hist.bin(ibin)=hist.bin(ibin)/norms.bin(ibin);
-//    return hist;
 
     Histogram1DFloat hist(maxRadius, 0., _axis[xAxis].hist2user(maxRadius));
-    for(size_t jr = 0;jr<maxRadius; jr++)
-    {
-      float val(0);
-      for(size_t jth = 1; jth<360; jth++)
+    Histogram1DFloat norms(maxRadius, 0., _axis[xAxis].hist2user(maxRadius));
+
+    const float NbrBins2=pow(static_cast<float>(maxRadius),2);
+
+    //    const int32_t xc(static_cast<int32_t>(center.first));
+    //    const int32_t yc(static_cast<int32_t>(center.second));
+
+    const int32_t row_min(static_cast<int32_t>(center.second-maxRadius));
+    const int32_t row_max(static_cast<int32_t>(center.second+maxRadius));
+
+    const int32_t col_min(static_cast<int32_t>(center.first-maxRadius));
+    const int32_t col_max(static_cast<int32_t>(center.first+maxRadius));
+
+    //here the following are safe as the radius has been safely reduced
+    for(int32_t row=row_min; row<row_max; ++row)
+      for(int32_t col=col_min; col<col_max; ++col)
       {
-        const float radius(jr);
-        const float angle(2.*M_PI * float(jth) / float(360));
-        size_t col(size_t(center.first  + radius*sin(angle)));
-        size_t row(size_t(center.second + radius*cos(angle)));
-        val += _memory[col + row * _axis[0].nbrBins()];
+        float iradius2= square(row-center.second)+square(col-center.first);
+        //only if inside the radius add the bin-values
+        if(iradius2<=NbrBins2)
+        {
+          hist.bin( static_cast<int32_t>( floor(std::sqrt(iradius2)) ) ) += bin(row, col);
+          norms.bin(static_cast<int32_t>( floor(std::sqrt(iradius2)) ) ) +=1.;
+        }
       }
-      hist.memory()[jr]+=val;
-    }
+    //and now normalise the output histogram
+    for(size_t ibin=0; ibin<maxRadius; ibin++)
+      {
+        if(norms.bin(ibin)>0)
+          hist.bin(ibin)=hist.bin(ibin)/norms.bin(ibin);
+        else hist.bin(ibin)=0.;
+      }
     return hist;
+
+    /** @todo make new function with somebody-else version, which is marked out*/
+    /** the following works only if the 2*pi*maxRadius<360 otherwise is ABSOLUTELY mathematically INCORRECT */
+    //    Histogram1DFloat hist(maxRadius, 0., _axis[xAxis].hist2user(maxRadius));
+    //    for(size_t jr = 0;jr<maxRadius; jr++)
+    //    {
+    //      float val(0);
+    //      for(size_t jth = 1; jth<360; jth++)
+    //      {
+    //        const float radius(jr);
+    //        const float angle(2.*M_PI * float(jth) / float(360));
+    //        size_t col(size_t(center.first  + radius*sin(angle)));
+    //        size_t row(size_t(center.second + radius*cos(angle)));
+    //        val += _memory[col + row * _axis[0].nbrBins()];
+    //      }
+    //      hist.memory()[jr]+=val;
+    //    }
+    //    return hist;
   }
 
 
@@ -131,43 +136,36 @@ namespace cass
                                                 const std::pair<size_t,size_t> &range,
                                                 size_t nbrBins) const
   {
-    /** @todo make new function with nicolas version, which is marked out*/
-//    size_t NbrBins=720;
-//    const float NbrBins2_min=pow(range.first,2);
-//    const float NbrBins2_max=pow(range.second,2);
-//    //size_t NbrBins=100;
-//    //Histogram1DFloat hist(NbrBins, -180.f, 180.f);
-//    Histogram1DFloat hist(NbrBins, -M_PI, M_PI);
-//    //Histogram1DFloat hist(NbrBins, -1.005f, 1.005f);
-//    //Histogram1DFloat norms(NbrBins, 0., radius);
-//
-//    const float xc(centre.first-0.5);
-//    const float yc(centre.second-0.5);
-//
-//    //here the following are NOT safe as the radius has NOT been reduced
-//
-//    int32_t row_min(static_cast<int32_t>(yc-range.second));
+    Histogram1DFloat hist(nbrBins, 0., 360.);
+    const float NbrBins2_min=pow(static_cast<float>(range.first),2);
+    const float NbrBins2_max=pow(static_cast<float>(range.second),2);
+    const float xc(static_cast<float>(center.first)-0.5);
+    const float yc(static_cast<float>(center.second)-0.5);
+
+    //here the following are NOT safe as the radius has NOT been reduced
+
+    //HHHHAAAAA I do not know anymore the yAxis...
+    // I have therefore lost the capability of drawing a part of the circle
+
+    int32_t row_min(static_cast<int32_t>(yc-NbrBins2_max));
 //    row_min=std::max(row_min,static_cast<int32_t>(_axis[yAxis].lowerLimit()));
-//    int32_t row_max(static_cast<int32_t>(yc+range.second));
+    row_min=std::max(row_min,0);
+    int32_t row_max(static_cast<int32_t>(yc+NbrBins2_max));
 //    row_max=std::min(row_max,static_cast<int32_t>(_axis[yAxis].upperLimit()));
-//
-//    int32_t col_min(static_cast<int32_t>(xc-range.second));
+
+    int32_t col_min(static_cast<int32_t>(xc-NbrBins2_max));
 //    col_min=std::max(col_min,static_cast<int32_t>(_axis[xAxis].lowerLimit()));
-//    int32_t col_max(static_cast<int32_t>(xc+range.second));
+    col_min=std::max(col_min,0);
+    int32_t col_max(static_cast<int32_t>(xc+NbrBins2_max));
 //    col_max=std::min(col_max,static_cast<int32_t>(_axis[xAxis].upperLimit()));
-//
-//    /*std::cout<<"0 4 "
-//             <<row_min << " "<<row_max<<" "
-//             <<col_min << " "<<col_max<<std::endl;*/
-//    //std::cout<<"0 5 " <<std::endl;
-//
-//    //the following loop is quite generous...
-//    for(int32_t row=row_min; row<row_max; ++row)
-//      for(int32_t col=col_min; col<col_max; ++col)
-//      {
-//      float iradius2= square(static_cast<float>(row)-yc)+square(static_cast<float>(col)-xc);
-//      //only if inside the corona of range.first,range.second add the bin-values
-//      //boundary included!!
+
+    //the following loop is quite generous...
+    for(int32_t row=row_min; row<row_max; ++row)
+      for(int32_t col=col_min; col<col_max; ++col)
+      {
+        float iradius2= square(static_cast<float>(row)-yc)+square(static_cast<float>(col)-xc);
+        //only if inside the corona of range.first,range.second add the bin-values
+        //boundary included!!
 //      if(iradius2<NbrBins2_max && iradius2>NbrBins2_min)
 //      {
 //
@@ -187,8 +185,11 @@ namespace cass
 //        //hist.fill( angle );
 //        hist.fill( angle,bin(row,col) );
 //      }
+      }
 
-    Histogram1DFloat hist(nbrBins, 0., 360.);
+    /** the following works only if the 2*pi*maxRadius<360 otherwise is ABSOLUTELY mathematically INCORRECT */
+    /** and it is clearly not as general as the previously implemented code */
+    //    Histogram1DFloat hist(nbrBins, 0., 360.);
     for(size_t jr = range.first;jr<range.second; jr++)
     {
       for(size_t jth = 1; jth<360; jth++)
