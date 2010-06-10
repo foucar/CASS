618ec892088c7d5a268008798af8fee742d02b42
Lutz Foucar <lutz.foucar@asg.mpg.de>
2010-06-10 21:33:59 +0000

    first implementation of the histogram backend changes that will make sure that the results of an evalutation are correct for that event

--- cass/postprocessing/backend.h
+++ cass/postprocessing/backend.h
@@ -6,6 +6,8 @@
 
 #include <list>
 #include <string>
+#include <stdint.h>
+#include <utility>
 
 #include "cass.h"
 #include "postprocessor.h"
@@ -15,7 +17,25 @@ namespace cass
   //forward declaration
   class CASSEvent;
 
-  /** @brief base class for postprocessors */
+  /** predicate class for find_if.
+   * this helps finding the right key in the list of pairs eventid - Histogram
+   * @author Lutz Foucar
+   */
+  class IsKey
+  {
+  public:
+    /** initialize the key in the constructor*/
+    IsKey(const uint64_t key):_key(key){}
+    /** compares the first element of the pair to the key*/
+    bool operator()(const std::pair<uint64_t,HistogramBackend*>& p)const
+    { return (_key == p.first); }
+  private:
+    /** the key that we will compare to in the operator*/
+    const uint64_t _key;
+  };
+
+
+  /** base class for postprocessors */
   class CASSSHARED_EXPORT PostprocessorBackend
   {
   public:
@@ -25,16 +45,68 @@ namespace cass
      * @param key the key in the container of this postprocessor
      */
     PostprocessorBackend(PostProcessors& pp, const PostProcessors::key_t &key)
-      : _key(key), _pp(pp)
+      :_key(key), _pp(pp)
     {}
 
     /** virtual destructor */
-    virtual ~PostprocessorBackend() { }
+    virtual ~PostprocessorBackend() {}
+
+    /** typedef describing how the list of histograms works */
+    typedef std::list<std::pair<uint64_t, HistogramBackend*> > histogramList_t;
+
+    /** process the event
+     *
+     * this will evaluate the event and fill the resulting histogram. This needs
+     * to be implemented in the postprocessors.
+     * @param event the cassevent to work on
+     */
+    virtual void process(const CASSEvent& event) = 0;
 
     /** the operator called for each event */
-    virtual void operator()(const CASSEvent&) = 0;
+    virtual const HistogramBackend& operator()(const CASSEvent& evt)
+    {
+      using namespace std;
+      QWriteLocker lock(&_histLock);
+      histogramList_t::iterator it
+        (find_if(_histList.begin(), _histList.end(), IsKey(evt.id())));
+      if(_histList.end() == it)
+      {
+        _result = _histList.back().second;
+        process(evt);
+        histogramList_t::value_type newPair (std::make_pair(evt.id(),_result));
+        _histList.push_front(newPair);
+        _histList.pop_back();
+        it = _histList.begin();
+      }
+      return *(it->second);
+    }
 
-    /** Provide default implementation of loadSettings that does nothings */
+    /** retrieve a histogram for a given id.
+     *
+     * When parameter is 0 then it just returns the last known histogram.
+     * @param eventid the event id of the histogram that is requested. Default is 0
+     */
+    virtual const HistogramBackend& getHist()(const uint64_t eventid)
+    {
+      using namespace std;
+      QReadLocker lock(&_histLock);
+      //if eventId is 0 then just return the latest event//
+      if (0 == eventId)
+      {
+        return *_histList.back().second;
+      }
+      else
+      {
+        histogramList_t::const_iterator it
+            (find_if(histList.begin(),histList.end(),IsKey(eventid)));
+        if (_histList.end() == it)
+          throw invalid_argument("PostProcessorBackend::getHist() : EventId is not present");
+        else
+          return *(it.second);
+      }
+    }
+
+    /** Provide default implementation of loadSettings that does nothing */
     virtual void loadSettings(size_t)
     {
       VERBOSEOUT(std::cout << "calling backend' load settings"<<std::endl);
@@ -44,41 +116,27 @@ namespace cass
      *
      * The dependencies must be run before the actual postprocessor is run by itself.
      */
-    virtual PostProcessors::active_t dependencies() { return PostProcessors::active_t(); }
-
-  protected:
-    /** @return histogram of the actual postprocessor we call this for */
-    virtual HistogramBackend *histogram_checkout() { return histogram_checkout(_key); }
-
-    /** @overload
-     *
-     * @return histogram of the requested postprocessor
-     */
-    virtual HistogramBackend *histogram_checkout(std::string name)
+    virtual PostProcessors::active_t dependencies()
     {
-      try
-      {
-        PostProcessors::histograms_t hist(_pp.histograms_checkout());
-        _pp.validate(name);
-        _pp.histograms_release();
-        return hist[name];
-      }
-      catch (InvalidHistogramError)
-      {
-        std::cout << "Invalid histogram: "<<name<<std::endl;
-        _pp.histograms_release();
-        return 0;
-      }
+      return PostProcessors::active_t();
     }
 
-    /** release the histogram container readwritelock */
-    void histogram_release() { _pp.histograms_release(); }
-
+  protected:
     /** the postprocessors key */
     PostProcessors::key_t _key;
 
+    /** the list of histograms - event ids */
+    histogramList_t _histList;
+
+    /** pointer to the current histogram */
+    HistogramBackend *_result;
+
     /** reference to the PostProcessors container */
-    PostProcessors& _pp;
+    PostProcessors &_pp;
+
+    /** histogram list lock */
+    QReadWriteLock _histlock;
+
   };
 
 } //end namespace cass
