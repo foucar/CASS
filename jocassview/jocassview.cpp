d111d82798c970a72dd910053433bed449675ca4
Nicola Coppola <nicola.coppola@xfel.edu>
2010-06-17 22:29:52 +0000

    a bit more of algebra with desktop

--- jocassview/jocassview.cpp
+++ jocassview/jocassview.cpp
@@ -24,7 +24,16 @@ int main(int argc, char *argv[])
     const int displ_height= displ.height();
     const int displ_width= displ.width();
     const int min_size = std::min(displ_height,displ_width);
-    window.resize(min_size,min_size );
+    const int nscreens= my_desktop->screenCount();
+    const QRect screen(my_desktop->screenGeometry(-1));
+    const int screen_height= screen.height();
+    const int screen_width= screen.width();
+    std::cout<<"Display dimensions: "<< displ_height << " x " 
+             << displ_width << " " << nscreens
+             << " "
+             << screen_height << " x " 
+             << screen_width << " " <<  my_desktop->isVirtualDesktop() <<std::endl;
+    window.resize(min_size/2,min_size/2);
 
     window.show();
     return app.exec();
