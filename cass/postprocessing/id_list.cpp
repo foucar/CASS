0ccaa4226d7635d313941d81b987bf305e383ecb
Lutz Foucar <lutz.foucar@asg.mpg.de>
2010-06-18 16:25:15 +0000

    updated output

--- cass/postprocessing/id_list.cpp
+++ cass/postprocessing/id_list.cpp
@@ -6,7 +6,7 @@
 cass::IdList::IdList(const PostProcessors::keyList_t& list)
   : Serializable(1), _list(list), _size(list.size())
 {
-  VERBOSEOUT(std::cerr << "Initial list size = " << _size << std::endl);
+  VERBOSEOUT(std::cerr << "IdList::IdList(): Initial list size = " << _size << std::endl);
 }
 
 bool cass::IdList::deserialize(SerializerBackend *in)
@@ -17,19 +17,19 @@ bool cass::IdList::deserialize(SerializerBackend *in)
   uint16_t ver = in->retrieveUint16();
   if(ver != _version)
   {
-    std::cerr<<"version conflict in IdList: "<<ver<<" "<<_version<<std::endl;
+    std::cerr<<"IdList::deserialize(): version conflict in IdList: "<<ver<<" "<<_version<<std::endl;
     return false;
   }
   _size = in->retrieveSizet();
-  VERBOSEOUT(std::cerr << "list size " << _size << std::endl);
+  VERBOSEOUT(std::cerr << "IdList::deserialize(): list size " << _size << std::endl);
   if (!in->endChecksumGroupForRead())
   {
-    VERBOSEOUT(std::cerr<<"wrong checksum IdList"<<std::endl);
+    VERBOSEOUT(std::cerr<<"IdList::deserialize(): wrong checksum IdList"<<std::endl);
     return false;
   }
   for(size_t ii=0; ii<_size; ++ii)
     _list.push_back(in->retrieveString());
-  VERBOSEOUT(std::cerr << "list is done " << std::endl);
+  VERBOSEOUT(std::cerr << "IdList::deserialize(): list is done " << std::endl);
   return true;
 }
 
