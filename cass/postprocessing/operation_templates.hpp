e53c22a5115c1cefc8b963695c19e8cca059908b
Lutz Foucar <lutz.foucar@asg.mpg.de>
2010-06-19 16:46:57 +0000

    adapted operation templates to new way

--- cass/postprocessing/operation_templates.hpp
+++ cass/postprocessing/operation_templates.hpp
@@ -21,7 +21,7 @@ namespace cass
    *
    * this templated class will compare the sum of all bins to a constant value
    *
-   * @cassttng PostProcessor/\%name\%/{HistOne} \n
+   * @cassttng PostProcessor/\%name\%/{HistName} \n
    *           the postprocessor name that contain the first histogram. Needs to
    *           be implemented, because default is "", which is invalid.
    * @cassttng PostProcessor/\%name\%/{Value} \n
@@ -30,6 +30,9 @@ namespace cass
    *           0D Postprocessor name that we check before filling image.
    *           if this setting is not defined, this postprocessor is unconditional.
    *           Therefore its always true.
+   * @cassttng PostProcessor/\%name\%/{Hide} \n
+   *           Flag that will hide this postprocessor in cassview's combobox.
+   *           Default is false
    *
    * @tparam ComparisonOperator comaprison-operator that will work on the data
    * @author Lutz Foucar
@@ -52,9 +55,7 @@ namespace cass
       settings.beginGroup("PostProcessor");
       settings.beginGroup(_key.c_str());
       _value = settings.value("Value",0).toFloat();
-      PostProcessors::key_t keyOne;
-      _one = retrieve_and_validate(_pp,_key,"HistOne", keyOne);
-      _dependencies.push_back(keyOne);
+      _one = setupDependency("HistName");
       bool ret (setupCondition());
       if (!_one || !ret) return;
       _result = new Histogram0DFloat();
@@ -71,19 +72,16 @@ namespace cass
     virtual void process(const CASSEvent& evt)
     {
       using namespace std;
-      if ((*_condition)(evt).isTrue())
-      {
-        const HistogramFloatBase &one
-            (dynamic_cast<const HistogramFloatBase&>((*_one)(evt)));
-        one.lock.lockForRead();
-        float first (accumulate(one.memory().begin(),
-                                one.memory().end(),
-                                0.f));
-        one.lock.unlock();
-        _result->lock.lockForWrite();
-        *dynamic_cast<Histogram0DFloat*>(_result) = op(first, _value);
-        _result->lock.unlock();
-      }
+      const HistogramFloatBase &one
+          (dynamic_cast<const HistogramFloatBase&>((*_one)(evt)));
+      one.lock.lockForRead();
+      float first (accumulate(one.memory().begin(),
+                              one.memory().end(),
+                              0.f));
+      one.lock.unlock();
+      _result->lock.lockForWrite();
+      *dynamic_cast<Histogram0DFloat*>(_result) = op(first, _value);
+      _result->lock.unlock();
     }
 
   protected:
@@ -117,6 +115,9 @@ namespace cass
    *           0D Postprocessor name that we check before filling image.
    *           if this setting is not defined, this postprocessor is unconditional.
    *           Therefore its always true.
+   * @cassttng PostProcessor/\%name\%/{Hide} \n
+   *           Flag that will hide this postprocessor in cassview's combobox.
+   *           Default is false
    *
    * @tparam BooleanOperator boolean operator to operate on 0D Hists
    * @author Lutz Foucar
@@ -138,12 +139,8 @@ namespace cass
       QSettings settings;
       settings.beginGroup("PostProcessor");
       settings.beginGroup(_key.c_str());
-      PostProcessors::key_t keyOne;
-      _one = retrieve_and_validate(_pp,_key,"HistOne", keyOne);
-      _dependencies.push_back(keyOne);
-      PostProcessors::key_t keyTwo;
-      _two = retrieve_and_validate(_pp,_key,"HistTwo", keyTwo);
-      _dependencies.push_back(keyTwo);
+      _one = setupDependency("HistOne");
+      _two = setupDependency("HistTwo");
       bool ret (setupCondition());
       if ( !(_one && _two && ret) ) return;
       if (_one->getHist(0).dimension() != 0 ||
@@ -162,20 +159,17 @@ namespace cass
     /** process event */
     virtual void process(const CASSEvent& evt)
     {
-      if ((*_condition)(evt).isTrue())
-      {
-        const HistogramFloatBase &one
-            (dynamic_cast<const HistogramFloatBase&>((*_one)(evt)));
-        const HistogramFloatBase &two
-            (dynamic_cast<const HistogramFloatBase&>((*_two)(evt)));
-        one.lock.lockForRead();
-        two.lock.lockForRead();
-        _result->lock.lockForWrite();
-        *dynamic_cast<Histogram0DFloat*>(_result) = op(one.isTrue(), two.isTrue());
-        _result->lock.unlock();
-        two.lock.unlock();
-        one.lock.unlock();
-      }
+      const HistogramFloatBase &one
+          (dynamic_cast<const HistogramFloatBase&>((*_one)(evt)));
+      const HistogramFloatBase &two
+          (dynamic_cast<const HistogramFloatBase&>((*_two)(evt)));
+      one.lock.lockForRead();
+      two.lock.lockForRead();
+      _result->lock.lockForWrite();
+      *dynamic_cast<Histogram0DFloat*>(_result) = op(one.isTrue(), two.isTrue());
+      _result->lock.unlock();
+      two.lock.unlock();
+      one.lock.unlock();
     }
 
   protected:
@@ -209,6 +203,9 @@ namespace cass
    *           0D Postprocessor name that we check before filling image.
    *           if this setting is not defined, this postprocessor is unconditional.
    *           Therefore its always true.
+   * @cassttng PostProcessor/\%name\%/{Hide} \n
+   *           Flag that will hide this postprocessor in cassview's combobox.
+   *           Default is false
    *
    * @tparam ComparisonOperator comaprison-operator that will work on the data
    * @author Lutz Foucar
@@ -230,12 +227,8 @@ namespace cass
       QSettings settings;
       settings.beginGroup("PostProcessor");
       settings.beginGroup(_key.c_str());
-      PostProcessors::key_t keyOne;
-      _one = retrieve_and_validate(_pp,_key,"HistOne", keyOne);
-      _dependencies.push_back(keyOne);
-      PostProcessors::key_t keyTwo;
-      _two = retrieve_and_validate(_pp,_key,"HistTwo", keyTwo);
-      _dependencies.push_back(keyTwo);
+      _one = setupDependency("HistOne");
+      _two = setupDependency("HistTwo");
       bool ret (setupCondition());
       if ( !(_one && _two && ret) ) return;
       if (_one->getHist(0).dimension() != _two->getHist(0).dimension())
@@ -254,26 +247,23 @@ namespace cass
     /** process event */
     virtual void process(const CASSEvent& evt)
     {
-      if ((*_condition)(evt).isTrue())
-      {
-        const HistogramFloatBase &one
-            (dynamic_cast<const HistogramFloatBase&>((*_one)(evt)));
-        const HistogramFloatBase &two
-            (dynamic_cast<const HistogramFloatBase&>((*_two)(evt)));
-        one.lock.lockForRead();
-        two.lock.lockForRead();
-        float first (accumulate(one.memory().begin(),
-                                one.memory().end(),
-                                0.f));
-        float second (accumulate(two.memory().begin(),
-                                 two.memory().end(),
-                                 0.f));
-        two.lock.unlock();
-        one.lock.unlock();
-        _result->lock.lockForWrite();
-        *dynamic_cast<Histogram0DFloat*>(_result) = op(first,second);
-        _result->lock.unlock();
-      }
+      const HistogramFloatBase &one
+          (dynamic_cast<const HistogramFloatBase&>((*_one)(evt)));
+      const HistogramFloatBase &two
+          (dynamic_cast<const HistogramFloatBase&>((*_two)(evt)));
+      one.lock.lockForRead();
+      two.lock.lockForRead();
+      float first (accumulate(one.memory().begin(),
+                              one.memory().end(),
+                              0.f));
+      float second (accumulate(two.memory().begin(),
+                               two.memory().end(),
+                               0.f));
+      two.lock.unlock();
+      one.lock.unlock();
+      _result->lock.lockForWrite();
+      *dynamic_cast<Histogram0DFloat*>(_result) = op(first,second);
+      _result->lock.unlock();
     }
 
   protected:
@@ -309,6 +299,9 @@ namespace cass
    *           0D Postprocessor name that we check before filling image.
    *           if this setting is not defined, this postprocessor is unconditional.
    *           Therefore its always true.
+   * @cassttng PostProcessor/\%name\%/{Hide} \n
+   *           Flag that will hide this postprocessor in cassview's combobox.
+   *           Default is false
    *
    * @tparam Operator operator that will work on the data
    * @author Lutz Foucar
@@ -330,12 +323,8 @@ namespace cass
       QSettings settings;
       settings.beginGroup("PostProcessor");
       settings.beginGroup(_key.c_str());
-      PostProcessors::key_t keyOne;
-      _one = retrieve_and_validate(_pp, _key, "HistOne", keyOne);
-      _dependencies.push_back(keyOne);
-      PostProcessors::key_t keyTwo;
-      _two = retrieve_and_validate(_pp, _key, "HistTwo", keyTwo);
-      _dependencies.push_back(keyTwo);
+      _one = setupDependency("HistOne");
+      _two = setupDependency("HistTwo");
       bool ret (setupCondition());
       if ( !(_one && _two && ret) ) return;
       const HistogramBackend &one(_one->getHist(0));
@@ -358,23 +347,20 @@ namespace cass
     /** process event */
     virtual void process(const CASSEvent& evt)
     {
-      if ((*_condition)(evt).isTrue())
-      {
-        const HistogramFloatBase &one
-            (dynamic_cast<const HistogramFloatBase&>((*_one)(evt)));
-        const HistogramFloatBase &two
-            (dynamic_cast<const HistogramFloatBase&>((*_two)(evt)));
-        one.lock.lockForRead();
-        two.lock.lockForRead();
-        _result->lock.lockForWrite();
-        transform(one.memory().begin(), one.memory().end(),
-                  two.memory().begin(),
-                  (dynamic_cast<HistogramFloatBase *>(_result))->memory().begin(),
-                  op);
-        _result->lock.unlock();
-        one.lock.unlock();
-        two.lock.unlock();
-      }
+      const HistogramFloatBase &one
+          (dynamic_cast<const HistogramFloatBase&>((*_one)(evt)));
+      const HistogramFloatBase &two
+          (dynamic_cast<const HistogramFloatBase&>((*_two)(evt)));
+      one.lock.lockForRead();
+      two.lock.lockForRead();
+      _result->lock.lockForWrite();
+      transform(one.memory().begin(), one.memory().end(),
+                two.memory().begin(),
+                (dynamic_cast<HistogramFloatBase *>(_result))->memory().begin(),
+                op);
+      _result->lock.unlock();
+      one.lock.unlock();
+      two.lock.unlock();
     }
 
   protected:
@@ -404,6 +390,9 @@ namespace cass
    *           0D Postprocessor name that we check before filling image.
    *           if this setting is not defined, this postprocessor is unconditional.
    *           Therefore its always true.
+   * @cassttng PostProcessor/\%name\%/{Hide} \n
+   *           Flag that will hide this postprocessor in cassview's combobox.
+   *           Default is false
    *
    * @tparam Operator operator that will work on the data
    * @author Lutz Foucar
@@ -426,9 +415,7 @@ namespace cass
       settings.beginGroup("PostProcessor");
       settings.beginGroup(_key.c_str());
       _value = settings.value("Factor",1).toFloat();
-      PostProcessors::key_t keyOne;
-      _one = retrieve_and_validate(_pp, _key, "HistOne", keyOne);
-      _dependencies.push_back(keyOne);
+      _one = setupDependency("HistName");
       bool ret (setupCondition());
       if (!(_one && ret)) return;
       const HistogramBackend &one(_one->getHist(0));
@@ -445,20 +432,17 @@ namespace cass
     /** process event */
     virtual void process(const CASSEvent& evt)
     {
-      using namespace std;
-      if ((*_condition)(evt).isTrue())
-      {
-        const HistogramFloatBase &one
-            (dynamic_cast<const HistogramFloatBase&>((*_one)(evt)));
-        one.lock.lockForRead();
-        _result->lock.lockForWrite();
-        transform(one.memory().begin(), one.memory().end(),
-                  dynamic_cast<HistogramFloatBase *>(_result)->memory().begin(),
-                  bind2nd(op,_value));
-        _result->lock.unlock();
-        one.lock.unlock();
-      }
-    }
+    using namespace std;
+    const HistogramFloatBase &one
+        (dynamic_cast<const HistogramFloatBase&>((*_one)(evt)));
+    one.lock.lockForRead();
+    _result->lock.lockForWrite();
+    transform(one.memory().begin(), one.memory().end(),
+              dynamic_cast<HistogramFloatBase *>(_result)->memory().begin(),
+              bind2nd(op,_value));
+    _result->lock.unlock();
+    one.lock.unlock();
+  }
 
   protected:
     /** pp containing input histogram */
