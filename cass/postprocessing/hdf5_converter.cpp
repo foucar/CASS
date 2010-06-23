6e0d25525dd6b24a21390d02c801335cd9e1d076
Lutz Foucar <lutz.foucar@asg.mpg.de>
2010-06-23 23:53:06 +0000

    changed indentation

--- cass/postprocessing/hdf5_converter.cpp
+++ cass/postprocessing/hdf5_converter.cpp
@@ -9,26 +9,27 @@
 
 namespace cass
 {
-hid_t createGroupNameFromEventId(uint64_t eventid, hid_t filehandler)
-{
+  hid_t createGroupNameFromEventId(uint64_t eventid, hid_t filehandler)
+  {
 
+  }
+
+  void write0DHist(const Histogram0DFloat& hist, hid_t groupid)
+  {
+
+  }
+
+  void write1DHist(const Histogram1DFloat& hist, hid_t groupid)
+  {
+
+  }
+
+  void write2DHist(const Histogram2DFloat& hist, hid_t groupid)
+  {
+
+  }
 }
 
-void write0DHist(const Histogram0DFloat& hist, hid_t groupid)
-{
-
-}
-
-void write1DHist(const Histogram1DFloat& hist, hid_t groupid)
-{
-
-}
-
-void write2DHist(const Histogram2DFloat& hist, hid_t groupid)
-{
-
-}
-}
 cass::pp1001::pp1001(cass::PostProcessors &pp, const cass::PostProcessors::key_t &key, const std::string& outfilename)
   :cass::PostprocessorBackend(pp,key),_outfilename(outfilename)
 {
@@ -56,11 +57,12 @@ void cass::pp1001::process(const cass::CASSEvent &evt)
   PostProcessors::postprocessors_t::iterator it (ppc.begin());
   for (;it != ppc.end(); ++it)
   {
-    if (it->second->write())
+    PostprocessorBackend &pp (*(it->second));
+    if (pp.write())
     {
-      hid_t ppgrouphandle (H5Gcreate1(eventgrouphandle, it->second->key().c_str(),0));
+      hid_t ppgrouphandle (H5Gcreate1(eventgrouphandle, pp.key().c_str(),0));
       /** @todo write metadata to group */
-      const HistogramBackend &hist ((*(it->second))(evt));
+      const HistogramBackend &hist (pp(evt));
       switch (hist.dimension())
       {
       case 0:
