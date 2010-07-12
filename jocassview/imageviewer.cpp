ef23263c7ec92f1fe9a2bcd895aa4105e0adc991
Stephan Kassemeyer <stephan.kassemeyer@mpimf-heidelberg.mpg.de>
2010-07-12 15:10:33 +0000

    jocassview: bugfix. when attachid.setEditable was set to false some commits ago,   plotwidget0D was no longer announced when a different postprocessor was selected, so   it drew its accumulative 1d curve in the same plot.

--- jocassview/imageviewer.cpp
+++ jocassview/imageviewer.cpp
@@ -172,6 +172,7 @@ ImageViewer::ImageViewer(QWidget *parent, Qt::WFlags flags)
   _plotWidget1D = new plotWidget1D;
   _plotWidget0D = new plotWidget0D(1000);
   connect(_attachId, SIGNAL(editTextChanged(const QString &)), _plotWidget0D, SLOT(setHistogramKey(const QString&)));
+  connect(_attachId, SIGNAL(currentIndexChanged(const QString &)), _plotWidget0D, SLOT(setHistogramKey(const QString&)));
   // dock widget containing image or histograms:
   _dock = new QDockWidget(tr("Histogram"), this);
   _spectrogramWidget = new spectrogramWidget;
