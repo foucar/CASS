fa911db19c87f947ad12b75d3c9a74944615db2d
Lutz Foucar <lutz.foucar@asg.mpg.de>
2010-07-15 09:18:08 +0000

    fixed typo

--- cass/serializable.h
+++ cass/serializable.h
@@ -1,7 +1,7 @@
 //Copyright (C) 2010 Lutz Foucar
 
 /**
- * @file seriablizable.h file contains base class all serializable classes
+ * @file serializable.h file contains base class all serializable classes
  *
  * @author Lutz Foucar
  */
