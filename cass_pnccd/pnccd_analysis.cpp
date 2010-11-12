6abcc0205a3a98c2cc6d9bbf69b80d76f4fd070c
Nicola Coppola <nicola.coppola@xfel.edu>
2010-11-12 16:59:14 +0000

    added 1 debug printout

--- cass_pnccd/pnccd_analysis.cpp
+++ cass_pnccd/pnccd_analysis.cpp
@@ -427,6 +427,12 @@ void cass::pnCCD::Analysis::loadSettings()
               dp._gain_ao_CTE[i+jrow*pnCCD::default_size]*=gain_ith;
           }
         }
+#ifdef debug_conf
+        for(size_t i=0;i<pnCCD::default_size_sq;i++) {
+          std::cout << printoutdef << " gain/cte map "
+                    << i << " " << dp._CTE << " " << gain_ith << " " << dp._gain_ao_CTE[i] << std::endl;
+#endif
+
         std::cout<< printoutdef << "GAIN maps loaded for det# "<<iDet <<std::endl;
       }
       else
