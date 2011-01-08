2c0532b46f349bf56a751ac4e7b54497f0a48af9
Lutz Foucar <lutz.foucar@asg.mpg.de>
2011-01-08 19:46:44 +0000

    modified lucassview such that it compiles

--- lucassview/main.cpp
+++ lucassview/main.cpp
@@ -1,17 +1,17 @@
 #include <TRint.h>
 #include <TROOT.h>
-#include "../MyClient/MyClient.h"
-#include "../MySettings/MySettings.h"
+//#include "../MyClient/MyClient.h"
+//#include "../MySettings/MySettings.h"
 
 
-void initializeSettings(MySettings&);
+//void initializeSettings(MySettings&);
 int main(int argc, char *argv[])
 {
 	TRint theApp("App", &argc, argv);
 	
-	MySettings set(false);
-	initializeSettings(set);
-	MyClient c(set);
+  //MySettings set(false);
+  //initializeSettings(set);
+  //MyClient c(set);
 
 	//gROOT->ProcessLine(".L OnlineMacros.c");
 
