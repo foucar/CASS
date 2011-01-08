4b2a05f2eff7bd080aba2eca7563b69f66edb9b6
Lutz Foucar <lutz.foucar@asg.mpg.de>
2011-01-08 19:20:49 +0000

    implement the creation of the det analyzer in the static member

--- cass_acqiris/classes/detector_analyzer/detector_analyzer_backend.cpp
+++ cass_acqiris/classes/detector_analyzer/detector_analyzer_backend.cpp
@@ -8,12 +8,21 @@
  */
 
 #include "detector_analyzer_backend.h"
+#include "delayline_detector_analyzer_simple.h"
 
 using namespace cass::ACQIRIS;
 
-DetectorAnalyzerBackend* DetectorAnalyzerBackend::instance(const DetectorAnalyzerType)
+DetectorAnalyzerBackend* DetectorAnalyzerBackend::instance(const DetectorAnalyzerType& type)
 {
   DetectorAnalyzerBackend* detanal(0);
-
+  switch(type)
+  {
+  case DelaylineSimple:
+    detanal = new DelaylineDetectorAnalyzerSimple();
+    break;
+  default:
+    throw std::invalid_argument("DetectorAnalyzerBackend::instance(): the requested type is unknown");
+    break;
+  }
   return detanal;
 }
