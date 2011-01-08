ed5fef8692b45c5254a20af904ff47bb663983ff
Lutz Foucar <lutz.foucar@asg.mpg.de>
2011-01-08 19:50:56 +0000

    implemented the operators

--- lucassview/tcpclient.cpp
+++ lucassview/tcpclient.cpp
@@ -5,6 +5,8 @@
  *
  * @author Lutz Foucar
  */
+#include <stdexcept>
+#include <sstream>
 
 #include "tcpclient.h"
 #include "soapCASSsoapProxy.h"
@@ -18,25 +20,83 @@ TCPClient::TCPClient(const std::string &server)
 
 std::list<std::string> TCPClient::operator() ()const
 {
-
+  using namespace std;
+  bool ret(false);
+  CASSsoapProxy client;
+  client.soap_endpoint = _server.c_str();
+  int retcode (client.getPostprocessorIds(&ret));
+  if( (retcode != SOAP_OK) || !ret)
+  {
+    stringstream ss;
+    ss << "Could not retrieve the List of Histograms on '"<<_server<<"'";
+    throw runtime_error(ss.str());
+  }
+  soap_multipart::iterator attachment (client.dime.begin());
+  if(client.dime.end() == attachment)
+  {
+    stringstream ss;
+    ss << "There is no attachmend in the received soap data";
+    throw runtime_error(ss.str());
+  }
+  cout << "TCPClient: DIME attachment:" << endl
+      << " TCPClient: Memory=" << (void*)(*attachment).ptr << endl
+      << " TCPClient: Size=" << (*attachment).size << endl
+      << " TCPClient: Type=" << ((*attachment).type?(*attachment).type:"null") << endl
+      << " TCPClient: ID=" << ((*attachment).id?(*attachment).id:"null") << endl;
+//  cass::Serializer serializer( std::string((char *)(*attachment).ptr, (*attachment).size) );
+//  cass::IdList list(serializer);
+//  return list.getList();
 }
 
 cass::HistogramBackend *TCPClient::operator() (const std::string &histogramkey)const
 {
-  bool ret;
-  CASSsoapProxy cass;
-  //	cass.soap_endpoint = (_servername->text() + ":" + _serverport->text()).toStdString().c_str();
-  cass.soap_endpoint = "xfhix:12321";
+  using namespace std;
+  bool ret(false);
+  CASSsoapProxy client;
+  client.soap_endpoint = _server.c_str();
+  client.getHistogram(histogramkey,0,&ret);
+  if(! ret)
+  {
+    stringstream ss;
+    ss << "Did not get Histogram with key '"<<histogramkey<<"'";
+    throw runtime_error(ss.str());
+  }
+  soap_multipart::iterator attachment(client.dime.begin());
+  if(client.dime.end() == attachment)
+  {
+    stringstream ss;
+    ss << "There is no attachmend in the received soap data";
+    throw runtime_error(ss.str());
+  }
+  cout << "TCPClient: DIME attachment:" << endl
+      << " TCPClient: Memory=" << (void*)(*attachment).ptr << endl
+      << " TCPClient: Size=" << (*attachment).size << endl
+      << " TCPClient: Type=" << ((*attachment).type?(*attachment).type:"null") << endl
+      << " TCPClient: ID=" << ((*attachment).id?(*attachment).id:"null") << endl;
+  std::string mimeType((*attachment).type);
 
-  std::string string;
-  cass.getHistogram(string,0,&ret);
-  //	if(ret)
-  //			cout << "return value: 'true'" << endl;
-  //	else {
-  //			cout << "return value is 'false'" << endl;
-  //			return;
-  //	}
-
-  soap_multipart::iterator attachment = cass.dime.begin();
-
+  if(mimeType == "application/cass2Dhistogram")
+  {
+//      cass::Serializer serializer( std::string((char *)(*attachment).ptr, (*attachment).size) );
+//      cass::Histogram2DFloat* hist = new cass::Histogram2DFloat(serializer);
+//      emit newHistogram(hist);  // slot deletes hist when done.
+  }
+  else if(mimeType == "application/cass1Dhistogram")
+  {
+//      cass::Serializer serializer( std::string((char *)(*attachment).ptr, (*attachment).size) );
+//      cass::Histogram1DFloat* hist = new cass::Histogram1DFloat(serializer);
+//      emit newHistogram(hist);  // slot deletes hist when done.
+  }
+  else if(mimeType == "application/cass0Dhistogram")
+  {
+//      cass::Serializer serializer( std::string((char *)(*attachment).ptr, (*attachment).size) );
+//      cass::Histogram0DFloat* hist = new cass::Histogram0DFloat(serializer);
+//      emit newHistogram(hist);  // slot deletes hist when done.
+  }
+  else
+  {
+    stringstream ss;
+    ss << "The mime type '"<<mimeType<<"' is unknown";
+    throw runtime_error(ss.str());
+  }
 }
