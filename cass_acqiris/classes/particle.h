9f3c0e82887817c13ea6dd694475897961a33020
Lutz Foucar <lutz.foucar@asg.mpg.de>
2011-01-08 19:34:09 +0000

    cleaned up comments

--- cass_acqiris/classes/particle.h
+++ cass_acqiris/classes/particle.h
@@ -148,67 +148,6 @@ namespace cass
 
       /** the charge of this Particle in atomic units */
       double _charge_au;
-
-//    public:
-//      //checks//
-//      bool							 operator==(const MyParticle &p)const		{return (fName.CompareTo(p.fName) == 0);}
-//      bool							 operator!=(const MyParticle &p)const		{return (!(*this==p));}
-//      bool							 operator==(const char * name)const			{return (fName.CompareTo(name) == 0);}
-//      bool							 operator!=(const char * name)const			{return (!(*this==name));}
-//      bool							 CheckTof(const MyDetektorHit &dh)			{return ((dh.Tof() > fCondTofFr) && (dh.Tof() < fCondTofTo));}
-//      bool							 CheckPos(const MyDetektorHit &dh)			{return fPosFlag?CheckQuad(dh):CheckRad(dh);}
-//      bool							 CheckRad(const MyDetektorHit &dh)			{return (TMath::Sqrt((dh.X()-fCondRadX)*(dh.X()-fCondRadX) + (dh.Y()-fCondRadY)*(dh.Y()-fCondRadY)) < fCondRad);}
-//      bool							 CheckQuad(const MyDetektorHit &dh)			{return (TMath::Abs(dh.X()-fCondRadX) < fCondWidthX) && (TMath::Abs(dh.Y()-fCondRadY) < fCondWidthY);}
-//      bool							 CheckTofAndPos(const MyDetektorHit &dh)	{return (CheckTof(dh) && CheckPos(dh));}
-//      //particlehit stuff//
-//      const MyParticleHit				&AddHit(const MyDetektorHit&);
-//      const size_t					 GetNbrOfParticleHits()const 		{return fPh.size();}
-//      const MyParticleHit				&operator[](size_t idx)const		{return fPh[idx];}
-//      void							 Clear()							{fPh.clear();}
-//      //properties and settings for this particle
-//      const MySpectrometer			&GetSpectrometer()const				{return fSp;}
-//      const char						*GetName()const						{return fName.Data();}
-//      double							 GetXcor()const						{return fXcor;}
-//      double							 GetYcor()const						{return fYcor;}
-//      double							 GetSfx()const						{return fSfx;}
-//      double							 GetSfy()const						{return fSfy;}
-//      double							 GetAngle()const					{return fAngle;}
-//      double							 GetT0()const						{return fT0;}
-//
-//      double							 GetCondTofFr()const				{return fCondTofFr;}
-//      double							 GetCondTofTo()const				{return fCondTofTo;}
-//      double							 GetCondTofRange()const				{return fCondTofTo-fCondTofFr;}
-//      double							 GetCondRad()const					{return fCondRad;}
-//      double							 GetCondRadX()const					{return fCondRadX;}
-//      double							 GetCondRadY()const					{return fCondRadY;}
-//      double							 GetCondWidthX()const				{return fCondWidthX;}
-//      double							 GetCondWidthY()const				{return fCondWidthY;}
-//      bool							 GetPosFlag()const					{return fPosFlag;}
-//
-//      double							 GetCharge_au()const				{return fCharge_au;}
-//      double							 GetMass_au()const					{return fMass_au;}
-//
-//    private:
-//      particleHits					 fPh;								//this contains all Hits on the detektor that belong to this particle
-//      double							 fCondTofFr;						//!the lower edge of the time of flight that this particle will be in
-//      double							 fCondTofTo;						//!the upper edge of the time of flight that this particle will be in
-//      double							 fCondRad;							//!the Radius of the circle on the detektor that this particle will be in
-//      double							 fCondRadX;							//!the x-center of the circle on the detektor that this particle will be in
-//      double							 fCondRadY;							//!the y-center of the circle on the detektor that this particle will be in
-//      double							 fCondWidthX;						//!the width x when choosing a quad condition
-//      double							 fCondWidthY;						//!the width y when choosing a quad condition
-//      bool							 fPosFlag;							//!flag to tell wether you want a radius or quad condition
-//
-//      double							 fAngle;							//!the angle to turn the detector in RAD; will be setted in deg but converted to rad while setting it
-//      double							 fXcor;								//!to move the center of the distribution to center of raw detektor
-//      double							 fYcor;								//!to move the center of the distribution to center of raw detektor
-//      double							 fSfx;								//!to scale the x such that it is realy mm
-//      double							 fSfy;								//!to scale the y such that it is realy mm
-//      double							 fT0;								//!to correct the time of flight
-//
-//      double							 fMass_au;							//!the Mass of this Particle in a.u.
-//      double							 fCharge_au;						//!the Charge of this Particle in a.u.
-//      TString							 fName;								//!how is this particle called
     };
   }
 }
