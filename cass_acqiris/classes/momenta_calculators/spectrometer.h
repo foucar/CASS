e5f9a72ba279e7ee9e7b2abdac0ffdaab3ad4ad3
Lutz Foucar <lutz.foucar@asg.mpg.de>
2011-01-08 19:27:01 +0000

    first adaptation of the spectrometer to fit in our layout

--- cass_acqiris/classes/momenta_calculators/spectrometer.h
+++ cass_acqiris/classes/momenta_calculators/spectrometer.h
@@ -4,11 +4,11 @@
 #include <vector>
 
 
-class MySpectrometerRegion
+class SpectrometerRegion
 {
 public:
-	MySpectrometerRegion():fL(0),fF(0)	{}
-	MySpectrometerRegion(double length_mm, double efield_Vpcm):
+  SpectrometerRegion():fL(0),fF(0)	{}
+  SpectrometerRegion(double length_mm, double efield_Vpcm):
 		fL(length_mm),
 		fF(efield_Vpcm)	{}
 
@@ -20,16 +20,14 @@ public:
 private:
 	double  fL;					//the length of the spectrometer region
 	double  fF;					//the electric field inside this region
-
-	ClassDef(MySpectrometerRegion,1)		//one part of the spectrometer
 };
 
 
-typedef std::vector<MySpectrometerRegion> SpecRegions;
-class MySpectrometer	//-------------Class describing a Spectrometer-Part ---------------------
+typedef std::vector<SpectrometerRegion> SpecRegions;
+class Spectrometer	//-------------Class describing a Spectrometer-Part ---------------------
 {
 public:
-	MySpectrometer():fMFieldOn(false),fCw(false),fCycFreq(0)								{}
+  Spectrometer():fMFieldOn(false),fCw(false),fCycFreq(0)								{}
 
 	double				 CyclotronPeriod_ns()const											{return fCycFreq;}
 	double				&CyclotronPeriod_ns()												{return fCycFreq;}
@@ -38,7 +36,7 @@ public:
 	bool				 RotationClockWise()const											{return fCw;}
 	bool				&RotationClockWise()												{return fCw;}
 
-	void				 AddSpectrometerRegion(double Length_mm, double EField_Vpcm)		{fSr.push_back(MySpectrometerRegion(Length_mm,EField_Vpcm));}
+  void				 AddSpectrometerRegion(double Length_mm, double EField_Vpcm)		{fSr.push_back(SpectrometerRegion(Length_mm,EField_Vpcm));}
 	const SpecRegions	&GetSpectrometerRegions() const										{return fSr;}
 	SpecRegions			&GetSpectrometerRegions() 											{return fSr;}
 	void				 Clear()															{fSr.clear();}
@@ -48,7 +46,5 @@ private:
 	bool				 fMFieldOn;															//flag showing wether a magnetic field was turned on
 	double				 fCycFreq;															//the cyclotron frequency of electrons, defines the strength of magnetic field
 	bool				 fCw;																//flag showing the direction of the magnetic field (by saying wether electron turn clock or counterclockwise
-
-	ClassDef(MySpectrometer,1)																//the spektrometer for a particle
 };
 #endif
