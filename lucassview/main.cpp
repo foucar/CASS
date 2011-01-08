8dc8ff79504ebf7de296c7206de132491ad681c6
Lutz Foucar <lutz.foucar@asg.mpg.de>
2011-01-08 19:46:34 +0000

    added main file for root based program

---
+++ lucassview/main.cpp
@@ -0,0 +1,21 @@
+#include <TRint.h>
+#include <TROOT.h>
+#include "../MyClient/MyClient.h"
+#include "../MySettings/MySettings.h"
+
+
+void initializeSettings(MySettings&);
+int main(int argc, char *argv[])
+{
+	TRint theApp("App", &argc, argv);
+	
+	MySettings set(false);
+	initializeSettings(set);
+	MyClient c(set);
+
+	//gROOT->ProcessLine(".L OnlineMacros.c");
+
+	theApp.Run();
+
+	return 0;
+}
