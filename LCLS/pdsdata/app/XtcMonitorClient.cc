a4bd4479e249f27185e70a0656e50cb18c388132
Lutz Foucar <lutz.foucar@asg.mpg.de>
2011-01-31 22:20:59 +0000

    initialize value to prevent a endlessloop

--- LCLS/pdsdata/app/XtcMonitorClient.cc
+++ LCLS/pdsdata/app/XtcMonitorClient.cc
@@ -49,6 +49,7 @@ static void _flushQueue(mqd_t q)
   XtcMonitorMsg m;
   unsigned priority;
   struct mq_attr mymq_attr;
+  mymq_attr.mq_curmsgs=0;
   do {
     mq_getattr(q, &mymq_attr);
     if (mymq_attr.mq_curmsgs)
