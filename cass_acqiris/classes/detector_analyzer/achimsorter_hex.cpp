02831b6fbcb146524d1dc11a1850c678d58ca24d
Lutz Foucar <lutz.foucar@asg.mpg.de>
2011-02-01 00:10:00 +0100

    partially implemented the intialization of achims routine
    added exmaple on what is needed to intialize the routine

--- cass_acqiris/classes/detector_analyzer/achimsorter_hex.cpp
+++ cass_acqiris/classes/detector_analyzer/achimsorter_hex.cpp
@@ -49,7 +49,68 @@ HexSorter::HexSorter()
   :DetectorAnalyzerBackend(),
    _achims_sorter(new sort_class())
 {
+  _achims_sorter->Cmcp = 0;
+  _achims_sorter->Cu1  = 1;
+  _achims_sorter->Cu2  = 2;
+  _achims_sorter->Cv1  = 3;
+  _achims_sorter->Cv2  = 4;
+  _achims_sorter->Cw1  = 5;
+  _achims_sorter->Cw2  = 6;
 
+  //	fAs->TDC_resolution_ns					= 0.1;
+//	fAs->tdc_array_row_length				= 1000;
+//	fAs->dont_overwrite_original_data		= true;
+//	fAs->count								= &fCnt[0];
+//	fAs->use_sum_correction					= static_cast<bool>(di.GetNbrSumUCorrPoints);
+//	fAs->use_pos_correction					= false;
+//	//this is needed to tell achims routine that we care for our own arrays//
+//	fAs->tdc[fAs->Cu1]						= (double*)(0x1);
+//	fAs->tdc[fAs->Cu2]						= (double*)(0x1);
+//	fAs->tdc[fAs->Cv1]						= (double*)(0x1);
+//	fAs->tdc[fAs->Cv2]						= (double*)(0x1);
+//	fAs->tdc[fAs->Cw1]						= (double*)(0x1);
+//	fAs->tdc[fAs->Cw2]						= (double*)(0x1);
+//	fAs->tdc[fAs->Cmcp]						= (double*)(0x1);
+//
+//	//set the variables that we get from the detektor//
+//	fAs->uncorrected_time_sum_half_width_u	= di.GetTsuWidth();
+//	fAs->uncorrected_time_sum_half_width_v	= di.GetTsvWidth();
+//	fAs->uncorrected_time_sum_half_width_w	= di.GetTswWidth();
+//	fAs->fu									= di.GetSfU();
+//	fAs->fv									= di.GetSfV();
+//	fAs->fw									= di.GetSfW();
+//	fAs->max_runtime						= di.GetRunTime();
+//	fAs->dead_time_anode					= di.GetDeadTimeAnode();
+//	fAs->dead_time_mcp						= di.GetDeadTimeMCP();
+//	fAs->MCP_radius							= di.GetMCPRadius();
+//	fAs->use_HEX							= di.IsHexAnode();
+//	fAs->use_MCP							= di.UseMCP();
+//
+//	//set the sum walk correction points, Achims Routine will internaly find out how many we gave it//
+//	for (int i=0;i<di.GetNbrSumUCorrPoints();++i)
+//		fAs->sum_corrector_U->set_point(di.GetUCorrPos(i),di.GetUCorrCorr(i));
+//
+//	for (int i=0;i<di.GetNbrSumVCorrPoints();++i)
+//		fAs->sum_corrector_V->set_point(di.GetVCorrPos(i),di.GetVCorrCorr(i));
+//
+//	for (int i=0;i<di.GetNbrSumWCorrPoints();++i)
+//		fAs->sum_corrector_W->set_point(di.GetWCorrPos(i),di.GetWCorrCorr(i));
+//
+//	//init() must be called only once
+//	int error_code = fAs->init();
+//	if (error_code != 0)
+//	{
+//		char error_text[500];
+//		fAs->get_error_text(error_code,500,error_text);
+//		std::cout << "Achims Sorter: "<<error_text<<std::endl;
+//		exit(1);
+//	}
+//	else
+//	{
+//		fAlreadyInitialized=true;
+//		fSwc = sum_walk_calibration_class::new_sum_walk_calibration_class(fAs,49);
+//		fSfc = new scalefactors_calibration_class(true,fAs->max_runtime*0.78,fAs->fu,fAs->fv,fAs->fw);
+//	}
 }
 
 detectorHits_t& HexSorter::operator()(detectorHits_t &hits)
@@ -69,7 +130,7 @@ void HexSorter::loadSettings(CASSSettings& s, DelaylineDetector &d)
         << "' which is a Quad Detector.";
     throw invalid_argument(ss.str());
   }
-  _signals.clear();
+  _signals.resize(7);
   _signals.push_back(make_pair(&d.mcp(),vector<double>()));
   _signals.push_back(make_pair(&d.layers()['U'].wireends()['1'],vector<double>()));
   _signals.push_back(make_pair(&d.layers()['U'].wireends()['2'],vector<double>()));
