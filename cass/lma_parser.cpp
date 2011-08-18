df956c8ced507b0fe444c8482961da54e14c001b
Lutz Foucar <lutz.foucar@asg.mpg.de>
2011-08-18 17:19:07 +0200

    first implementation of the lma parser

--- cass/lma_parser.cpp
+++ cass/lma_parser.cpp
@@ -11,6 +11,7 @@
 #include <stdexcept>
 
 #include "lma_parser.h"
+#include "lma_file_header.h"
 
 using namespace cass;
 using namespace std;
@@ -18,4 +19,52 @@ using namespace std::tr1;
 
 void LMAParser::run()
 {
+  ifstream &file(*(_filepointer._filestream));
+
+  vector<char> headerarray(sizeof(lmaFile::GeneralHeader));
+  file.read(&headerarray.front(),sizeof(lmaFile::GeneralHeader));
+  const lmaFile::GeneralHeader &header
+      (*reinterpret_cast<lmaFile::GeneralHeader*>(&headerarray.front()));
+
+  if (header.nbrBits != 16)
+  {
+    stringstream ss;
+    ss<<"LMAParser():run: The lma file seems to contain 8-bit wavefroms '"
+     <<header.nbrBits<<"'. Currently this is not supported.";
+    throw runtime_error(ss.str());
+  }
+
+  for (int16_t i(0) ; i < header.nbrChannels ;++i)
+  {
+    if (header.usedChannelBitmask & (0x1<<i))
+    {
+      vector<char> chanheaderarray(sizeof(lmaFile::ChannelHeader));
+      file.read(&chanheaderarray.front(),sizeof(lmaFile::ChannelHeader));
+    }
+  }
+
+  while(!file.eof())
+  {
+    uint32_t eventID(FileStreaming::peek<int32_t>(file));
+    savePos(eventID);
+
+    eventID = FileStreaming::retrieve<int32_t>(file);
+    double horpos(FileStreaming::retrieve<double>(file));
+
+    for (size_t i=0; i<header.nbrChannels;++i)
+    {
+      if (header.usedChannelBitmask & (0x1<<i))
+      {
+        int16_t nbrPulses(FileStreaming::retrieve<int16_t>(file));
+        for (int16_t i(0); i < nbrPulses; ++i)
+        {
+          int32_t wavefromOffset(FileStreaming::retrieve<int32_t>(file));
+          int32_t pulslength(FileStreaming::retrieve<int32_t>(file));
+          size_t dataSize(pulslength * 2);
+          file.seekg(file.tellg() + dataSize);
+        }
+      }
+    }
+  }
+
 }
--- cass/lma_parser.h
+++ cass/lma_parser.h
@@ -20,6 +20,16 @@ namespace cass
   class LMAParser : public FileParser
   {
   public:
+    /** parse the lma file
+     *
+     * first read the headers and check whether this parser is suitable for
+     * reading this kind of waveforms.\n
+     * Go through all events an remember the position and correlate them with
+     * the even id.\n
+     * since we zero substracted the waveform in the lma file, we need to
+     * iterate through the waveform sinpplets called pulses and find out how
+     * long they are to be able to jump to the next event.
+     */
     void run();
   };
 }
