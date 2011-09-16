5dfed2a43155c7a451096cf4a9123ed6846f9700
Lutz Foucar <lutz.foucar@asg.mpg.de>
2011-09-16 21:07:11 +0200

    moved all pixel detector related files to a separate sub project

--- cass_pnccd/pnccd_analysis_new.cpp
+++ cass_pnccd/pnccd_analysis_new.cpp
@@ -8,6 +8,10 @@
 
 #include "pnccd_analysis_new.h"
 
+#include "pnccd_device.h"
+#include "pixel_detector.h"
+#include "cass_event.h"
+
 using namespace cass;
 using namespace pnCCD;
 
@@ -28,5 +32,11 @@ void NewAnalysis::saveSettings()
 
 void NewAnalysis::operator ()(CASSEvent *evt)
 {
+  //retrieve the right device
+  pnCCDDevice &dev (*dynamic_cast<pnCCDDevice*>(evt->devices()[CASSEvent::pnCCD]));
+
+  const size_t detId = 0;
+  PixelDetector &det = (*dev.detectors())[detId];
+
 
 }
