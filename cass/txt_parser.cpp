d5194a9e44431c7366fdf7889458d604d2f21913
Lutz Foucar <lutz.foucar@asg.mpg.de>
2011-10-23 14:26:03 +0900

    implemented the txt parser

--- cass/txt_parser.cpp
+++ cass/txt_parser.cpp
@@ -9,8 +9,11 @@
 #include <fstream>
 #include <sstream>
 #include <stdexcept>
+#include <iostream>
 
 #include "txt_parser.h"
+
+#include "cass_settings.h"
 
 using namespace cass;
 using namespace std;
@@ -18,4 +21,65 @@ using namespace std::tr1;
 
 void TxtParser::run()
 {
+  CASSSettings s;
+  s.beginGroup("TxtParser");
+  char delim = s.value("Deliminator",'\t').toInt();
+  string eventIdhead = s.value("EventIdHeader","Event ID 24 bits").toString().toStdString();
+  s.endGroup();
+  ifstream &file(*(_filepointer._filestream));
+  file.seekg (0, ios::end);
+  const streampos filesize(file.tellg());
+  file.seekg (0, ios::beg);
+
+  string headerline;
+  vector<string> headers;
+  splitString splitHeader;
+  while (true)
+  {
+    getline(file, headerline);
+    headers.clear();
+    splitHeader(headerline,headers,delim);
+    if (!headers.empty() &&
+        !QString::fromStdString(headerline).contains("GMD	(GMD_DATA)	-	GMD main vlaues"))
+      break;
+  }
+  cout <<"TxtParser: the txt file contains the following variables:";
+  vector<string>::const_iterator h(headers.begin());
+  for (; h != headers.end();++h)
+    cout <<"'"<<*h<<"',";
+  cout <<endl;
+
+  string line;
+  vector<double> values;
+  map<string,double> head2value;
+  streampos eventstartpos;
+  split splitLine;
+  while(file.tellg()<filesize)
+  {
+    while(true)
+    {
+      eventstartpos = file.tellg();
+      getline(file, line);
+      values.clear();
+      splitLine(line,values,delim);
+      if (!values.empty())
+        break;
+    }
+    if(headers.size() != values.size())
+      throw runtime_error("TxtParser():There are not enough values for the amount of values suggested by the header");
+
+    vector<double>::const_iterator value(values.begin());
+    vector<string>::const_iterator head(headers.begin());
+    for (;value != values.end(); ++value, ++head)
+    {
+      head2value[*head] = *value;
+//      cout << "'"<<*head<<"'="<<*value<<", ";
+    }
+//    cout <<endl;
+    const streampos currentpos(file.tellg());
+    file.seekg(eventstartpos);
+    savePos(head2value[eventIdhead]);
+//    cout << "EventID: '"<<eventIdhead<<"'= "<<head2value[eventIdhead]<<endl;
+    file.seekg(currentpos);
+  }
 }
