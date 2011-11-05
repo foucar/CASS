973399218616227ffa9820d40e8ef0a04d065bd6
Lutz Foucar <Lutz.Foucar@asg.mpg.de>
2011-11-05 21:42:19 +0900

    using input base class for shared memory input

--- cass/sharedmemory_input.cpp
+++ cass/sharedmemory_input.cpp
@@ -16,128 +16,69 @@
 #include "pdsdata/xtc/Dgram.hh"
 
 
-cass::SharedMemoryInput::SharedMemoryInput(char * partitionTag,
-                                           int index,
-                                           cass::RingBuffer<cass::CASSEvent,cass::RingBufferSize>& ringbuffer,
-                                           QObject *parent)
-                                             :QThread(parent),
-                                             _ringbuffer(ringbuffer),
-                                             _partitionTag(partitionTag),
-                                             _index(index),
-                                             _quit(false),
-                                             _convert(*cass::FormatConverter::instance()),
-                                             _pause(false),
-                                             _paused(false)
+using namespace cass;
+using namespace std;
+
+SharedMemoryInput::SharedMemoryInput(char * partitionTag,
+                                     int index,
+                                     RingBuffer<CASSEvent,RingBufferSize>& ringbuffer,
+                                     QObject *parent)
+  :InputBase(ringbuffer,parent),
+    _partitionTag(partitionTag),
+    _index(index),
+    _convert(*FormatConverter::instance())
+{}
+
+SharedMemoryInput::~SharedMemoryInput()
 {
+  VERBOSEOUT(cout<<"deleting shared memory input"<<endl);
 }
 
-cass::SharedMemoryInput::~SharedMemoryInput()
+
+void SharedMemoryInput::load()
 {
-  VERBOSEOUT(std::cout<<"deleting shared memory input"<<std::endl);
+  _convert.loadSettings(0);
 }
 
-
-void cass::SharedMemoryInput::loadSettings(size_t what)
+void SharedMemoryInput::run()
 {
-  //pause yourselve//
-  VERBOSEOUT(std::cout << "Shared Memory Input: Load Settings: suspend before loading settings"
-      <<std::endl);
-  suspend();
-  //load settings//
-  VERBOSEOUT(std::cout << "Shared Memory Input: Load Settings: suspended. Now loading Settings"
-      <<std::endl);
-  _convert.loadSettings(what);
-  //resume yourselve//
-  VERBOSEOUT(std::cout << "Shared Memory Input: Load Settings: Done loading Settings. Now Resuming Thread"
-      <<std::endl);
-  resume();
-  VERBOSEOUT(std::cout << "Shared Memory Input: Load Settings: thread is resumed"
-      <<std::endl);
-}
-
-void cass::SharedMemoryInput::suspend()
-{
-  _pause=true;
-  //wait until you are paused//
-  waitUntilSuspended();
-}
-
-void cass::SharedMemoryInput::resume()
-{
-  //if the thread has not been paused return here//
-  if(!_pause)
-    return;
-  //reset the pause flag;
-  _pause=false;
-  //tell run to resume via the waitcondition//
-  _pauseCondition.wakeOne();
-}
-
-void cass::SharedMemoryInput::waitUntilSuspended()
-{
-  //if it is already paused then retrun imidiatly//
-  if(_paused)
-    return;
-  //otherwise wait until the conditions has been called//
-  QMutex mutex;
-  QMutexLocker lock(&mutex);
-  _waitUntilpausedCondition.wait(&mutex);
-}
-
-void cass::SharedMemoryInput::run()
-{
-  //start the xtcmonitorclient//
-  //this eventqueue will subscripe to a partitiontag//
-  VERBOSEOUT(std::cout << "starting shared memory in put with partition Tag: \""
+  _status = lmf::PausableThread::running;
+  VERBOSEOUT(cout << "starting shared memory in put with partition Tag: \""
       <<_partitionTag <<"\""
-      << " and Client Index "<< _index<<std::endl);
+      << " and Client Index "<< _index<<endl);
   Pds::XtcMonitorClient::run(_partitionTag,_index,_index,2);
-  VERBOSEOUT(std::cout << "shared memory input is closing down"<<std::endl);
+  VERBOSEOUT(cout << "shared memory input is closing down"<<endl);
 }
 
-void cass::SharedMemoryInput::end()
+void SharedMemoryInput::end()
 {
-  VERBOSEOUT(std::cout << "shared memory input got signal to close"<<std::endl);
-  //tell the loop that it should quit
-  _quit=true;
-  //wait until we have finished, but only for 2 seconds//
-  //if we were not finished by that time the we want to terminate//
-  //ourselves//
-  VERBOSEOUT(std::cout << "wait for 5 s that shared memory shuts down"<<std::endl);
+  VERBOSEOUT(cout << "SharedMemoryInput::end(): got signal to close"<<endl);
+  _control = _quit;
+  VERBOSEOUT(cout << "SharedMemoryInput::end(): wait for 5 s that shared memory"
+             << " shuts down"<<endl);
   if(!wait(5000))
   {
-    VERBOSEOUT(std::cout << "time has elapsed. So we probably lost connection to"
-        <<"the shared memory. Therefore we will terminate the thread"<<std::endl);
+    VERBOSEOUT(cout << "SharedMemoryInput::end(): time has elapsed. So we"
+        <<" probably lost connection to the shared memory. Therefore we will"
+        <<" terminate the thread"<<endl);
     terminate();
   }
-  VERBOSEOUT(std::cout << "Ok. Shared Memory input thread has shut down within 5 s"<<std::endl);
+  else
+  {
+    VERBOSEOUT(cout << "Ok. Shared Memory input thread has shut down within 5 s"<<endl);
+  }
 }
 
-int cass::SharedMemoryInput::processDgram(Pds::Dgram* datagram)
+int SharedMemoryInput::processDgram(Pds::Dgram* datagram)
 {
-  //pause execution if suspend has been called//
-  if (_pause)
-  {
-    //lock the mutex to prevent that more than one thread is calling pause//
-    _pauseMutex.lock();
-    //set the status flag to paused//
-    _paused=true;
-    //tell the wait until paused condtion that we are now pausing//
-    _waitUntilpausedCondition.wakeOne();
-    //wait until the condition is called again
-    _pauseCondition.wait(&_pauseMutex);
-    //set the status flag//
-    _paused=false;
-    //unlock the mutex, such that others can work again//
-    _pauseMutex.unlock();
-  }
+  pausePoint();
 
   //check if it just timed out, if so return
   if(!datagram)
-    return _quit;
+    return (_control == _quit);
 
   //make a pointer to a element in the ringbuffer//
-  cass::CASSEvent *cassevent(0);
+  CASSEvent *cassevent(0);
 
   //retrieve a new element from the ringbuffer//
   _ringbuffer.nextToFill(cassevent);
@@ -145,13 +86,13 @@ int cass::SharedMemoryInput::processDgram(Pds::Dgram* datagram)
   //read the datagram to the ringbuffer//
   Pds::Dgram& dg = *reinterpret_cast<Pds::Dgram*>(cassevent->datagrambuffer());
   memcpy(&dg,datagram,sizeof(Pds::Dgram));
-  if (datagram->xtc.sizeofPayload() > static_cast<int>(cass::DatagramBufferSize))
+  if (datagram->xtc.sizeofPayload() > static_cast<int>(DatagramBufferSize))
   {
-    std::cout << "datagram size is bigger than the maximum buffer size of "
-        <<cass::DatagramBufferSize/1024/1024
-        <<" MB. Something is wrong. Skipping the datagram"
-        <<std::endl;
-    return _quit;
+    cout << "datagram size is bigger than the maximum buffer size of "
+         <<DatagramBufferSize/1024/1024
+         <<" MB. Something is wrong. Skipping the datagram"
+         <<endl;
+    return  _control == _quit;
   }
   memcpy(dg.xtc.payload(),datagram+1,datagram->xtc.sizeofPayload());
 
@@ -165,7 +106,7 @@ int cass::SharedMemoryInput::processDgram(Pds::Dgram* datagram)
   emit newEventAdded();
 
   //return the quit code//
-  return _quit;
+  return  _control == _quit;
 }
 
 
--- cass/sharedmemory_input.h
+++ cass/sharedmemory_input.h
@@ -10,12 +10,8 @@
 #ifndef __SHAREDMEMORYINPUT_H__
 #define __SHAREDMEMORYINPUT_H__
 
-#include <QtCore/QMutex>
-#include <QtCore/QObject>
-#include <QtCore/QThread>
-#include <QtCore/QWaitCondition>
-
 #include "cass.h"
+#include "input_base.h"
 #include "pdsdata/app/XtcMonitorClient.hh"
 #include "ringbuffer.h"
 #include "cass_event.h"
@@ -25,23 +21,26 @@ namespace cass
   //forward declaration//
   class FormatConverter;
 
-  /** Shared Memory Input.
+  /** Shared Memory Input for receiving xtc datagrams
+   *
    * This class is a thread that connects to the sahred memory of LCLS. The
    * baseclass does all the connection and once there is new data available
    * it calles processDatagram, where we can add code that we want to use.
    * The first datagram that will be send to this class is a datagram
    * containing the last known configure transition. This is to make sure that
    * when starting the program we will always get the latest state of the DAQ.
+   *
    * @author Lutz Foucar
    */
-  class CASSSHARED_EXPORT SharedMemoryInput
-    : public QThread, Pds::XtcMonitorClient
+  class SharedMemoryInput
+    : public InputBase, Pds::XtcMonitorClient
   {
-    Q_OBJECT;
   public:
-    /** constructor.
+    /** constructor
+     *
      * creates the thread. The base class will create the interface to the shared
      * memory.
+     *
      * @param PartitionTag the name of the partition tag we want to connect to
      * @param buffer the ringbuffer, that we take events out and fill it
      *        with the incomming information
@@ -56,85 +55,47 @@ namespace cass
     /** deletes the thread*/
     ~SharedMemoryInput();
 
-    /** starts the thread.
+    /** starts the thread
+     *
      * Starts the thread and the loop in the shared memory client we inerhited
      * from. The loop will be notified when there are new events available in
      * the shared memory.
      */
     void run();
 
-    /** suspends the thread.
-     * Suspends the thread after it has executed the event we are working on
-     * right now. Will return only when the thread has really suspenden by calling
-     * @see waitUntilSuspended() internally.
-     */
-    void suspend();
-
-    /** resumes the thread, when it was suspended. Otherwise it just retruns*/
-    void resume();
-
-    /** overwrite the base class function with our code.
+    /** overwrite the base class function with our code
+     *
      * This is called once the eventqueue has new data available.
+     *
      * @return the errorcode, when != 0, then the baseclasses will exit its loop
      * @param[in] dg The datagram we can work on
      */
-    int  processDgram(Pds::Dgram*dg);
+    int processDgram(Pds::Dgram*dg);
 
-  signals:
-    /** signal to indicate that we are done processing an event.
-     * this is used for by the ratemeter to evaluate how fast we get events.
-     */
-    void newEventAdded();
-
-  public slots:
-    /** call this slot, when you want to quit the thread.
-     * Makes the thread execute the last event and then quit normally.
+    /** do all clean up when quitting
+     *
+     * this function from the base class needs to be overwritten, since it might
+     * happen that we loose connection to the shared memory in wich case we
+     * will never be able to check the quit status. Therefore after waiting 5 s
+     * this will just terminate the thread.
      */
     void end();
 
-    /** load the parameters used for this thread*/
-    void loadSettings(size_t what);
-
-  protected:
-    /** function that will wait until we really suspended.
-     * will be called by suspend, so that it returns only when thread has really
-     * suspended.
-     */
-    void waitUntilSuspended();
+    /** load the parameters of the FormatConverter */
+    void load();
 
   private:
-    /** the Ringbuffer we take events out and fill them with the incomming data*/
-    cass::RingBuffer<cass::CASSEvent,cass::RingBufferSize>  &_ringbuffer;
-
     /** the name of the partition tag we connect to*/
     char *_partitionTag;
 
     /** the client index of the shared memory */
     int _index;
 
-    /** a flag to tell the thread wther the user wants to quit, is set by @see end()*/
-    bool _quit;
-
     /** a reference to the format converter functor
      *
      * The converter will convert the incomming data to our CASSEvent
      */
     FormatConverter &_convert;
-
-    /** a mutex for suspending the thread*/
-    QMutex _pauseMutex;
-
-    /** a condition that we will wait on until we are not suspended anymore*/
-    QWaitCondition _pauseCondition;
-
-    /** flag telling whether we shouodl suspend ourselves*/
-    bool _pause;
-
-    /** flag telling whether we are already suspended*/
-    bool _paused;
-
-    /** condition that will wait until the thread is rally suspended*/
-    QWaitCondition _waitUntilpausedCondition;
   };
 
 }//end namespace cass
