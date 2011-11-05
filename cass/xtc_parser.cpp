225b75cff03be49bb4325e804a85ec0bc3ca8ef4
Lutz Foucar <Lutz.Foucar@asg.mpg.de>
2011-11-05 23:29:48 +0900

    some compile bugfixes solved

--- cass/xtc_parser.cpp
+++ cass/xtc_parser.cpp
@@ -16,21 +16,21 @@
 using namespace cass;
 using namespace std;
 
-bool XtcParser::run()
+void XtcParser::run()
 {
   ifstream &file(*(_readerpointerpair.second._filestream));
   Pds::Dgram datagram;
   while(!file.eof())
   {
     const streampos eventstartpos(file.tellg());
-    file.read(datagram, sizeof(Pds::Dgram));
-    file.seekg(dg.xtc.sizeofPayload(), ios_base::cur);
+    file.read(reinterpret_cast<char*>(&datagram), sizeof(Pds::Dgram));
+    file.seekg(datagram.xtc.sizeofPayload(), ios_base::cur);
 
     if (datagram.seq.service() == Pds::TransitionId::L1Accept)
     {
       uint64_t bunchId = datagram.seq.clock().seconds();
       bunchId = (bunchId<<32) + static_cast<uint32_t>(datagram.seq.stamp().fiducials()<<8);
-      savepos(eventstartpos,bunchId);
+      savePos(eventstartpos,bunchId);
     }
   }
 }
