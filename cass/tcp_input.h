f96b18586f77da666e43d9bfaf4c43007502d448
Lutz Foucar <lutz.foucar@asg.mpg.de>
2011-12-19 13:58:28 +0100

    docu

--- cass/tcp_input.h
+++ cass/tcp_input.h
@@ -35,8 +35,11 @@ namespace cass
  *           "agat". Possible values are:
  *           - "agat": The type of data that is streamed from a normal version
  *                     of AGAT3.
- *           - "shm": The type of data that is streamed from the RACOON shm2tcp
- *                    server.
+ *           - "shmToOld": The type of data that is streamed from the RACOON
+ *                         shm2tcp server. The output is written to the old
+ *                         pnCCD container to be used with the old analysis chain.
+ *            - "shm": Type of data that is streamed from RACOON shm2tcp server.
+ *                     is to be used with the new ccd analysis chain.
  *
  * @author Lutz Foucar
  */
