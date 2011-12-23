0de621a8787f291a5c3bcf9dc9b92ce70d833859
Lutz Foucar <lutz.foucar@asg.mpg.de>
2011-12-23 15:22:45 +0100

    redid tcpserver so that it works without

--- cass/tcpserver.cpp
+++ cass/tcpserver.cpp
@@ -1,4 +1,11 @@
 // Copyright (C) 2010 Jochen Küpper
+// Copyright (C) 2011 Lutz Foucar
+
+/**
+ * @file tcpserver.cpp the soap server implementation
+ *
+ * @author Jochen Kuepper
+ */
 
 #include <stdexcept>
 
@@ -10,208 +17,258 @@
 #include <QtGui/QImage>
 #include <QThreadPool>
 
+#include "tcpserver.h"
+
 #include "CASSsoap.nsmap"
 #include "histogram_getter.h"
-#include "postprocessing/postprocessor.h"
-#include "tcpserver.h"
+#include "postprocessor.h"
+#include "backend.h"
 #include "id_list.h"
 #include "cass_exceptions.h"
+#include "input_base.h"
+#include "worker.h"
+#include "analyzer.h"
 
 
-namespace cass
-{
 
+using namespace cass;
+using namespace std;
 
+//==========DEFINE static variables=========================
 SoapServer::shared_pointer SoapServer::_instance;
 QMutex SoapServer::_mutex;
 const size_t SoapServer::_backlog(100);
+
+SoapServer::shared_pointer SoapServer::instance(const EventGetter& event,
+                                                const HistogramGetter& hist,
+                                                size_t port)
+{
+  QMutexLocker locker(&_mutex);
+  if(!_instance)
+    _instance = shared_pointer(new SoapServer(event, hist, port));
+  return _instance;
+}
+//==========================================================
 
 
 
 void SoapHandler::run()
 {
-    _soap->serve();   // serve request
-    _soap->destroy(); // dealloc C++ data, dealloc data and clean up (destroy + end)
+  _soap->serve();   // serve request
+  _soap->destroy(); // dealloc C++ data, dealloc data and clean up (destroy + end)
 }
-
-
-
-SoapServer::shared_pointer SoapServer::instance(const EventGetter& event, const HistogramGetter& hist, size_t port)
-{
-    QMutexLocker locker(&_mutex);
-    if(!_instance)
-        _instance = shared_pointer(new SoapServer(event, hist, port));
-    return _instance;
-}
-
-
-
-void SoapServer::destroy()
-{
-    QMutexLocker locker(&_mutex);
-    _instance->terminate();
-    _instance->wait();
-    _instance.reset();
-}
-
 
 
 void SoapServer::run()
 {
-    // define timeouts and such for individual requests
-    _soap->send_timeout   =   60; // 60 seconds
-    _soap->recv_timeout   =   60; // 60 seconds
-    _soap->accept_timeout =    0; // server never stops accepting
-    _soap->max_keep_alive = 1000; // max keep-alive sequence
-    // allow immediate re-use of address/socket
-    _soap->bind_flags = SO_REUSEADDR;
-    // start SOAP
-    if(SOAP_INVALID_SOCKET == _soap->bind(NULL, _port, _backlog))
-        throw std::runtime_error("No valid socket for SOAP server");
-    while(true) {
-        if(SOAP_INVALID_SOCKET == _soap->accept()) {
-            if(_soap->errnum) {
-                _soap->soap_stream_fault(std::cerr);
-                std::cerr << "*** No valid socket for SOAP connection ***" << std::endl;
-            } else
-                std::cerr << "*** Server timeout for SOAP connection ***" << std::endl;
-            break;
-        }
-        CASSsoapService *tsoap(_soap->copy()); // make a safe copy
-        if(! tsoap)
-            break;
-        SoapHandler *handler(new SoapHandler(tsoap));
-        QThreadPool::globalInstance()->start(handler);  // SoapHandler::setAutoDelete() is set by default.
+  // define timeouts and such for individual requests
+  _soap->send_timeout   =   60; // 60 seconds
+  _soap->recv_timeout   =   60; // 60 seconds
+  _soap->accept_timeout =    0; // server never stops accepting
+  _soap->max_keep_alive = 1000; // max keep-alive sequence
+  // allow immediate re-use of address/socket
+  _soap->bind_flags = SO_REUSEADDR;
+  // start SOAP
+  if(SOAP_INVALID_SOCKET == _soap->bind(NULL, _port, _backlog))
+    throw runtime_error("No valid socket for SOAP server");
+  while(true)
+  {
+    if(SOAP_INVALID_SOCKET == _soap->accept())
+    {
+      if(_soap->errnum)
+      {
+        _soap->soap_stream_fault(cerr);
+        cerr << "*** No valid socket for SOAP connection ***" << endl;
+      }
+      else
+        cerr << "*** Server timeout for SOAP connection ***" << endl;
+      break;
     }
+    CASSsoapService *tsoap(_soap->copy()); // make a safe copy
+    if(! tsoap)
+      break;
+    SoapHandler *handler(new SoapHandler(tsoap));
+    QThreadPool::globalInstance()->start(handler);  // SoapHandler::setAutoDelete() is set by default.
+  }
 }
 
-} // end namespace cass
-
-
-
+/** quits CASS by telling the input to end */
 int CASSsoapService::quit(bool *success)
 {
-    VERBOSEOUT(std::cerr << "CASSsoapService::quit" << std::endl);
-    cass::SoapServer::instance()->emit_quit();
-    *success = true;;
-    return SOAP_OK;
+  VERBOSEOUT(cerr << "CASSsoapService::quit" << endl);
+  InputBase::reference().end();
+  *success = true;;
+  return SOAP_OK;
 }
 
-
-
+/** will call the load settings members of input and the workers
+ *
+ * first pause the input and the workers. Then call the load settings. then
+ * resume the threads.
+ */
 int CASSsoapService::readini(size_t what, bool *success)
 {
-    VERBOSEOUT(std::cerr << "CASSsoapService::readini(what=" << what << ")" << std::endl);
-    cass::SoapServer::instance()->emit_readini(what);
-    *success = true;;
-    return SOAP_OK;
+  VERBOSEOUT(cerr << "CASSsoapService::readini(what=" << what << ")" << endl);
+  QMutexLocker inputLock(&InputBase::reference().lock);
+  QMutexLocker workerLock(&Workers::reference().lock);
+  InputBase::reference().pause(true);
+  Workers::reference().pause();
+  InputBase::reference().loadSettings(what);
+  Analyzer::instance()->loadSettings(what);
+  QWriteLocker pplock(&PostProcessors::instance()->lock);
+  PostProcessors::instance()->loadSettings(what);
+  Workers::reference().resume();
+  InputBase::reference().resume();
+  *success = true;;
+  return SOAP_OK;
 }
 
 int CASSsoapService::getPostprocessorIds(bool *success)
 {
-    static QQueue< std::string* > queue;
-    int result;
-    cass::PostProcessors::shared_pointer pp(cass::PostProcessors::instance(""));
-    QReadLocker lock(&pp->lock);
-    std::tr1::shared_ptr<cass::IdList> idlist(pp->getIdList());
-    cass::Serializer* ser(new cass::Serializer);
-    idlist->serialize(*ser);
-    *success = true;
-    soap_set_dime(this);
-    std::string* datstr = new std::string(ser->buffer());
-    queue.enqueue(datstr);
-    result = soap_set_dime_attachment(this, (char*) datstr->data(), ser->buffer().size(), "application/postprocessorList", "0", 0, NULL);
-    if(100 < queue.size())
-        delete queue.dequeue();
-    delete ser;
-    return result;
+  /** @todo use shared pointer inside the queue */
+  /** @todo make sure that the id list is copied and protected */
+  static QQueue< string* > queue;
+  int result;
+  PostProcessors::shared_pointer pp(PostProcessors::instance(""));
+  QReadLocker lock(&pp->lock);
+  tr1::shared_ptr<IdList> idlist(pp->getIdList());
+  Serializer* ser(new Serializer);
+  idlist->serialize(*ser);
+  *success = true;
+  soap_set_dime(this);
+  string* datstr = new string(ser->buffer());
+  queue.enqueue(datstr);
+  result = soap_set_dime_attachment(this, (char*) datstr->data(), ser->buffer().size(), "application/postprocessorList", "0", 0, NULL);
+  if(100 < queue.size())
+    delete queue.dequeue();
+  delete ser;
+  return result;
 }
 
-
+/** will call savesettings members of workers
+ *
+ * pause the workers before calling this function then call savesettings for
+ * analyzers and postprocessors, then resume the worker threads Prevent others
+ * from retrieving data from the postprocessors by locking it
+ */
 int CASSsoapService::writeini(size_t what, bool *success)
 {
-    VERBOSEOUT(std::cerr << "CASSsoapService::readini(what=" << what << ")" << std::endl);
-    cass::SoapServer::instance()->emit_writeini(what);
-    *success = true;;
-    return SOAP_OK;
+  VERBOSEOUT(cerr << "CASSsoapService::readini(what=" << what << ")" << endl);
+  QMutexLocker workerLock(&Workers::reference().lock);
+  Workers::reference().pause();
+  Analyzer::instance()->saveSettings();
+  QWriteLocker pplock(&PostProcessors::instance()->lock);
+  PostProcessors::instance()->saveSettings();
+  Workers::reference().resume();
+  InputBase::reference().resume();
+  *success = true;;
+  return SOAP_OK;
 }
 
-int CASSsoapService::clearHistogram(cass::PostProcessors::key_t type, bool *success)
+/** clear the selected postprocessors histogram list
+ *
+ * lock the postprocessors handler postprocessor list and retrieve the
+ * requested postprocessor from it. Tell it to clear its histogram list.
+ */
+int CASSsoapService::clearHistogram(PostProcessors::key_t type, bool *success)
 {
-    VERBOSEOUT(std::cerr << "CASSsoapService::clearHistogram(type=" << type << ")" << std::endl);
-    cass::SoapServer::instance()->emit_clearHistogram(type);
+  VERBOSEOUT(cerr << "CASSsoapService::clearHistogram(type=" << type << ")" << endl);
+  QWriteLocker pplock(&PostProcessors::instance()->lock);
+  try
+  {
+    PostProcessors::instance()->getPostProcessor(type).clearHistograms();
     *success = true;;
     return SOAP_OK;
+  }
+  catch(const InvalidPostProcessorError&)
+  {
+    *success = false;
+    return SOAP_FATAL_ERROR;
+  }
 }
 
-int CASSsoapService::receiveCommand(cass::PostProcessors::key_t type, std::string command, bool *success)
+/** pass the string on to the requested postprocessor
+ *
+ * lock the postprocessor handler list then retrieve the requested postprocessor
+ * and pass the string to process to it.
+ */
+int CASSsoapService::receiveCommand(PostProcessors::key_t type, string command, bool *success)
 {
-    VERBOSEOUT(std::cerr << "CASSsoapService::receiveCommand(type=" << type << ")" << std::endl);
-    cass::SoapServer::instance()->emit_receiveCommand(type, command);
+  VERBOSEOUT(cerr << "CASSsoapService::receiveCommand(type=" << type << ")" << endl);
+  QWriteLocker pplock(&PostProcessors::instance()->lock);
+  try
+  {
+    PostProcessors::instance()->getPostProcessor(type).processCommand(command);
     *success = true;;
     return SOAP_OK;
+  }
+  catch(const InvalidPostProcessorError&)
+  {
+    *success = false;
+    return SOAP_FATAL_ERROR;
+  }
 }
-
-
 
 int CASSsoapService::getEvent(size_t type, unsigned t1, unsigned t2, bool *success)
 {
-    VERBOSEOUT(std::cerr << "CASSsoapService::getEvent" << std::endl);
-    static QQueue<std::string *> queue;
-    std::string *data(new std::string(cass::SoapServer::instance()->get_event(cass::EventParameter(type, t1, t2))));
-    queue.enqueue(data);
-    if(10 < queue.size())
-        queue.dequeue();
-    *success = true;
-    soap_set_dime(this); // enable dime
-    return soap_set_dime_attachment(this, (char *)data->c_str(), data->size(), "application/cassevent",
-                                    QString::number(type).toStdString().c_str(), 0, NULL);
+  /** @todo use shared pointer inside the queue */
+  /** @todo get rid of the functor and do everything here */
+  VERBOSEOUT(cerr << "CASSsoapService::getEvent" << endl);
+  static QQueue<string *> queue;
+  string *data(new string(SoapServer::instance()->get_event(EventParameter(type, t1, t2))));
+  queue.enqueue(data);
+  if(10 < queue.size())
+    queue.dequeue();
+  *success = true;
+  soap_set_dime(this); // enable dime
+  return soap_set_dime_attachment(this, (char *)data->c_str(), data->size(), "application/cassevent",
+                                  QString::number(type).toStdString().c_str(), 0, NULL);
 }
 
-
-
-int CASSsoapService::getHistogram(cass::PostProcessors::key_t type, ULONG64 /*eventId*/, bool *success)
+int CASSsoapService::getHistogram(PostProcessors::key_t type, ULONG64 /*eventId*/, bool *success)
 {
-//    VERBOSEOUT(std::cerr << "CASSsoapService::getHistogram" << std::endl);
-    static QQueue<std::pair<size_t, std::string> *> queue;
-    try {
-        // get data
-        cass::SoapServer::shared_pointer server(cass::SoapServer::instance());
-        std::pair<size_t, std::string> *data(
-            new std::pair<size_t, std::string>(server->get_histogram(cass::HistogramParameter(type))));
-        // MIME type
-        std::string mimetype;
-        switch(data->first) {
-        case 0:
-            mimetype = std::string("application/cass0Dhistogram");
-            break;
-        case 1:
-            mimetype = std::string("application/cass1Dhistogram");
-            break;
-        case 2:
-            mimetype = std::string("application/cass2Dhistogram");
-            break;
-        default:
-            mimetype = std::string("application/casshistogram");
-            break;
-        }
-        // keep bytes around for a while -- this should mitigate the "zeros" problem
-        queue.enqueue(data);
-        if(200 < queue.size())
-            delete queue.dequeue();
-        // answer request
-        *success = true;
-        soap_set_dime(this);
-        return soap_set_dime_attachment(this, (char *)data->second.data(), data->second.size(), mimetype.c_str(),
-                                        type.c_str(), 0, NULL);
-    } catch(cass::InvalidHistogramError) {
-        *success = false;
-        return SOAP_FATAL_ERROR;
+  VERBOSEOUT(cerr << "CASSsoapService::getHistogram" << endl);
+  /** @todo use shared pointer inside the queue */
+  /** @todo get rid of the functor and do everything here */
+  static QQueue<pair<size_t, string> *> queue;
+  try {
+    // get data
+    SoapServer::shared_pointer server(SoapServer::instance());
+    pair<size_t, string> *data(
+          new pair<size_t, string>(server->get_histogram(HistogramParameter(type))));
+    // MIME type
+    string mimetype;
+    switch(data->first) {
+      case 0:
+        mimetype = string("application/cass0Dhistogram");
+        break;
+      case 1:
+        mimetype = string("application/cass1Dhistogram");
+        break;
+      case 2:
+        mimetype = string("application/cass2Dhistogram");
+        break;
+      default:
+        mimetype = string("application/casshistogram");
+        break;
     }
-    catch(cass::InvalidPostProcessorError) {
-        *success = false;
-        return SOAP_FATAL_ERROR;
-    }
+    // keep bytes around for a while -- this should mitigate the "zeros" problem
+    queue.enqueue(data);
+    if(200 < queue.size())
+      delete queue.dequeue();
+    // answer request
+    *success = true;
+    soap_set_dime(this);
+    return soap_set_dime_attachment(this, (char *)data->second.data(), data->second.size(), mimetype.c_str(),
+                                    type.c_str(), 0, NULL);
+  } catch(InvalidHistogramError) {
+    *success = false;
+    return SOAP_FATAL_ERROR;
+  }
+  catch(InvalidPostProcessorError) {
+    *success = false;
+    return SOAP_FATAL_ERROR;
+  }
 }
 
 
--- cass/tcpserver.h
+++ cass/tcpserver.h
@@ -1,6 +1,13 @@
 // CASS TCP server
 //
 // Copyright (C) 2010 Jochen Küpper
+// Copyright (C) 2011 Lutz Foucar
+
+/**
+ * @file tcpserver.h the soap server implementation
+ *
+ * @author Jochen Kuepper
+ */
 
 #ifndef CASS_TCPSERVER_H
 #define CASS_TCPSERVER_H
@@ -10,7 +17,7 @@
 
 #include <QtCore/QThread>
 #include <QtCore/QThread>
-#include <QRunnable>
+#include <QtCore/QRunnable>
 
 #include "cass_event.h"
 #include "event_getter.h"
@@ -18,163 +25,155 @@
 #include "soapCASSsoapService.h"
 
 
-using std::tr1::shared_ptr;
-
 namespace cass
 {
 
-
-/*! Handle a single SOAP request
-
- @todo setup soap, such that while compiling there are not warnings anymore:
-       soapC.cpp:457: warning: dereferencing type-punned pointer will break strict-aliasing rules
-       soapC.cpp:1093: warning: unused parameter 'tt'
-@author Jochen Küpper
-*/
+/** Handle a single SOAP request
+ *
+ * @todo setup soap, such that while compiling there are not warnings anymore:
+ *        soapC.cpp:457: warning: dereferencing type-punned pointer will break strict-aliasing rules
+ *        soapC.cpp:1093: warning: unused parameter 'tt'
+ *
+ * @author Jochen Küpper
+ */
 class SoapHandler : public QRunnable
 {
 public:
+  /** constructor
+   *
+   * @param soap the soap instance to be used be handeld
+   */
+  SoapHandler(CASSsoapService *soap)
+    : _soap(soap)
+  {}
 
-    SoapHandler(CASSsoapService *soap)
-        : _soap(soap)
-        {};
+  /** destructor
+   *
+   * destroys the soap instance that was handelt by this
+   */
+  virtual ~SoapHandler() { delete _soap; }
 
-    virtual ~SoapHandler() { delete _soap; };
-
-    /** handle request and terminate*/
-    virtual void run();
-
+  /** handle request and terminate*/
+  virtual void run();
 
 protected:
-
-    /** the service */
-    CASSsoapService *_soap;
+  /** the service */
+  CASSsoapService *_soap;
 };
 
 
 
 /** SOAP server
-
-@author Jochen Küpper
-
-@todo Update getImage to actually return an direct (i.e., unscaled) TIFF image of the float values
-@todo Provide a multi-content query -- with the SOAP-attachment strategy, we can very well/easily deliver
-multiple histograms/images within one message. Let's do it...(requires a smart updated API).
-*/
+ *
+ * @todo Update getImage to actually return an direct (i.e., unscaled) TIFF
+ *       image of the float values
+ * @todo Provide a multi-content query -- with the SOAP-attachment strategy, we
+ *       can very well/easily deliver multiple histograms/images within one
+ *       message. Let's do it...(requires a smart updated API).
+ *
+ * @author Jochen Küpper
+ */
 class SoapServer : public QThread
 {
-    Q_OBJECT;
-
-    friend class ::CASSsoapService;
-
+  /** be friend with the soap instances */
+  friend class ::CASSsoapService;
 
 public:
-    /** a shared pointer of this class */
-    typedef shared_ptr<SoapServer> shared_pointer;
+  /** a shared pointer of this class */
+  typedef std::tr1::shared_ptr<SoapServer> shared_pointer;
 
-    /** create the instance if not it does not exist already */
-    static shared_pointer instance(const EventGetter& event, const HistogramGetter& hist, size_t port=12321);
-
-    /** destroy the instance */
-    static void destroy();
-
-
-signals:
-
-    void quit();
-
-    void readini(size_t what);
-
-    void writeini(size_t what);
-
-    void clearHistogram(cass::PostProcessors::key_t type);
- 
-    void receiveCommand(cass::PostProcessors::key_t type, std::string);
-
+  /** create the instance if not it does not exist already
+   *
+   * @return the instance to this server
+   * @param event The event getter functor
+   * @param hist The hist getter functor
+   * @param port The port that the soap instance is running on. Default is 12321
+   */
+  static shared_pointer instance(const EventGetter& event,
+                                 const HistogramGetter& hist,
+                                 size_t port=12321);
 
 protected:
+  /** perform thread-work
+   *
+   * Sets up and connects to the soap service then waits until the soap service
+   * get an information. Copies the soap instance and create an new handler to
+   * serve the soap request.
+   *
+   * The handler is put into a QThreadPool which will automatically destroy it
+   * once one is done serving.
+   */
+  virtual void run();
 
-    /** perform thread-work */
-    virtual void run();
+  /** return existing instance for our friends
+   *
+   * if it doesn't exist, throw exception
+   */
+  static shared_pointer instance()
+  {
+    QMutexLocker locker(&_mutex);
+    if(!_instance)
+      throw std::logic_error("SoapServer does not exist");
+    return _instance;
+  }
 
-    /** return existing instance for our friends -- if it doesn't exist, throw exception */
-    static shared_pointer instance() {
-        QMutexLocker locker(&_mutex);
-        if(!_instance)
-            throw std::runtime_error("SoapServer does not exist");
-        return _instance;
-    };
+  /** get_event functor */
+  const EventGetter& get_event;
 
+  /** get_histogram functor */
+  const HistogramGetter& get_histogram;
 
-    /** get_event functor */
-    const EventGetter& get_event;
+  /** the service */
+  CASSsoapService *_soap;
 
-    /** get_histogram functor */
-    const HistogramGetter& get_histogram;
+  /** maximum backlog of open requests */
+  static const size_t _backlog;
 
-    /** allow our friends to emit the quit() signal */
-    void emit_quit() { emit quit(); };
-
-    /** allow our friends to emit the readini() signal */
-    void emit_readini(size_t what) { emit readini(what); };
-
-    /** allow our friends to emit the writeini() signal */
-    void emit_writeini(size_t what) { emit writeini(what); };
-
-    /** allow our friends to emit the clearHistogram() signal */
-    void emit_clearHistogram(cass::PostProcessors::key_t type) { emit clearHistogram(type); };
- 
-    /** allow our friends to emit the receiveCommand() signal */
-    void emit_receiveCommand(cass::PostProcessors::key_t type, std::string command) { emit receiveCommand(type, command); };
-
-    /** the service */
-    CASSsoapService *_soap;
-
-    /** maximum backlog of open requests */
-    static const size_t _backlog;
-
-    /** server port */
-    const size_t _port;
-
+  /** server port */
+  const size_t _port;
 
 private:
+  /** Constructor
+   *
+   * sets up the initial values
+   *
+   * @param event The event getter functor
+   * @param hist The histogram getter functor
+   * @param port The port that the soap will run on
+   * @param parent The Qt parent object that this class belong to
+   */
+  SoapServer(const EventGetter& event, const HistogramGetter& hist, size_t port, QObject *parent=0)
+    : QThread(parent), get_event(event), get_histogram(hist),
+      _soap(new CASSsoapService), _port(port)
+  {
+    VERBOSEOUT(std::cout << "SoapServer starting on port " << _port << std::endl);
+  }
 
-    /** Constructor */
-    SoapServer(const EventGetter& event, const HistogramGetter& hist, size_t port, QObject *parent=0)
-        : QThread(parent), get_event(event), get_histogram(hist),
-          _soap(new CASSsoapService), _port(port)
-        {
-            VERBOSEOUT(std::cout << "SoapServer starting on port " << _port << std::endl);
-        };
+  /** Disabled default constructor */
+  SoapServer();
 
-    /** Disabled constructor */
-    SoapServer();
+  /** Disabled copy constructor */
+  SoapServer(const SoapServer&);
 
-    /** Disabled copy constructor */
-    SoapServer(const SoapServer&);
-
-    /** Disabled assignment */
-    SoapServer& operator=(const SoapServer&);
+  /** Disabled assignment */
+  SoapServer& operator=(const SoapServer&);
 
 public:
-    /** Destructor
-
-    clean up SOAP
-    */
-    ~SoapServer() { _soap->destroy(); delete _soap; };
+  /** Destructor
+   *
+   * clean up SOAP by destroying and deleting the soap instance
+   */
+  ~SoapServer() { _soap->destroy(); delete _soap; }
 
 private:
-    /** pointer to the singleton instance */
-    static shared_pointer _instance;
+  /** pointer to the singleton instance */
+  static shared_pointer _instance;
 
-    /** Singleton operation locker */
-    static QMutex _mutex;
+  /** Singleton operation locker */
+  static QMutex _mutex;
 };
 
-}
-
-
-
+} //end namespace cass
 #endif
 
 
