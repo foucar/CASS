0fc1925183bbdc62d7feeaa189b33ad00ddbf90d
Lutz Foucar <lutz.foucar@asg.mpg.de>
2012-11-03 16:02:08 +0100

    whitespace

--- cass/postprocessing/achimcalibrator_hex.cpp
+++ cass/postprocessing/achimcalibrator_hex.cpp
@@ -25,170 +25,170 @@ using namespace std::tr1;
 
 namespace cass
 {
-  namespace ACQIRIS
+namespace ACQIRIS
+{
+namespace AchimCalibrator
+{
+
+/** write the profile part of calibration data into an ini file
+ *
+ * extract the calibration data from the calibrators and write them into
+ * the requested ini file which will be handled by the QSettings object
+ *
+ * extract the profile data from the layer sum profiles, that later
+ * correct the timesum.
+ *
+ * @param s the QSettings object that handles the .ini file
+ * @param profile the profile that whos stuff needs to be written to the
+ *                .ini file
+ *
+ * @author Lutz Foucar
+ */
+void writeProfileData(QSettings &s, profile_class& profile)
+{
+  const int size (profile.number_of_columns);
+  for (int i = 0; i < size; ++i)
   {
-    namespace AchimCalibrator
-    {
-
-      /** write the profile part of calibration data into an ini file
-       *
-       * extract the calibration data from the calibrators and write them into
-       * the requested ini file which will be handled by the QSettings object
-       *
-       * extract the profile data from the layer sum profiles, that later
-       * correct the timesum.
-       *
-       * @param s the QSettings object that handles the .ini file
-       * @param profile the profile that whos stuff needs to be written to the
-       *                .ini file
-       *
-       * @author Lutz Foucar
-       */
-      void writeProfileData(QSettings &s, profile_class& profile)
-      {
-        const int size (profile.number_of_columns);
-        for (int i = 0; i < size; ++i)
-        {
-          s.setArrayIndex(i);
-          const double pos
-              (profile.get_bin_center_x(static_cast<double>(i)));
-          const double cor (profile.get_y(i));
-          s.setValue("Position",pos );
-          s.setValue("Correction",cor );
-        }
-      }
-
-      /** write all calibration data into an ini file
-       *
-       * use the calibrators to extract the calibration data and then write that
-       * into the .ini file that is handled by a QSettings Object.
-       *
-       * First generate the time sum profiles and write the correction data that
-       * is contained in the profiles to the .ini file.
-       * Then use the created histogram of the detector to get the scalefactors
-       * and the w layer offset.
-       *
-       * @param s the QSettings object that handles the sorter/calibrator .ini
-       *          file
-       * @param tsum_calibrator the timesum calibrator that hold infomration
-       *                        about the timesum calibration data
-       * @param scalefactor_calibrator the scalefactor calibrator that holds
-       *                               information about the scalefactor
-       *                               calibration data.
-       *
-       * @author Lutz Foucar
-       */
-      void writeCalibData(QSettings &s,
-                          shared_ptr<sum_walk_calibration_class> tsum_calibrator,
-                          shared_ptr<scalefactors_calibration_class>  scalefactor_calibrator)
-      {
-        tsum_calibrator->generate_sum_walk_profiles();
-        if (tsum_calibrator->sumu_profile)
-        {
-          s.beginWriteArray("SumUCorrectionPoints");
-          writeProfileData(s,*tsum_calibrator->sumu_profile);
-          s.endArray();
-        }
-        if (tsum_calibrator->sumv_profile)
-        {
-          s.beginWriteArray("SumVCorrectionPoints");
-          writeProfileData(s,*tsum_calibrator->sumv_profile);
-          s.endArray();
-        }
-        if (tsum_calibrator->sumw_profile)
-        {
-          s.beginWriteArray("SumWCorrectionPoints");
-          writeProfileData(s,*tsum_calibrator->sumw_profile);
-          s.endArray();
-        }
-        scalefactor_calibrator->
-            do_auto_calibration(s.value("WLayerOffset",0).toDouble());
-        s.setValue("ScalefactorV",scalefactor_calibrator->best_fv);
-        s.setValue("ScalefactorW",scalefactor_calibrator->best_fw);
-        s.setValue("WLayerOffset",scalefactor_calibrator->best_w_offset);
-      }
-
-      /** shift the values so that the timesum peaks around 0
-       *
-       * @param values The values that need to be shifted
-       * @param sums The sums that they need to be shifted by
-       *
-       * @author Achim Czasch
-       * @author Lutz Foucar
-       */
-      void shift_sum(vector<double> &values, const vector<pair<double,double> > &sums)
-      {
-        int direction(1);
-        const double dpOSumu = direction*sums[HexCalibrator::u].first * 0.5;
-        const double dpOSumv = direction*sums[HexCalibrator::v].first * 0.5;
-        const double dpOSumw = direction*sums[HexCalibrator::w].first * 0.5;
-        values[HexCalibrator::u1] += dpOSumu;
-        values[HexCalibrator::u2] += dpOSumu;
-        values[HexCalibrator::v1] += dpOSumv;
-        values[HexCalibrator::v2] += dpOSumv;
-        values[HexCalibrator::w1] += dpOSumw;
-        values[HexCalibrator::w2] += dpOSumw;
-      }
-
-      /** shift the position
-       *
-       * when the image is not centered around 0 one can use this function to
-       * shift it to 0
-       *
-       * @param layer The vector containing all the layers signals
-       * @param center The center of the image in mm
-       * @param scalefactors The scalefactors for the layers
-       *
-       * @author Achim Czasch
-       * @author Lutz Foucar
-       */
-      void shift_pos(vector<double> &layer,
-                     const pair<double,double> &center,
-                     const vector<double> &scalefactors)
-      {
-        int direction(1);
-        const double offs_u (0.50*center.first/scalefactors[HexCalibrator::u]);
-        const double offs_v
-            (0.25*(center.first - center.second *sqrt(3))/scalefactors[HexCalibrator::v]);
-        const double offs_w
-            (0.25*(center.first + center.second *sqrt(3))/scalefactors[HexCalibrator::w]);
-
-        layer[HexCalibrator::u1] += direction*offs_u;
-        layer[HexCalibrator::u1] -= direction*offs_u;
-        layer[HexCalibrator::v2] += direction*offs_v;
-        layer[HexCalibrator::v2] -= direction*offs_v;
-        layer[HexCalibrator::w1] += direction*offs_w;
-        layer[HexCalibrator::w2] -= direction*offs_w;
-      }
-
-      /** shift the w-layer
-       *
-       * use this function to align the w-layer to the u and v layer.
-       *
-       * @param w1 The first signal on the w-layer
-       * @param w2 The second signal on the w-layer
-       * @param w_offset The offset of the w-layer with respect to the u and v
-       *                 layers
-       *
-       * @author Achim Czasch
-       * @author Lutz Foucar
-       */
-      void shift_wLayer(double &w1, double &w2, const double w_offset)
-      {
-        int direction(1);
-        const double w_offset_shift = direction * w_offset * 0.5;
-        w1 += w_offset_shift;
-        w2 -= w_offset_shift;
-      }
-    }
+    s.setArrayIndex(i);
+    const double pos
+        (profile.get_bin_center_x(static_cast<double>(i)));
+    const double cor (profile.get_y(i));
+    s.setValue("Position",pos );
+    s.setValue("Correction",cor );
   }
 }
+
+/** write all calibration data into an ini file
+ *
+ * use the calibrators to extract the calibration data and then write that
+ * into the .ini file that is handled by a QSettings Object.
+ *
+ * First generate the time sum profiles and write the correction data that
+ * is contained in the profiles to the .ini file.
+ * Then use the created histogram of the detector to get the scalefactors
+ * and the w layer offset.
+ *
+ * @param s the QSettings object that handles the sorter/calibrator .ini
+ *          file
+ * @param tsum_calibrator the timesum calibrator that hold infomration
+ *                        about the timesum calibration data
+ * @param scalefactor_calibrator the scalefactor calibrator that holds
+ *                               information about the scalefactor
+ *                               calibration data.
+ *
+ * @author Lutz Foucar
+ */
+void writeCalibData(QSettings &s,
+                    shared_ptr<sum_walk_calibration_class> tsum_calibrator,
+                    shared_ptr<scalefactors_calibration_class>  scalefactor_calibrator)
+{
+  tsum_calibrator->generate_sum_walk_profiles();
+  if (tsum_calibrator->sumu_profile)
+  {
+    s.beginWriteArray("SumUCorrectionPoints");
+    writeProfileData(s,*tsum_calibrator->sumu_profile);
+    s.endArray();
+  }
+  if (tsum_calibrator->sumv_profile)
+  {
+    s.beginWriteArray("SumVCorrectionPoints");
+    writeProfileData(s,*tsum_calibrator->sumv_profile);
+    s.endArray();
+  }
+  if (tsum_calibrator->sumw_profile)
+  {
+    s.beginWriteArray("SumWCorrectionPoints");
+    writeProfileData(s,*tsum_calibrator->sumw_profile);
+    s.endArray();
+  }
+  scalefactor_calibrator->
+      do_auto_calibration(s.value("WLayerOffset",0).toDouble());
+  s.setValue("ScalefactorV",scalefactor_calibrator->best_fv);
+  s.setValue("ScalefactorW",scalefactor_calibrator->best_fw);
+  s.setValue("WLayerOffset",scalefactor_calibrator->best_w_offset);
+}
+
+/** shift the values so that the timesum peaks around 0
+ *
+ * @param values The values that need to be shifted
+ * @param sums The sums that they need to be shifted by
+ *
+ * @author Achim Czasch
+ * @author Lutz Foucar
+ */
+void shift_sum(vector<double> &values, const vector<pair<double,double> > &sums)
+{
+  int direction(1);
+  const double dpOSumu = direction*sums[HexCalibrator::u].first * 0.5;
+  const double dpOSumv = direction*sums[HexCalibrator::v].first * 0.5;
+  const double dpOSumw = direction*sums[HexCalibrator::w].first * 0.5;
+  values[HexCalibrator::u1] += dpOSumu;
+  values[HexCalibrator::u2] += dpOSumu;
+  values[HexCalibrator::v1] += dpOSumv;
+  values[HexCalibrator::v2] += dpOSumv;
+  values[HexCalibrator::w1] += dpOSumw;
+  values[HexCalibrator::w2] += dpOSumw;
+}
+
+/** shift the position
+ *
+ * when the image is not centered around 0 one can use this function to
+ * shift it to 0
+ *
+ * @param layer The vector containing all the layers signals
+ * @param center The center of the image in mm
+ * @param scalefactors The scalefactors for the layers
+ *
+ * @author Achim Czasch
+ * @author Lutz Foucar
+ */
+void shift_pos(vector<double> &layer,
+               const pair<double,double> &center,
+               const vector<double> &scalefactors)
+{
+  int direction(1);
+  const double offs_u (0.50*center.first/scalefactors[HexCalibrator::u]);
+  const double offs_v
+      (0.25*(center.first - center.second *sqrt(3))/scalefactors[HexCalibrator::v]);
+  const double offs_w
+      (0.25*(center.first + center.second *sqrt(3))/scalefactors[HexCalibrator::w]);
+
+  layer[HexCalibrator::u1] += direction*offs_u;
+  layer[HexCalibrator::u1] -= direction*offs_u;
+  layer[HexCalibrator::v2] += direction*offs_v;
+  layer[HexCalibrator::v2] -= direction*offs_v;
+  layer[HexCalibrator::w1] += direction*offs_w;
+  layer[HexCalibrator::w2] -= direction*offs_w;
+}
+
+/** shift the w-layer
+ *
+ * use this function to align the w-layer to the u and v layer.
+ *
+ * @param w1 The first signal on the w-layer
+ * @param w2 The second signal on the w-layer
+ * @param w_offset The offset of the w-layer with respect to the u and v
+ *                 layers
+ *
+ * @author Achim Czasch
+ * @author Lutz Foucar
+ */
+void shift_wLayer(double &w1, double &w2, const double w_offset)
+{
+  int direction(1);
+  const double w_offset_shift = direction * w_offset * 0.5;
+  w1 += w_offset_shift;
+  w2 -= w_offset_shift;
+}
+}//end namespace achimcalibrator
+}//end namespace acqiris
+}//end namespace cass
 
 
 HexCalibrator::HexCalibrator(PostProcessors &pp, const PostProcessors::key_t &key)
   :PostprocessorBackend(pp,key),
-   _timesums(3,make_pair(0,0)),
-   _scalefactors(2,1)
+    _timesums(3,make_pair(0,0)),
+    _scalefactors(2,1)
 {
   loadSettings(0);
 }
