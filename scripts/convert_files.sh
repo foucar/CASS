a73b75c24c1bbed7f9c21cca8366ed19a00ec92d
Lutz Foucar <Lutz.Foucar@asg.mpg.de>
2012-11-28 08:56:33 -0800

    added functionality to script to monitor and recursively call itself and some more improvements

--- scripts/convert_files.sh
+++ scripts/convert_files.sh
@@ -8,7 +8,7 @@
 # config
 
 # the diretory where the analysis is taken place
-BASEDIR=/reg/d/ana12/cxi/cxii0512/scratch/cass
+BASEDIR=/reg/d/psdm/cxi/cxii0512/scratch/cass
 
 # the dir where all the .ini for the runs will be placed
 INIDIR=$BASEDIR/ini
@@ -31,8 +31,14 @@ OUTPUTDIR=$BASEDIR/hdf5
 # the dir where the jobs output will be written to
 JOBDIR=$BASEDIR/jobs
 
-# sting the precedes the runnumber
+# string the precedes the runnumber
 FILEBASENAME=e220-r0
+
+# string that indicates that the data file cannot be read yet
+PROGRESSSUFFIX=inprogress
+
+# name of the file that contains the runnumbers that have been processed
+PROCESSEDRUNSFILE=$BASEDIR/processedRuns.txt
 
 # the ini file that works as template for the runs
 INIRUNTEMPLATE=$INIDIR/Run.ini
@@ -66,7 +72,48 @@ OUTPUTEXT=h5
 
 
 
-# check whether the run has been anlyzed
+
+
+# check if run number is in file
+
+check_runnumber_in_file()
+{
+  runnbr=$1
+  filename=$2
+  for number in `cat "$filename"`
+  do
+    if [ "$runnbr" -eq "$number" ]
+    then
+      return 1
+    fi
+  done
+  return 0
+}
+
+
+
+# check if a directoy had contents added to it (just an example)
+
+wait_until_contents_added_to_dir()
+{
+  dir="$1"
+  sum1=`find "$dir" -maxdepth 1 -type f -print | wc -l`
+  sum2=$sum1
+
+#    chsum1=`digest -a md5 $dir | awk '{print $1}'`
+
+  while [ $sum1 -eq $sum2 ]
+  do
+    sleep 10
+    sum2=`find "$dir" -maxdepth 1 -type f -print | wc -l`
+  done
+
+ #eval $2
+}
+
+
+
+# check whether the run has been anlyzed (obsolete)
 
 check_analyzed()
 {
@@ -81,21 +128,108 @@ check_analyzed()
 
 
 
-# check whether the run is in the queue right now
+# check if a file has changed contents, if so call this script with the monitored
+# file as argument
 
-check_in_queue()
+monitor_file()
 {
-  jobname=$1
-  echo "check if $jobname is in the queue"
-  retval=0
-  for line in `$QUERYCOMMAND`
+  echo "Monitoring file $1 for changes. If changes occur calls \"sh $0 $1\""
+  file="$1"
+  chsum1=`md5sum "$file" | awk '{print $1}'`
+  chsum2=$chsum1
+  while [[ TRUE ]]
   do
-    if [[ "$line" == *"$jobname"* ]]
+    chsum1=`md5sum "$file" | awk '{print $1}'`
+    while [ "$chsum1" == "$chsum2" ]
+    do
+      sleep 10
+      chsum2=`md5sum "$file" | awk '{print $1}'`
+      echo ""$chsum1" "$chsum2""
+    done
+    echo "call \"sh $0 $1\""
+    sh ./$0 $1
+  done
+}
+
+
+
+# check if run number is in file
+
+check_runnumber_in_file()
+{
+  runnbr=$1
+  filename=$2
+  for number in `cat "$filename"`
+  do
+    if [ "$runnbr" -eq "$number" ]
     then
-      retval=1
+      return 1
     fi
   done
-  return $retval
+  return 0
+}
+
+
+
+# check how many slices exist for a run
+
+find_amount_of_slices_for_run()
+{
+  greatestslice=-1
+  for line in `ls $1*`;
+  do
+    slice=$(echo "$line" | cut -d"-" -f3 | cut -c 2-3)
+    if [ "$slice" -gt "$greatestslice" ]
+    then
+      greatestslice=$slice
+    fi
+  done
+  return $greatestslice
+}
+
+
+
+# check if files with a base name do not exist or are still marked the the
+# progress suffix
+
+check_files_not_exists()
+{
+  filebasename=$1
+  sumall=`ls $filebasenme* 2> /dev/null | wc -l`
+  sumprogress=`ls $filebasenme* 2> /dev/null | grep "$PROGRESSSUFFIX" | wc -l`
+  if [ "$sumall" -eq  0 ]
+  then
+    echo "files with basename $filebasename do not exist.";
+    return 1;
+  fi
+  if [ $sumprogress -ne 0 ]
+  then
+    echo "files with basename $filebasename exist, but some contain the $PROGRESSUFFIX extension"
+    return 1;
+  fi
+  return 0;
+}
+
+
+
+# wait until the files with a basename exist
+
+wait_until_files_exist()
+{
+  check_files_not_exists $1
+  fileDoesNotExist=$?
+  fileDoesNotExistInBeginning=$fileDoesNotExist
+  while [ "$fileDoesNotExist" -eq 1 ]
+  do
+    echo "Files with basename $1 do not exist. Wait a little and check again"
+    check_file_not_exists $1
+    fileDoesNotExist=$?
+    sleep 5
+  done
+  if [ "$fileDoesNotExistInBeginning" -eq 1 ]
+  then
+    echo "Files with basename $1 exist now"
+  fi
 }
 
 
@@ -126,40 +260,6 @@ check_darkcal_not_exists()
 
 
 
-# wait until the jobs appears in the list of jobs submitted to the queue
-
-wait_until_submitted()
-{
-  in_queue=0
-  while [ "$in_queue" -eq 0 ]
-  do
-    check_in_queue $1
-    in_queue=$?
-    sleep 5
-  done
-  echo "Job $1 is now submitted"
-}
-
-
-
-# check how many slices exist for a run
-
-find_amount_of_slices_for_run()
-{
-  greatestslice=0
-  for line in `ls $1*`;
-  do
-    slice=$(echo "$line" | cut -d"-" -f3 | cut -c 2-3)
-    if [ "$slice" -gt "$greatestslice" ]
-    then
-      greatestslice=$slice
-    fi
-  done
-  return $greatestslice
-}
-
-
-
 # wait unit the darkcal is created
 
 wait_until_darkcal_created()
@@ -172,6 +272,84 @@ wait_until_darkcal_created()
     sleep 5
   done
   echo "Darkcalfiles $1 and $2 do exist now"
+}
+
+
+
+# check whether the run is in the queue right now
+
+check_in_queue()
+{
+  jobname=$1
+#  echo "check if $jobname is in the queue"
+  retval=0
+  for line in `$QUERYCOMMAND`
+  do
+    if [[ "$line" == *"$jobname"* ]]
+    then
+      retval=1
+    fi
+  done
+  return $retval
+}
+
+
+
+# wait until the jobs appears in the list of jobs submitted to the queue
+
+wait_until_submitted()
+{
+  check_in_queue $1
+  in_queue=$?
+  in_queue_at_beginning=$in_queue
+  while [ "$in_queue" -eq 0 ]
+  do
+    echo "Job $1 is not submitted. Wait and then check again"
+    check_in_queue $1
+    in_queue=$?
+    sleep 5
+  done
+  if [ "$in_queue_at_beginning" -eq 0 ]
+  then
+    echo "Job $1 is now submitted"
+  fi
+}
+
+
+
+#check if file does not exist
+
+check_file_not_exists()
+{
+  filename=$1
+  if [ ! -f "$filename" ]
+  then
+    echo "$filename does not exist.";
+    return 1;
+  fi
+  return 0;
+}
+
+
+
+# wait until a file exists
+
+wait_until_file_exists()
+{
+  check_file_not_exists $1
+  fileDoesNotExist=$?
+  fileDidNotExistInBeginning=$fileDidNotExistInBeginning
+  while [ "$fileDoesNotExist" -eq 1 ]
+  do
+    echo "File $1 does not exist yet. Wait and check again"
+    check_file_not_exists $1
+    fileDoesNotExist=$?
+    sleep 5
+  done
+  if [ "$fileDidNotExistInBeginning" -eq 1 ]
+  then
+    echo "File $1 exists now"
+  fi
 }
 
 
@@ -203,21 +381,27 @@ test()
 
 # ------------------------- main ---------------------------------
 
+if [ "$2" == "-m" ]
+then
+  monitor_file "$1"
+fi
+
 
 # parameter 1 should contain the file that links run numbers to the corresponding
 # darkcal run
-
-
 
 # check if at least a file that contains the runs an the corresponding darkcal
 # runs has been given
 if [ -z "$1" ]
 then
   echo "No file containing the run numbers $1 to process has been given";
+  echo "Usage: "$0" datafile-darkfile_assiciation txt file"
+  echo "optional parameter -m lets this script monitor the datafile-darkfile_assiciation txt file for changes and then calls this script recursively"
 else
   # if the file containing the run numbers has been provided read it line by line
   # assuming that the first is the runnumber and the second is the corresponding
   # darkcalrun number
+  echo "processing runs-darkcals in file $1"
   cat $1 | while read runnbr darkcalrunnbr;
   do
     # ignore lines that contain a '#'
@@ -225,15 +409,33 @@ else
     then
       echo "line containing $runnbr will be ignored";
     else
+      # find out whether this run has already been processed, by checking if the
+      # runnumber is in the file that contains the processed runs. If so skip
+      # this run
+      check_runnumber_in_file $runnbr $PROCESSEDRUNSFILE
+      return_val=$?
+      if [ "$return_val" -eq 1 ]
+      then
+        echo "Run $runnbr has already been processed. Skipping it"
+        continue
+      fi
+
       #define the names that the darkcals should have
       darkcal0Filename=$DARKCALDIR/darkcal_run"$darkcalrunnbr"_7.cal
       darkcal1Filename=$DARKCALDIR/darkcal_run"$darkcalrunnbr"_8.cal
 
       # now check whether a darkcal for the darkcal run already exists
-      check_darkcal_not_exists $darkcal0Filename $darkcal1Filename
+#      check_darkcal_not_exists $darkcal0Filename $darkcal1Filename
+      check_file_not_exists $darkcal0Filename
       return_val=$?
-      if [ "$return_val" -eq 1 ]
+#      check_file_not_exists $darkcal1Filename
+#      return_val= $? + "$return_val"
+      if [ "$return_val" -ne 0 ]
       then
+        # wait until all the data files exist if necessary
+        darkcalDataFilesBaseName=$DATAFILESDIR/$FILEBASENAME$darkcalrunnbr
+        wait_until_files_exist $darkcalDataFilesBaseName
+
         # when the darkrun needs to be created first, create the txt file
         # containing the datafiles for the darkcal run, the ini and the sh
         # file to submit it to the queue.
@@ -266,18 +468,30 @@ else
         # the darkcals are created
 #        echo $SUBMITCOMMAND $jobname $shFilename #!!!
         $SUBMITCOMMAND -o $joboutput -J $jobname $shFilename #!!!
-        wait_until_darkcal_created $darkcal0Filename $darkcal1Filename
+#        wait_until_darkcal_created $darkcal0Filename $darkcal1Filename
+        wait_until_file_exists $darkcal0Filename
+#        wait_until_file_exists $darkcal1Filename
       fi
 
+      # wait until all the data files exist if necessary
+      DataFilesBaseName=$DATAFILESDIR/$FILEBASENAME$runnbr
+      wait_until_files_exist $DataFilesBaseName
+
       # at this point the darkcal for the run we want to analyze has been created
-      # now find out into how many slices the run has been sliced.
-      find_amount_of_slices_for_run $DATAFILESDIR/$FILEBASENAME$runnbr
+      # now find out into how many slices the run has been sliced. If there are
+      # any slices, write the runnumber into the processed runs file. Then go
+      # through all slices
+      find_amount_of_slices_for_run $DataFilesBaseName
       nbrSlices=$?
+      if [ "$return_val" -ne -1 ]
+      then
+        echo ""$runnbr"" >> $PROCESSEDRUNSFILE
+      fi
       for ((slice=0; slice<=$nbrSlices; ++slice ))
       do
 
         #check if slice exists, if not continue with the next slice
-        if [ ! -f "$DATAFILESDIR/$FILEBASENAME$runnbr-s0$slice-c00.xtc" ]
+        if [ ! -f "$DataFilesBaseName-s0$slice-c00.xtc" ]
         then
           echo "slice $slice does not exist, skipping it"
           continue
@@ -315,8 +529,11 @@ else
 
         # now submit job to the queue and wait until its submitted
 #        echo $SUBMITCOMMAND $jobname $shFilename
+        echo "Submitting $jobname"
         $SUBMITCOMMAND -o $joboutput -J $jobname $shFilename
-        wait_until_submitted $jobname
+        # what is the reason to wait until the job is submitted? Disabling this
+        # for now.
+#        wait_until_submitted $jobname
       done
     fi
   done
