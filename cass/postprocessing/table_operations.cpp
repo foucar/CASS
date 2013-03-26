49dbe58081b7c8ca8e0d506eb9ce26fedf908787
Lutz Foucar <lutz.foucar@asg.mpg.de>
2013-03-26 10:55:44 +0100

    moved pp72 to new location

--- cass/postprocessing/table_operations.cpp
+++ cass/postprocessing/table_operations.cpp
@@ -9,6 +9,69 @@
 
 #include "table_operations.h"
 
+#include "log.h"
+#include "cass_settings.h"
+
 using namespace cass;
 using namespace std;
+
+
+
+// ***  pp 72 returns column of a table ***
+
+pp72::pp72(PostProcessors& pp, const cass::PostProcessors::key_t &key)
+  : PostprocessorBackend(pp, key)
+{
+  loadSettings(0);
+}
+
+void pp72::loadSettings(size_t)
+{
+  CASSSettings s;
+  s.beginGroup("PostProcessor");
+  s.beginGroup(QString::fromStdString(_key));
+  setupGeneral();
+  _table = setupDependency("TableName");
+  bool ret (setupCondition());
+  if (!(ret && _table))
+    return;
+  _colIdx = s.value("ColumnIndex",0).toUInt();
+
+  size_t maxIdx(_table->getHist(0).axis()[HistogramBackend::xAxis].size());
+  if (_colIdx >= maxIdx)
+    throw runtime_error("pp72::loadSettings(): '" + _key + "' The requested " +
+                        "column index '" + toString(_colIdx) + " 'exeeds the " +
+                        "maximum possible index value '" + toString(maxIdx) + "'");
+
+  _result = new Histogram1DFloat();
+  createHistList(2*cass::NbrOfWorkers);
+  Log::add(Log::INFO,"PostProcessor '" + _key +
+           "' retrieves column with index '" + toString(_colIdx) +
+           "' from table " + _table->key() + "' .Condition on postprocessor '" +
+           _condition->key() + "'");
+}
+
+void pp72::process(const cass::CASSEvent& evt)
+{
+  const Histogram2DFloat& table
+      (dynamic_cast<const Histogram2DFloat&>((*_table)(evt)));
+  const HistogramFloatBase::storage_t &tableContents(table.memory());
+  Histogram1DFloat &col(dynamic_cast<Histogram1DFloat&>(*_result));
+
+  table.lock.lockForRead();
+  col.lock.lockForWrite();
+
+  col.clearline();
+
+  size_t nCols(table.axis()[HistogramBackend::xAxis].size());
+  size_t nRows(table.axis()[HistogramBackend::yAxis].size());
+
+  for (size_t row=0; row < nRows; ++row)
+    col.append(tableContents[row*nCols + _colIdx]);
+
+  col.nbrOfFills()=1;
+  col.lock.unlock();
+  table.lock.unlock();
+}
+
 
--- cass/postprocessing/table_operations.h
+++ cass/postprocessing/table_operations.h
@@ -16,5 +16,45 @@
 namespace cass
 {
 
+/** get specific column from table like histogram
+ *
+ * Will copy all contents of the input tables user specified column into a
+ * 1d histogram.
+ *
+ * @see PostprocessorBackend for a list of all commonly available cass.ini
+ *      settings.
+ *
+ * @cassttng PostProcessor/\%name\%/{TableName} \n
+ *           name of postprocessor that contains the table like histogram
+ *           subset from. Default is "".
+ * @cassttng PostProcessor/\%name\%/{ColumnIndex} \n
+ *           The index of the column in the table that one wants to have
+ *           extracted. Please refer to the PostProcessor description of the
+ *           PostProcessor that  contains the table to find out what column
+ *           indizes are available.
+ *           Default is "0".
+ *
+ * @author Lutz Foucar
+ */
+class pp72 : public PostprocessorBackend
+{
+public:
+  /** constructor */
+  pp72(PostProcessors& hist, const PostProcessors::key_t&);
+
+  /** process event */
+  virtual void process(const CASSEvent&);
+
+  /** load the settings of the pp */
+  virtual void loadSettings(size_t);
+
+protected:
+  /** pp containing input table */
+  PostprocessorBackend *_table;
+
+  /** offset of first bin in input in Histogram coordinates */
+  size_t _colIdx;
+};
+
 }//end namespace cass
 #endif
