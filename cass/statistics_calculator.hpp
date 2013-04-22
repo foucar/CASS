b68f4c39ab5e17fb167f5e1b8af311cf60817cd7
Lutz Foucar <lutz.foucar@asg.mpg.de>
2013-04-22 20:16:09 +0200

    added statistics class that uses the cummulative statistics to create a mean and stdv that are free of outliers

--- cass/statistics_calculator.hpp
+++ cass/statistics_calculator.hpp
@@ -114,6 +114,144 @@ private:
 
 
 
+/** statistics calculator for a cummulative statistic, removes outliers
+ *
+ * class uses the CummulativeStatisticsCalculator to calculate the mean and
+ * stdv, but removes the outliers from the statistics.
+ *
+ * @tparam type of the values for the average, defines the precision
+ *
+ * @author Lutz Foucar
+ */
+template <typename Type>
+class CummulativeStatisticsNoOutlier
+{
+public:
+  /** define the value type */
+  typedef Type value_type;
+
+  /** define the statistics */
+  typedef CummulativeStatisticsCalculator<value_type> statistics_t;
+
+  /** define the type of container used for the values */
+  typedef std::vector<value_type> container_type;
+
+  /** define a const iterator of the container  */
+  typedef typename container_type::const_iterator const_iterator_t;
+
+  /** constructor sets the signal to noise ratio
+   *
+   * @param snr the signal to noise ratio to detect outliers
+   */
+  CummulativeStatisticsNoOutlier(const value_type &snr)
+    : _snr(snr)
+  {}
+
+  /** add a datum to the container
+   *
+   * @param datum The datum to be added
+   */
+  void addDatum(const value_type & datum)
+  {
+    _container.push_back(datum);
+    _containerChanged=true;
+  }
+
+  /** retrieve the mean  of the distribution without outliers
+   *
+   * @return mean of the distribution
+   */
+  value_type mean()
+  {
+    updateStat();
+    return _mean;
+  }
+
+  /** retrieve the standart deviation of the distribution
+   *
+   * @return standart deviation of the distribution
+   */
+  value_type stdv()
+  {
+    updateStat();
+    return _stdv;
+  }
+
+  /** reset the container */
+  void reset()
+  {
+    _container.clear();
+    _containerChanged = true;
+  }
+
+private:
+  /** update the statistic values
+   *
+   * first calculate the mean and stdv of the container. Then remove
+   * outliers and reacalculate the mean and stdv. Do this until no outliers are
+   * present in the remaining distribution.
+   *
+   * @return mean without outliers of the distribution
+   */
+  void updateStat()
+  {
+    using namespace std;
+
+
+    if (!_containerChanged)
+      return;
+
+    container_type c(_container);
+    sort(c.begin(),c.end());
+    const_iterator_t lowPos(c.begin());
+    const_iterator_t upPos(c.end());
+
+    bool outliersdetected(true);
+    while (outliersdetected)
+    {
+      _stat.reset();
+      const_iterator_t val(lowPos);
+      const_iterator_t End(upPos);
+      while (val != End)
+        _stat.addDatum(*val++);
+
+      const value_type lowBound(_stat.mean() - _snr * _stat.stdv());
+      const value_type upBound(_stat.mean() + _snr * _stat.stdv());
+      const_iterator_t newLowPos(lower_bound(c.begin(), c.end(), lowBound));
+      const_iterator_t newUpPos(upper_bound (c.begin(), c.end(), upBound));
+
+      if ( newLowPos == lowPos && newUpPos == upPos)
+        outliersdetected = false;
+
+      lowPos = newLowPos;
+      upPos = newUpPos;
+
+      _mean = _stat.mean();
+      _stdv = _stat.stdv();
+    }
+
+    _containerChanged = false;
+  }
+
+  /** the statistics calculator */
+  statistics_t _stat;
+
+  /** container to store the values in */
+  container_type _container;
+
+  /** the mean without outliers */
+  Type _mean;
+
+  /** the stdv without outliers */
+  Type _stdv;
+
+  /** flag to show whether the statistic values have to be updated */
+  bool _containerChanged;
+
+  /** the signal to noise ratio that will detectect outliers */
+  Type _snr;
+};
+
 
 /** statistics calculator for a exponential moving statistics
  *
