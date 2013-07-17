afa5e99b2b7571150ccf1078b94e7324977c39db
Lutz Foucar <lutz.foucar@asg.mpg.de>
2013-07-17 17:10:05 +0200

    added missing settings for the new make file environment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,19 +8,18 @@ BASEDIR=$1
 #BASEDIR=/path/to/CASS/base/directory
 
 # the location where CASS should be installed to
-INSTLOC=/lfs/l3/asg
+INSTLOC=/lfs/l3/asg/cass/v$(env -i git describe --abbrev=4)
 
 
 # set the environment variables
 export ROOTSYS=/lfs/l3/asg/root_v5.32.00
-export QTDIR=/usr/local/Packages/qt-4.6.1
+export QTDIR=/lfs/l3/asg/qt/4.8.2
 export HDF5DIR=/lfs/l3/asg
 export GSOAPDIR=/lfs/l3/asg
+export QWTDIR=/lfs/l3/asg
+export VIGRADIR=/lfs/l3/asg
 
-export PATH=$ROOTSYS/bin:$QTDIR/bin:$HDF5DIR/bin:$GSOAPDIR/bin:$PATH
-export CPLUS_INCLUDE_PATH=$QTDIR/include:$GSOAPDIR/include:$HDF5DIR/include:
-export LIBRARY_PATH=$GSOAPDIR/lib:$HDF5DIR/lib:$LIBRARY_PATH
-export LD_LIBRARY_PATH=$ROOTSYS/lib:$LD_LIBRARY_PATH
+export PATH=$ROOTSYS/bin:$QTDIR/bin:$GSOAPDIR/bin:$PATH
 
 
 
@@ -32,12 +31,34 @@ export LD_LIBRARY_PATH=$ROOTSYS/lib:$LD_LIBRARY_PATH
 # Since this script should automatically install CASS we need to modify the 
 # cass_defaultconfig.pri file from within this script. We add all options that
 # we want to have enabled just at the end of the file.
-echo "CONFIG += hdf5"               >> $BASEDIR/cass_defaultconfig.pri
-echo "CONFIG += singleparticle_hit" >> $BASEDIR/cass_defaultconfig.pri
-echo "CONFIG += cernroot"           >> $BASEDIR/cass_defaultconfig.pri
-echo "CONFIG += JoCASSView"         >> $BASEDIR/cass_defaultconfig.pri
-echo "CONFIG += LuCASSView"         >> $BASEDIR/cass_defaultconfig.pri
+echo "CONFIG += hdf5"                         >> ${BASEDIR}/cass_defaultconfig.pri
+echo "QMAKE_INCDIR   += "${HDF5DIR}"/include" >> ${BASEDIR}/cass_defaultconfig.pri
+echo "QMAKE_LIBDIR   += "${HDF5DIR}"/lib"     >> ${BASEDIR}/cass_defaultconfig.pri
+echo "QMAKE_RPATHDIR += "${HDF5DIR}"/lib"     >> ${BASEDIR}/cass_defaultconfig.pri
+
+echo "CONFIG += singleparticle_hit"          >> ${BASEDIR}/cass_defaultconfig.pri
+echo "QMAKE_INCDIR += "${VIGRADIR}"/include" >> ${BASEDIR}/cass_defaultconfig.pri
+
+echo "CONFIG += cernroot"                     >> ${BASEDIR}/cass_defaultconfig.pri
+echo "QMAKE_INCDIR   += "${ROOTSYS}"/include" >> ${BASEDIR}/cass_defaultconfig.pri
+echo "QMAKE_LIBDIR   += "${ROOTSYS}"/lib"     >> ${BASEDIR}/cass_defaultconfig.pri
+echo "QMAKE_RPATHDIR += "${ROOTSYS}"/lib"     >> ${BASEDIR}/cass_defaultconfig.pri
+
+echo "CONFIG += JoCASSView"                  >> ${BASEDIR}/cass_defaultconfig.pri
+echo "QMAKE_INCDIR   += "${QWTDIR}"/include" >> ${BASEDIR}/cass_defaultconfig.pri
+echo "QMAKE_LIBDIR   += "${QWTDIR}"/lib"     >> ${BASEDIR}/cass_defaultconfig.pri
+echo "QMAKE_RPATHDIR += "${QWTDIR}"/lib"     >> ${BASEDIR}/cass_defaultconfig.pri
+
+echo "CONFIG += LuCASSView"                  >> ${BASEDIR}/cass_defaultconfig.pri
+
+
+#QMAKE_CXXFLAGS += -fopenmp
+#QMAKE_LFLAGS   += -fopenmp
+#DEFINES        += _GLIBCXX_PARALLEL   #enables openmp support for gcc stdlibc++
+
+
+
 
 # make a clean build of the binaries and install them in the right location for
 # the users to run the latest stable version of CASS
-cd $BASEDIR && qmake INSTALLBASE=$INSTLOC && make && make install
+cd ${BASEDIR} && qmake PREFIX=${INSTLOC} && make && make install
