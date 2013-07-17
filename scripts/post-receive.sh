b6396f722092ee4b8f62735d50603f6bab6d55ad
Lutz Foucar <lutz.foucar@asg.mpg.de>
2013-07-17 17:42:43 +0200

    updated post receive script

--- scripts/post-receive.sh
+++ scripts/post-receive.sh
@@ -11,12 +11,12 @@ TMP_REPO=$HOME/tmp/diode
 # check whether a commit was pushed to the master branch if so start the deploy-skript
 while read oldrev newrev refname
 do
-    if [ "$refname" = "refs/heads/master" ] ; then
+    if [ "${refname}" = "refs/heads/master" ] ; then
       echo "Branch master has changed; deploying website and binaries:"
-      git clone $GIT_REPO $TMP_REPO
-      $TMP_REPO/scripts/createDoc.sh $TMP_REPO
-      $TMP_REPO/scripts/deploy.sh $TMP_REPO
-      rm -rf $TMP_REPO
+      git clone ${GIT_REPO} ${TMP_REPO}
+      ${TMP_REPO}/scripts/deployMPIK.sh ${TMP_REPO}
+      ${TMP_REPO}/scripts/setupMPIKWebpage.sh ${TMP_REPO}
+      rm -rf ${TMP_REPO}
     else
       echo "Branch master has not been changed"
     fi
