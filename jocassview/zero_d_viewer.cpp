2582b7f367859b4c1e29d43cd15bf55737961151
Lutz Foucar <lutz.foucar@asg.mpg.de>
2013-12-21 13:50:37 +0100

    added stub for 0d data

---
+++ jocassview/zero_d_viewer.cpp
@@ -0,0 +1,21 @@
+// Copyright (C) 2013 Lutz Foucar
+
+/**
+ * @file zero_d_viewer.cpp contains viewer for 0d data
+ *
+ * @author Lutz Foucar
+ */
+
+#include "zero_d_viewer.h"
+
+using namespace jocassview;
+
+ZeroDViewer::ZeroDViewer(QWidget *parent)
+{
+
+}
+
+void ZeroDViewer::setData(cass::Histogram0DFloat *histogram)
+{
+
+}
