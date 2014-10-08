183e15e4f1190e2ba27876504c523eb9b6d09f5f
Lutz Foucar <lutz.foucar@mpimf-heidelberg.mpg.de>
2014-10-08 10:45:26 +0200

    added script to download the necessary dependencies on ubuntu, that are not part of the INSTALL.md file

---
+++ scripts/ubuntu-prequisites.sh
@@ -0,0 +1,19 @@
+sudo apt-get install \
+dpkg-dev \
+make \
+g++ \
+gcc \
+binutils \
+build-essential \
+bison \
+libssl-dev \
+libx11-dev \
+libxext-dev \
+libxpm-dev \
+libxft-dev \
+git \
+flex \
+autoconf \
+texlive-latex-base \
+
+graphviz
