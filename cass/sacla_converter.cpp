7e564e427af529e4493b2c23a82b5feb54b7b02a
Lutz Foucar <lutz.foucar@mpimf-heidelberg.mpg.de>
2014-10-28 10:07:46 +0100

    added retrieval of additional data, made normalization an option

--- cass/sacla_converter.cpp
+++ cass/sacla_converter.cpp
@@ -29,26 +29,34 @@ using namespace std;
  * @tparam T the type of the detector data
  *
  * @return size of the retrieved data
- * @param det
- * @param detName
- * @param blNbr
- * @param highTagNbr
- * @param tagNbr
+ * @param det reference to the detector that should be filled with data
+ * @param md reference to the machine data. Needed for additional det information
+ * @param detName Name of the ocatal detector (tile names will be deferred from it)
+ * @param normalize flag to tell whether individual tiles should be normalized
+ *                  to the first tile
+ * @param blNbr the Beamline number of the experiment
+ * @param highTagNbr the high tag number for the experiment
+ * @param tagNbr the tag number that is associated with the requested data
  */
 template <typename T>
-uint64_t retrieveOctal(pixeldetector::Detector &det, string detName,
-                   int blNbr, int highTagNbr, int tagNbr)
+uint64_t retrieveOctal(pixeldetector::Detector &det, MachineDataDevice &md,
+                       string detName, bool normalize,
+                       int blNbr, int highTagNbr, int tagNbr)
 {
-  vector<T> buffer;
-  det.frame().resize(512*1024*8);
-  det.columns() = 512;
-  det.rows() = 8*1024;
+  det.frame().clear();
 
-  float gainRef=0;
+  vector<T> buffer;
+  float gainRef(0);
+  uint64_t datasize(0);
+
+  /** go through all 8 detector tiles of the octal MPCCD */
   for (int i=0; i<8 ; ++i)
   {
+    /** generate the name of the current detector tile */
     string detTileName(detName + "-" + toString(i+1));
-    int width=0, height=0, funcstatus=0;
+
+    /** get the height and the width of the detector */
+    int width(0), height(0), funcstatus(0);
     funcstatus = ReadXSizeOfDetData(width,detTileName.c_str(), blNbr, highTagNbr, tagNbr);
     if (funcstatus)
     {
@@ -65,15 +73,73 @@ uint64_t retrieveOctal(pixeldetector::Detector &det, string detName,
                "' ErrorCode is '" + toString(funcstatus) + "'");
       return 0;
     }
-    buffer.resize(width*height);
-    funcstatus = ReadDetData(&buffer.front(),detTileName.c_str(), blNbr, highTagNbr, tagNbr);
+    md.BeamlineData()[detTileName+"_Width"] = width;
+    md.BeamlineData()[detTileName+"_Height"] = height;
+
+    /** retrieve the datasize of the detector tile in bytes */
+    int tiledatasize(0);
+    funcstatus = ReadSizeOfDetData(tiledatasize,detTileName.c_str(), blNbr, highTagNbr, tagNbr);
     if (funcstatus)
     {
-      Log::add(Log::ERROR,"retrieve Octal: could not retrieve data of '" +
+      Log::add(Log::ERROR,"retrieve Octal: could not retrieve datasize of '" +
                detName + "' for tag '" + toString(tagNbr) +
                "' ErrorCode is '" + toString(funcstatus) + "'");
       return 0;
     }
+    datasize += tiledatasize;
+
+    /** retrieve the position and tilt of the tile */
+    float posx(0), posy(0), posz(0), tilt_deg(0);
+    funcstatus = ReadDetPosX(posx,detTileName.c_str(), blNbr, highTagNbr, tagNbr);
+    if (funcstatus)
+    {
+      Log::add(Log::ERROR,"retrieve Octal: could not retrieve pos X of '" +
+               detName + "' for tag '" + toString(tagNbr) +
+               "' ErrorCode is '" + toString(funcstatus) + "'");
+      return 0;
+    }
+    funcstatus = ReadDetPosY(posy,detTileName.c_str(), blNbr, highTagNbr, tagNbr);
+    if (funcstatus)
+    {
+      Log::add(Log::ERROR,"retrieve Octal: could not retrieve pos Y of '" +
+               detName + "' for tag '" + toString(tagNbr) +
+               "' ErrorCode is '" + toString(funcstatus) + "'");
+      return 0;
+    }
+    funcstatus = ReadDetPosZ(posz,detTileName.c_str(), blNbr, highTagNbr, tagNbr);
+    if (funcstatus)
+    {
+      Log::add(Log::ERROR,"retrieve Octal: could not retrieve pos Z of '" +
+               detName + "' for tag '" + toString(tagNbr) +
+               "' ErrorCode is '" + toString(funcstatus) + "'");
+      return 0;
+    }
+    funcstatus = ReadDetRotationAngle(tilt_deg,detTileName.c_str(), blNbr, highTagNbr, tagNbr);
+    if (funcstatus)
+    {
+      Log::add(Log::ERROR,"retrieve Octal: could not retrieve tilt of '" +
+               detName + "' for tag '" + toString(tagNbr) +
+               "' ErrorCode is '" + toString(funcstatus) + "'");
+      return 0;
+    }
+    md.BeamlineData()[detTileName+"_PosX_um"] = posx;
+    md.BeamlineData()[detTileName+"_PosY_um"] = posy;
+    md.BeamlineData()[detTileName+"_PosZ_um"] = posz;
+    md.BeamlineData()[detTileName+"_Angle_deg"] = tilt_deg;
+
+    /** retrieve the pixelsize of the detector tile */
+    float pixsize_um(0);
+    funcstatus = ReadPixelSizeInMicroMeter(pixsize_um,detTileName.c_str(), blNbr, highTagNbr, tagNbr);
+    if (funcstatus)
+    {
+      Log::add(Log::ERROR,"retrieve Octal: could not retrieve pixelsize of '" +
+               detName + "' for tag '" + toString(tagNbr) +
+               "' ErrorCode is '" + toString(funcstatus) + "'");
+      return 0;
+    }
+    md.BeamlineData()[detTileName+"_PixSize_um"] = pixsize_um;
+
+    /** get the gain of the detector tile */
     float gain(0);
     funcstatus = ReadAbsGain(gain,detTileName.c_str(), blNbr, highTagNbr, tagNbr);
     if (funcstatus)
@@ -83,20 +149,66 @@ uint64_t retrieveOctal(pixeldetector::Detector &det, string detName,
                "' ErrorCode is '" + toString(funcstatus) + "'");
       return 0;
     }
+    md.BeamlineData()[detTileName+"_AbsGain"] = gain;
 
-    pixeldetector::frame_t::iterator tileStart(det.frame().begin() + i*512*1024);
+    /** detector recording frequency */
+    int ibuf(0);
+    funcstatus = ReadConfigOfDetRecordFreq(ibuf, const_cast<char*>(detTileName.c_str()),
+                                           blNbr, highTagNbr, tagNbr);
+    if (funcstatus)
+    {
+      Log::add(Log::ERROR,"retrieve Octal: could not retrieve det recording frequency of det '" +
+               detName + "' for tag '" + toString(tagNbr) + "' ErrorCode is '" +
+               toString(funcstatus) + "'");
+      return 0;
+    }
+    md.BeamlineData()[detTileName+"_DetectorRecordingFrequency"] = ibuf;
+
+    /** detector frequency */
+    funcstatus = ReadConfigOfDetFreq(ibuf, const_cast<char*>(detTileName.c_str()),
+                                     blNbr, highTagNbr, tagNbr);
+    if (funcstatus)
+    {
+      Log::add(Log::ERROR,"retrieve Octal: could not retrieve detector frequency of det '" +
+               detName + "' for tag '" + toString(tagNbr) + "' ErrorCode is '" +
+               toString(funcstatus) + "'");
+      return 0;
+    }
+    md.BeamlineData()[detTileName + "_DetectorFrequency"] = ibuf;
+
+    /** retrieve the detector data */
+    buffer.resize(width*height);
+    funcstatus = ReadDetData(&buffer.front(),detTileName.c_str(), blNbr, highTagNbr, tagNbr);
+    if (funcstatus)
+    {
+      Log::add(Log::ERROR,"retrieve Octal: could not retrieve data of '" +
+               detName + "' for tag '" + toString(tagNbr) +
+               "' ErrorCode is '" + toString(funcstatus) + "'");
+      return 0;
+    }
+
+    /** copy the data of the detector to the assembled image */
+    int sizebefore(det.frame().size());
+    det.frame().resize(sizebefore + width*height);
+    pixeldetector::frame_t::iterator tileStart(det.frame().begin() + sizebefore);
     copy(buffer.begin(),buffer.end(),tileStart);
 
-    if (i)
+    /** in an octal MPCCD one needs to leverage the different tiles by its gain,
+     *  taking the gain of the first tile as a reference
+     */
+    if (normalize)
     {
-      const float relGain(gain / gainRef);
-      pixeldetector::frame_t::iterator tileEnd(det.frame().begin() + (i+1)*512*1024);
-      transform(tileStart,tileEnd, tileStart, bind1st(multiplies<T>(),relGain));
+      if (i)
+      {
+        const float relGain(gain / gainRef);
+        pixeldetector::frame_t::iterator tileEnd(det.frame().end());
+        transform(tileStart,tileEnd, tileStart, bind1st(multiplies<T>(),relGain));
+      }
+      else
+        gainRef = gain;
     }
-    else
-      gainRef = gain;
   }
-  return det.frame().size() * sizeof(T);
+  return datasize;
 }
 
 
@@ -108,6 +220,9 @@ void SACLAConverter::loadSettings()
   CASSSettings s;
   s.beginGroup("SACLAConverter");
 
+  /** set the flag to retrieve the accelerator data */
+  _retrieveAcceleratorData = s.value("RetrieveAcceleratorData",true).toBool();
+
   /** set the requested octal detectors */
   int size = s.beginReadArray("OctalPixelDetectors");
   for (int i = 0; i < size; ++i)
@@ -115,26 +230,23 @@ void SACLAConverter::loadSettings()
     s.setArrayIndex(i);
     string detID(s.value("DetectorIDName","Invalid").toString().toStdString());
     int32_t key(s.value("CASSID",0).toInt());
-
+    bool normalize(s.value("NormalizeToAbsGain",true).toBool());
     /** skip if the detector name has not been set */
     if (detID == "Invalid")
       continue;
-
-    _octalDetectors[key] = detID;
+    _octalDetectors[key] = make_pair(detID,normalize);
   }
   s.endArray();
 
   /** set the requested octal detectors */
-  size = s.beginReadArray("MachineParameters");
+  size = s.beginReadArray("DatabaseValues");
   for (int i = 0; i < size; ++i)
   {
     s.setArrayIndex(i);
     string machineValName(s.value("ValueName","Invalid").toString().toStdString());
-
     /** skip if the value name has not been set */
     if (machineValName == "Invalid")
       continue;
-
     _machineVals.push_back(machineValName);
   }
   s.endArray();
@@ -156,6 +268,66 @@ uint64_t SACLAConverter::operator()(const int blNbr, const int highTagNbr,
     throw runtime_error("SACLAConverter():The CASSEvent does not contain a Machine Data Device");
   MachineDataDevice &md
     (*dynamic_cast<MachineDataDevice*>(event.devices()[CASSEvent::MachineData]));
+
+
+  /** if requested retrieve the accelarator parameters */
+  if (_retrieveAcceleratorData)
+  {
+    int funcstatus(0);
+    double fbuf(0);
+    int ibuf(0);
+    /** electron energy */
+    funcstatus = ReadConfigOfElectronEnergy(fbuf, blNbr, highTagNbr, tagNbr);
+    if (funcstatus)
+    {
+      Log::add(Log::ERROR,"retrieve Octal: could not retrieve electron energy for tag'" +
+               toString(tagNbr) + "' ErrorCode is '" + toString(funcstatus) + "'");
+      return 0;
+    }
+    md.BeamlineData()["Acc_electronEnergy_eV"] = fbuf;
+
+    /** k-params */
+    funcstatus = ReadConfigOfKPars(fbuf, blNbr, highTagNbr, tagNbr);
+    if (funcstatus)
+    {
+      Log::add(Log::ERROR,"retrieve Octal: could not retrieve k params for tag '" +
+               toString(tagNbr) + "' ErrorCode is '" + toString(funcstatus) + "'");
+      return 0;
+    }
+    md.BeamlineData()["Acc_KParams"] = fbuf;
+
+    /** the set photon energy */
+    funcstatus = ReadConfigOfPhotonEnergy(fbuf, blNbr, highTagNbr, tagNbr);
+    if (funcstatus)
+    {
+      Log::add(Log::ERROR,"retrieve Octal: could not retrieve set photon energy for tag '" +
+               toString(tagNbr) + "' ErrorCode is '" + toString(funcstatus) + "'");
+      return 0;
+    }
+    md.BeamlineData()["Acc_PhotonEnergy"] = fbuf;
+
+    /** sacla frequency */
+    funcstatus = ReadConfigOfSACLAFreq(ibuf, blNbr, highTagNbr, tagNbr);
+    if (funcstatus)
+    {
+      Log::add(Log::ERROR,"retrieve Octal: could not retrieve sacla frequency for tag '" +
+               toString(tagNbr) + "' ErrorCode is '" + toString(funcstatus) + "'");
+      return 0;
+    }
+    md.BeamlineData()["Acc_SACLAFrequency"] = ibuf;
+
+    /** master frequency */
+    funcstatus = ReadConfigOfMasterFreq(ibuf, highTagNbr, tagNbr);
+    if (funcstatus)
+    {
+      Log::add(Log::ERROR,"retrieve Octal: could not retrieve master frequency for tag '" +
+               toString(tagNbr) + "' ErrorCode is '" + toString(funcstatus) + "'");
+      return 0;
+    }
+    md.BeamlineData()["Acc_MasterFrequency"] = ibuf;
+  }
+
+
   /** go through all requested machine data events and retrieve the corresponding *  values for the tag */
   vector<string>::const_iterator machineValsIter(_machineVals.begin());
   vector<string>::const_iterator machineValsEnd(_machineVals.end());
@@ -164,11 +336,14 @@ uint64_t SACLAConverter::operator()(const int blNbr, const int highTagNbr,
     /** retrieve the machine data value as string */
     vector<string> machineValueStringList;
     vector<int>tagNbrList(1,tagNbr);
-    if (ReadSyncDataList(&machineValueStringList,const_cast<char*>(machineValsIter->c_str()),
-                         highTagNbr,tagNbrList) != 0)
+    int funcstatus(0);
+    funcstatus = ReadSyncDataList(&machineValueStringList,const_cast<char*>(machineValsIter->c_str()),
+                                  highTagNbr,tagNbrList);
+    if (funcstatus)
     {
       Log::add(Log::ERROR,"SACLAConverter: could not retrieve value of '" +
-               *machineValsIter + "' for tag '" + toString(tagNbr) + "'");
+               *machineValsIter + "' for tag '" + toString(tagNbr) +
+               "' ErrorCode is '" + toString(funcstatus) + "'");
       continue;
     }
     if (machineValueStringList.size() != 1)
@@ -178,7 +353,18 @@ uint64_t SACLAConverter::operator()(const int blNbr, const int highTagNbr,
                "' did not return the right size");
       continue;
     }
-
+    /** sync data frequency */
+    int ibuf(0);
+    funcstatus = ReadConfigOfSyncDataFreq(ibuf, const_cast<char*>(machineValsIter->c_str()),
+                                          highTagNbr, tagNbr);
+    if (funcstatus)
+    {
+      Log::add(Log::ERROR,"retrieve Octal: could not retrieve sync data frequency of '" +
+               *machineValsIter + "' for tag '" + toString(tagNbr) +
+               "' ErrorCode is '" + toString(funcstatus) + "'");
+      return 0;
+    }
+    md.BeamlineData()[*machineValsIter + "_SyncDataFrequency"] = ibuf;
     /** check if retrieved value can be converted to double, and if so add it
      *  to the machine data, otherwise issue an error and continue
      */
@@ -203,16 +389,16 @@ uint64_t SACLAConverter::operator()(const int blNbr, const int highTagNbr,
     throw runtime_error("pixeldetector::retrieveDet: There is no  pixeldetector device within the CASSEvent");
   pixeldetector::Device &dev (*dynamic_cast<pixeldetector::Device*>(devIt->second));
 
-  map<int32_t,string>::const_iterator octalDetsIter(_octalDetectors.begin());
-  map<int32_t,string>::const_iterator octalDetsEnd(_octalDetectors.end());
+  octalDets_t::const_iterator octalDetsIter(_octalDetectors.begin());
+  octalDets_t::const_iterator octalDetsEnd(_octalDetectors.end());
   for (; octalDetsIter != octalDetsEnd; ++octalDetsIter)
   {
     /** determine the detector pixel data type */
     Sacla_DetDataType type;
-    if (ReadDetDataType(type,octalDetsIter->second.c_str(), blNbr, highTagNbr, tagNbr) != 0)
+    if (ReadDetDataType(type,octalDetsIter->second.first.c_str(), blNbr, highTagNbr, tagNbr) != 0)
     {
       Log::add(Log::ERROR,"SACLAConverter: could not retrieve data type of '" +
-               octalDetsIter->second + "' for tag '" + toString(tagNbr) + "'");
+               octalDetsIter->second.first + "' for tag '" + toString(tagNbr) + "'");
       continue;
     }
 
@@ -224,12 +410,13 @@ uint64_t SACLAConverter::operator()(const int blNbr, const int highTagNbr,
     switch(type)
     {
       case Sacla_DATA_TYPE_FLOAT:
-        datasize += retrieveOctal<float>(det,octalDetsIter->second,
+        datasize += retrieveOctal<float>(det,md,octalDetsIter->second.first,
+                                         octalDetsIter->second.second,
                                          blNbr,highTagNbr,tagNbr);
         break;
       default:
         Log::add(Log::ERROR,"SACLAConverter: Data type of octal detector '" +
-                 octalDetsIter->second + "' for tag '" + toString(tagNbr) +
+                 octalDetsIter->second.first + "' for tag '" + toString(tagNbr) +
                  "' is unkown");
         break;
     }
@@ -237,34 +424,4 @@ uint64_t SACLAConverter::operator()(const int blNbr, const int highTagNbr,
   }
 
   return datasize;
-
-//  string line;
-//  vector<double> values;
-//  while(true)
-//  {
-//    getline(file, line);
-//    values.clear();
-//    _split(line,values,_delim);
-//    if (!values.empty())
-//      break;
-//  }
-//  if(_headers.size() != values.size())
-//    throw runtime_error("TextReader(): In file '" + _filename +
-//                        "' are not enough values for the amount of values '" + toString(values.size()) +
-//                        "' suggested by the header '" + toString(_headers.size())+
-//                        "'. This is the line: " + line);
-//
-//  if (event.devices().find(CASSEvent::MachineData) == event.devices().end())
-//    throw runtime_error("TextReader():The CASSEvent does not contain a Machine Data Device");
-//
-//  MachineDataDevice &md
-//    (*dynamic_cast<MachineDataDevice*>(event.devices()[CASSEvent::MachineData]));
-//
-//  vector<double>::const_iterator value(values.begin());
-//  vector<string>::const_iterator head(_headers.begin());
-//  for (;value != values.end(); ++value, ++head)
-//    md.BeamlineData()[*head] = *value;
-//
-//  event.id() = md.BeamlineData()[_eventIdhead];
-//
 }
--- cass/sacla_converter.h
+++ cass/sacla_converter.h
@@ -15,20 +15,61 @@ namespace cass
 {
 class CASSEvent;
 
-/** class for reading txt files
+/** Class for reading SACLA data.
  *
- * first line has to give the names of the variables, which are then ordered
- * line by line.
+ * This class will interface the API provided by SACLA to retrieve the available
+ * data. One can use it to retrieve data from the Database, Octal Detector and
+ * other detectors.
  *
- * @cassttng TxtReader/\%filename\%/{Deliminator}\n
- *           The deliminator that is used to separate the values. Default is '\\t',
- *           which is a tab.
- * @cassttng TxtReader/\%filename\%/{EventIdHeader}\n
- *           The name of the Header under which the Event Id is stored. Default
- *           is "".
- * @cassttng TxtReader/\%filename\%/{LinesToSkip}\n
- *           How many lines do we have to skip before the line appears that
- *           contains the headers. Default is 3
+ * The additional data of the MPCCD, such as the position, angle, gain,
+ * width and height will be written in to the BeamlineData and can be
+ * retrieved for each tile using the tilename:
+ * - "%tilename%_Width": The number of pixels in X
+ * - "%tilename%_Height": The number of pixels in Y
+ * - "%tilename%_PosX_um": The x position of the tile in um
+ * - "%tilename%_PosY_um": The y position of the tile in um
+ * - "%tilename%_PosZ_um": The z position of the tile in um
+ * - "%tilename%_Angle_deg": The angle of the tile in deg
+ * - "%tilename%_PixSize_um": The pixel size in um
+ * - "%tilename%_AbsGain": The absolute gain of the tile
+ * - "%tilename%_DetectorRecordingFrequency": The detector recording frequency
+ * - "%tilename%_DetectorFrequency": The detector frequency
+ *
+ * For each requested Database Parameter the following additional information is
+ * available in the machine data:
+ * - "%ValueName%_SyncDataFrequency": The recorded frequency
+ *
+ * @cassttng OctalPixelDetectors/{RetrieveAcceleratorData}\n
+ *           Flag that tells whether the accelerator data should be retrieved.
+ *           Default is true. When set to true the following data is available
+ *           in the Machine Data:
+ *           - "Acc_electronEnergy_eV"
+ *           - "Acc_KParams"
+ *           - "Acc_PhotonEnergy"
+ *           - "Acc_SACLAFrequency"
+ *           - "Acc_MasterFrequency"
+ * @cassttng OctalPixelDetectors/{size}\n
+ *           The number of octal MPCCD detectors one wants to retrieve. Be sure
+ *           that for each detector there is a unique id in the list below.
+ * @cassttng OctalPixelDetectors/\%index\%/{DetectorName}\n
+ *           The base name of the octal MPCCD. Unlike the API this name will be
+ *           used to determine the names of the individual tiles of the MPCCD.
+ *           Default is "Invalid" which will caus to skip this index.
+ * @cassttng OctalPixelDetectors/\%index\%/{CASSID}\n
+ *           The id of the detector that CASS will use internally.
+ * @cassttng OctalPixelDetectors/\%index\%/{NormalizeToAbsGain}\n
+ *           Using this option one can control whether the pixel values of the
+ *           individual tiles will be normalized to the gain value of the first
+ *           tile. When true, the pixel values of tiles 2 to 8 will modified
+ *           using:
+ *           \f$ pixval_{tile} = \frac{gain_{tile}}{gain_{tile1}}*pixval_{tile}\f$
+ *           Default is true.
+ * @cassttng DatabaseValues/{size}\n
+ *           The number of values one wants to retrieve from the database. Be sure
+ *           that for each detector there is a unique id in the list below.
+ * @cassttng DatabaseValues/\%index\%/{ValueName}\n
+ *           The name of the database value to retrieve. Default is "Invalid"
+ *           which will cause to skip that index.
  *
  * @author Lutz Foucar
  */
@@ -56,8 +97,14 @@ private:
   /** the list of requested machine values */
   std::vector<std::string> _machineVals;
 
+  /** define the octal detectors container type */
+  typedef std::map<int32_t,std::pair<std::string,bool> > octalDets_t;
+
   /** the list of requested octal detectors */
-  std::map<int32_t,std::string> _octalDetectors;
+  octalDets_t _octalDetectors;
+
+  /** flag to tell whether to retrieve the accelerator data */
+  bool _retrieveAcceleratorData;
 };
 }//end namespace cass
 #endif
