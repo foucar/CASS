9dc1edafb12462f19d705ead94aa26a809519c70
Lutz Foucar <lutz.foucar@mpimf-heidelberg.mpg.de>
2015-07-10 12:58:43 +0200

    updated the deploy script at MPIK

--- scripts/deployMPIK.sh
+++ scripts/deployMPIK.sh
@@ -14,47 +14,61 @@ INSTLOC=/lfs/l4/ullrch/foucar/cass/v$(git describe --abbrev=4)
 
 
 # set the environment variables
-export ROOTSYS=/lfs/l3/asg/root_v5.32.00
-export QTDIR=/lfs/l3/asg/qt/4.8.2
-export HDF5DIR=/lfs/l3/asg
-export GSOAPDIR=/lfs/l3/asg
-export QWTDIR=/lfs/l4/ullrch/foucar/cass/dependencies/qwt/v6.0.1
-export VIGRADIR=/lfs/l3/asg
+export ROOTSYS=/lfs/l4/ullrch/foucar/dependencies/root/v5.32.04
+export QTDIR=/lfs/l4/ullrch/foucar/dependencies/qt/v4.8.4
+export HDF5DIR=/lfs/l4/ullrch/foucar/dependencies/hdf5/v1.8.12
+export GSOAPDIR=/lfs/l4/ullrch/foucar/dependencies/gsoap/v2.8.17
+export QWTDIR=/lfs/l4/ullrch/foucar/cass/dependencies/qwt/v6.1.0
+export VIGRADIR=/lfs/l4/ullrch/foucar/dependencies/vigra/v1.10.0
 export DOXYDIR=/lfs/l4/ullrch/foucar/cass/dependencies/doxygen/v1.8.4
+export FFTWDIR=/lfs/l4/ullrch/foucar/cass/dependencies/fftw/v3.3.3
 
-export PATH=$ROOTSYS/bin:$QTDIR/bin:$GSOAPDIR/bin:$PATH
+export PATH=$QTDIR/bin:$PATH
 
 
 
 
 ### start build and install process
 
-# ususally one now copies the cass_defaultconfig.pri file to 
-# cass_myconfig.pri and then edits the cass_myconfig.pri file by hand. 
-# Since this script should automatically install CASS we need to modify the 
+# usually one now copies the cass_defaultconfig.pri file to
+# cass_myconfig.pri and then edits the cass_myconfig.pri file by hand.
+# Since this script should automatically install CASS we need to modify the
 # cass_defaultconfig.pri file from within this script. We add all options that
 # we want to have enabled just at the end of the file.
-echo "CONFIG += hdf5"                         >> ${BASEDIR}/cass_defaultconfig.pri
-echo "QMAKE_INCDIR   += "${HDF5DIR}"/include" >> ${BASEDIR}/cass_defaultconfig.pri
-echo "QMAKE_LIBDIR   += "${HDF5DIR}"/lib"     >> ${BASEDIR}/cass_defaultconfig.pri
-echo "QMAKE_RPATHDIR += "${HDF5DIR}"/lib"     >> ${BASEDIR}/cass_defaultconfig.pri
+echo "QMAKE_INCDIR   *= "${GSOAPDIR}"/include/"     >> ${BASEDIR}/cass_defaultconfig.pri
+echo "QMAKE_LIBDIR   *= "${GSOAPDIR}"/lib"          >> ${BASEDIR}/cass_defaultconfig.pri
+echo "QMAKE_RPATHDIR *= "${GSOAPDIR}"/lib"          >> ${BASEDIR}/cass_defaultconfig.pri
+echo "GSOAP_BIN       = "${GSOAPDIR}"/bin/soapcpp2" >> ${BASEDIR}/cass_defaultconfig.pri
 
-echo "CONFIG += singleparticle_hit"          >> ${BASEDIR}/cass_defaultconfig.pri
-echo "QMAKE_INCDIR += "${VIGRADIR}"/include" >> ${BASEDIR}/cass_defaultconfig.pri
+echo "CONFIG         *= DOCS"                     >> ${BASEDIR}/cass_defaultconfig.pri
+echo "DOXYGEN_BIN     = "${DOXYDIR}"/bin/doxygen" >> ${BASEDIR}/cass_defaultconfig.pri
 
-echo "CONFIG += cernroot"                     >> ${BASEDIR}/cass_defaultconfig.pri
-echo "QMAKE_INCDIR   += "${ROOTSYS}"/include" >> ${BASEDIR}/cass_defaultconfig.pri
-echo "QMAKE_LIBDIR   += "${ROOTSYS}"/lib"     >> ${BASEDIR}/cass_defaultconfig.pri
-echo "QMAKE_RPATHDIR += "${ROOTSYS}"/lib"     >> ${BASEDIR}/cass_defaultconfig.pri
+echo "CONFIG         *= hdf5"                 >> ${BASEDIR}/cass_defaultconfig.pri
+echo "QMAKE_INCDIR   *= "${HDF5DIR}"/include" >> ${BASEDIR}/cass_defaultconfig.pri
+echo "QMAKE_LIBDIR   *= "${HDF5DIR}"/lib"     >> ${BASEDIR}/cass_defaultconfig.pri
+echo "QMAKE_RPATHDIR *= "${HDF5DIR}"/lib"     >> ${BASEDIR}/cass_defaultconfig.pri
 
-echo "CONFIG += JoCASSView"                  >> ${BASEDIR}/cass_defaultconfig.pri
-echo "QMAKE_INCDIR   += "${QWTDIR}"/include" >> ${BASEDIR}/cass_defaultconfig.pri
-echo "QMAKE_LIBDIR   += "${QWTDIR}"/lib"     >> ${BASEDIR}/cass_defaultconfig.pri
-echo "QMAKE_RPATHDIR += "${QWTDIR}"/lib"     >> ${BASEDIR}/cass_defaultconfig.pri
+echo "CONFIG       *= singleparticle_hit"    >> ${BASEDIR}/cass_defaultconfig.pri
+echo "QMAKE_INCDIR *= "${VIGRADIR}"/include" >> ${BASEDIR}/cass_defaultconfig.pri
 
-echo "CONFIG += LuCASSView"                  >> ${BASEDIR}/cass_defaultconfig.pri
+echo "CONFIG         *= cernroot"                     >> ${BASEDIR}/cass_defaultconfig.pri
+echo "QMAKE_INCDIR   *= "${ROOTSYS}"/include"         >> ${BASEDIR}/cass_defaultconfig.pri
+echo "QMAKE_LIBDIR   *= "${ROOTSYS}"/lib"             >> ${BASEDIR}/cass_defaultconfig.pri
+echo "QMAKE_RPATHDIR *= "${ROOTSYS}"/lib"             >> ${BASEDIR}/cass_defaultconfig.pri
+echo "ROOTCINT_BIN    = "${ROOTSYS}"/bin/rootcint"    >> ${BASEDIR}/cass_defaultconfig.pri
+echo "ROOTCONFIG_BIN  = "${ROOTSYS}"/bin/root-config" >> ${BASEDIR}/cass_defaultconfig.pri
 
-echo "DOXYGEN = "${DOXYDIR}"/bin/doxygen"    >> ${BASEDIR}/cass_defaultconfig.pri
+echo "CONFIG         *= fftw"                 >> ${BASEDIR}/cass_defaultconfig.pri
+echo "QMAKE_INCDIR   *= "${FFTWDIR}"/include" >> ${BASEDIR}/cass_defaultconfig.pri
+echo "QMAKE_LIBDIR   *= "${FFTWDIR}"/lib"     >> ${BASEDIR}/cass_defaultconfig.pri
+echo "QMAKE_RPATHDIR *= "${FFTWDIR}"/lib"     >> ${BASEDIR}/cass_defaultconfig.pri
+
+echo "CONFIG         *= JoCASSView"          >> ${BASEDIR}/cass_defaultconfig.pri
+echo "QMAKE_INCDIR   *= "${QWTDIR}"/include" >> ${BASEDIR}/cass_defaultconfig.pri
+echo "QMAKE_LIBDIR   *= "${QWTDIR}"/lib"     >> ${BASEDIR}/cass_defaultconfig.pri
+echo "QMAKE_RPATHDIR *= "${QWTDIR}"/lib"     >> ${BASEDIR}/cass_defaultconfig.pri
+
+echo "CONFIG *= LuCASSView" >> ${BASEDIR}/cass_defaultconfig.pri
 
 echo "QMAKE_CXXFLAGS += -fopenmp"            >> ${BASEDIR}/cass_defaultconfig.pri
 echo "QMAKE_LFLAGS   += -fopenmp"            >> ${BASEDIR}/cass_defaultconfig.pri
